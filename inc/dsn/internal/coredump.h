e8322c7a44ecb2ca70c180ff8dc0667a24b1025f
Zhenyu Guo <zhenyu.guo@microsoft.com>
2015-03-31 15:38:34 +0800

    enable timer randomization

--- inc/dsn/internal/coredump.h
+++ inc/dsn/internal/coredump.h
@@ -1,8 +1,7 @@
 /*
  * The MIT License (MIT)
 
- * Copyright (c) 2015 Microsoft Corporation
- * Robust Distributed System Nucleus (rDSN)
+ * Copyright (c) 2015 Microsoft Corporation, Robust Distributed System Nucleus (rDSN)
 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
