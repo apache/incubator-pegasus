c83fe494e920a2ed980c1f0041f0a77483dcae6f
Zhenyu Guo <imzhenyu@outlook.com>
2015-04-09 10:56:01 +0800

    refine php type defs

--- codegen/libs/type.php
+++ codegen/libs/type.php
@@ -36,7 +36,7 @@ class t_type
 	{
 		$this->program = $program;
 		$this->name = $name;
-		$_PROG->types[] = $this;
+		$program->types[] = $this;
 	}
 	
 	function is_void() { return false; }
@@ -52,7 +52,7 @@ class t_typedef extends t_type
 	{
 		parent::__construct($program, $alias);
 		$this->type = $type;
-		$_PROG->typedefs[] = $this;
+		$program->typedefs[] = $this;
 	}	
 }
 
@@ -64,7 +64,7 @@ class t_enum extends t_type
 	{
 		parent::__construct($program, $name);
 		$this->values = array();
-		$_PROG->enums[] = $this;
+		$program->enums[] = $this;
 	}
 	
 	function add_value($name, $value)
@@ -95,7 +95,7 @@ class t_struct extends t_type
 	{
 		parent::__construct($program, $name);
 		$this->fields = array();
-		$_PROG->structs[] = $this;
+		$program->structs[] = $this;
 	}
 	
 	function add_field($name, $type_name)
@@ -135,7 +135,7 @@ class t_service extends t_type
 	{
 		parent::__construct($program, $name);
 		$this->functions = array();
-		$_PROG->services[] = $this;
+		$program->services[] = $this;
 	}
 	
 	function add_function($ret, $name)
@@ -145,227 +145,4 @@ class t_service extends t_type
 		return $f;
 	}
 }
-
-/*
-class Func {
-	var $module;
-	var $name;
-	var $rawName;
-	var $attrs;
-	var $ret;
-	var $params;
-	var $space;
-	
-	function __construct($module, $name, $attrs, $ret, $params) {
-		$this->module = $module;
-		$this->name = $name;
-		$this->attrs = $attrs;
-		$this->ret = $ret;
-		$this->params = $params;
-		$this->rawName = $name;
-		$this->space = "BEYOND_DEFAULT_MONITOR_MODE";
-	}
-
-	function has_key($key)
-	{
-		return array_key_exists($key, $this->attrs);
-	}
-
-	function value_by_key($key)
-	{
-		if (array_key_exists($key, $this->attrs))
-			return $this->attrs[$key];
-		else
-			return false;
-	}
-
-	function has_attr($aname)
-	{
-		return $this->has_key($aname);
-	}
-	
-	function has_nonh_out_pa()
-	{
-		foreach ($this->params as $pa) 
-		{
-			if ($pa->has_attr('out') && !$pa->is_handle())
-				return true;
-		}
-		return false;
-	}
-	
-	function has_named_param($pname)
-	{
-		foreach ($this->params as $pa) 
-		{
-			if ($pa->name == $pname)
-				return true;
-		}
-		return false;
-	}
-	
-	function has_attred_param($att)
-	{
-		foreach ($this->params as $pa) 
-		{
-			if ($pa->has_attr($att))
-				return true;
-		}
-		return false;
-	}
-	function get_attred_param($att)
-	{
-		foreach ($this->params as $pa) 
-		{
-			if ($pa->has_attr($att))
-				return $pa;
-		}
-		return NULL;
-	}
-
-	function get_res_handle()
-	{
-		$rh = "";
-		foreach ($this->params as $pa) 
-		{
-			if (array_key_exists('handle', $pa->attrs))
-			{
-				if (!array_key_exists('out', $pa->attrs) || array_key_exists('in', $pa->attrs))  // sole [in] annotation may be omited, but [in] cannot be omited in case of [in,out]
-				{
-					$rh = $pa->name;
-					break;
-				}
-			}
-		}
-	
-		if ($rh != "")
-			return $rh;
-		else if (count($this->params) == 0)
-			return "RESID_VOID";
-		else if (array_key_exists('out', $this->params[0]->attrs) && !array_key_exists('in', $this->params[0]->attrs))
-			return "RESID_UNKNOWN";
-		else
-			return "(*(uint32*)(&" . $this->params[0]->name . "))";
-	}
-
-	function is_ret_void()
-	{
-		return (strcasecmp($this->ret->type, "VOID") == 0);
-	}
-	
-	function get_succ_predicate()
-	{
-		$pred = "";
-		if ($this->has_attr('succ'))
-			$pred = $this->attrs['succ'];
-		else if ($this->ret->type == "NTSTATUS")
-			$pred = "NT_SUCCESS(sx_last_ret)";
-		else if ($this->ret->type == "BOOL" || $this->ret->type == "bool")
-			$pred = "sx_last_ret != FALSE";
-			
-		if ($pred != "")
-			$pred = str_replace("return", "sx_last_ret", $pred);
-		return $pred;
-	}
-
-	function get_fail_predicate()
-	{
-		$pred = "";
-		if ($this->has_attr('fail'))
-			$pred = $this->attrs['fail'];
-		else if ($this->ret->type == "NTSTATUS")
-			$pred = "!NT_SUCCESS(sx_last_ret)";
-		else if ($this->ret->type == "BOOL" || $this->ret->type == "bool")
-			$pred = "sx_last_ret == FALSE";
-			
-		if ($pred != "")
-			$pred = str_replace("return", "sx_last_ret", $pred);
-		return $pred;
-	}
-	
-	function get_event_flag()
-	{
-		$ev_flag = "EVENT_FLAG_NORMAL";
-		if ($this->has_attr('nlr'))
-			$ev_flag = $ev_flag . "|EVENT_FLAG_API_NLR";
-		if ($this->has_attr('cache'))
-			$ev_flag = $ev_flag . "|EVENT_FLAG_API_CACHE";
-		if ($this->has_attr('nonblock'))
-			$ev_flag = $ev_flag . "|EVENT_FLAG_NON_BLOCK";
-		if ($this->has_attr('sched'))
-			$ev_flag = $ev_flag . "|EVENT_FLAG_API_USER_SCHED";
-		return $ev_flag;
-	}
-}
-
-class Param {
-	var $type;
-	var $name;
-	var $attrs;
-	function __construct($type, $name, $attrs) {
-		$this->type = $type;
-		$this->name = $name;
-		$this->attrs = $attrs;
-	}
-
-	function has_key($key)
-	{
-		return array_key_exists($key, $this->attrs);
-	}
-
-	function value_by_key($key)
-	{
-		if (array_key_exists($key, $this->attrs))
-			return $this->attrs[$key];
-		else
-			return false;
-	}
-
-	function get_binded_prototype()
-	{
-		global $_FUNCTION_PROTOTYPES;
-		if (array_key_exists($this->type, $_FUNCTION_PROTOTYPES))
-			return $_FUNCTION_PROTOTYPES[$this->type];
-		else
-			return false;
-	}
-
-	function get_etype()
-	{
-		$etype = "";
-		if ($this->has_attr('bcap') || $this->has_attr('bsize'))
-			$etype = "CHAR";
-		else if ($this->has_attr('etype'))
-			$etype = $this->attrs['etype'];
-		else
-			$etype = "deref<" . $this->type . ">::type";
-		return $etype;
-	}
-
-	function get_ecap()
-	{
-		$ecap = "";
-		if ($this->has_attr('bcap') || $this->has_attr('bsize'))
-		{
-			if ($this->has_attr('bcap'))
-				$ecap = $this->attrs['bcap'];
-			else
-				$ecap = $this->attrs['bsize'];
-		}
-		else if ($this->has_attr('ecap') || $this->has_attr('esize'))
-		{
-			if ($this->has_attr('ecap'))
-				$ecap = $this->attrs['ecap'];
-			else
-				$ecap = $this->attrs['esize'];
-		}			
-		
-		if ($ecap != "")
-			$ecap = str_replace("return", "sx_last_ret", $ecap);
-
-		return $ecap;
-	}
-
-}
-*/
 ?>
