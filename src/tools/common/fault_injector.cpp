fca50b500ba12a278de243b1d5f5d13b522b1489
imzhenyu <zhenyu.guo@microsoft.com>
2015-05-31 23:28:34 +0800

    disk fault injection delay too large

--- src/tools/common/fault_injector.cpp
+++ src/tools/common/fault_injector.cpp
@@ -59,13 +59,13 @@ namespace dsn {
 
         CONFIG_BEGIN(fj_opt)
             CONFIG_FLD(double, rpc_request_drop_ratio, 0.0001)
-            CONFIG_FLD(double, rpc_response_drop_ratio, 0.0001)
-            CONFIG_FLD(double, disk_read_fail_ratio, 0.0)
-            CONFIG_FLD(double, disk_write_fail_ratio, 0.0)
+            CONFIG_FLD(double, rpc_response_drop_ratio, 0.001)
+            CONFIG_FLD(double, disk_read_fail_ratio, 0.00001)
+            CONFIG_FLD(double, disk_write_fail_ratio, 0.00001)
 
             CONFIG_FLD(uint32_t, rpc_message_delay_ms_min, 0)
             CONFIG_FLD(uint32_t, rpc_message_delay_ms_max, 1000)
-            CONFIG_FLD(uint32_t, rpc_message_delay_ms_min, 1)
+            CONFIG_FLD(uint32_t, disk_io_delay_ms_min, 1)
             CONFIG_FLD(uint32_t, disk_io_delay_ms_max, 12)
             CONFIG_FLD(uint32_t, execution_extra_delay_us_max, 0)
 
