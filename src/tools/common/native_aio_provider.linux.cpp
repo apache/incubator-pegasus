ac83afa2b9a873a6f006b4f415ef77ebe7bc94d2
lishenglong <lishenglong1002@gmail.com>
2015-06-16 15:28:50 +0800

    add linux kernel aio

--- src/tools/common/native_aio_provider.linux.cpp
+++ src/tools/common/native_aio_provider.linux.cpp
@@ -1,28 +1,28 @@
 /*
- * The MIT License (MIT)
- *
- * Copyright (c) 2015 Microsoft Corporation
- * 
- * -=- Robust Distributed System Nucleus (rDSN) -=- 
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
+* The MIT License (MIT)
+*
+* Copyright (c) 2015 Microsoft Corporation
+*
+* -=- Robust Distributed System Nucleus (rDSN) -=-
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+*/
 
 #include "native_aio_provider.linux.h"
 
@@ -34,56 +34,56 @@
 #define __TITLE__ "aio.provider.native"
 
 namespace dsn {
-    namespace tools {
+	namespace tools {
 
-        native_linux_aio_provider::native_linux_aio_provider(disk_engine* disk, aio_provider* inner_provider)
-            : aio_provider(disk, inner_provider)
-        {
+		native_linux_aio_provider::native_linux_aio_provider(disk_engine* disk, aio_provider* inner_provider)
+			: aio_provider(disk, inner_provider)
+		{
 			new std::thread(std::bind(&native_linux_aio_provider::get_event, this));
-        }
+		}
 
-        native_linux_aio_provider::~native_linux_aio_provider()
-        {
-        }
+		native_linux_aio_provider::~native_linux_aio_provider()
+		{
+		}
 
-        handle_t native_linux_aio_provider::open(const char* file_name, int flag, int pmode)
-        {
-            return (handle_t)::open(file_name, flag, pmode);
-        }
+		handle_t native_linux_aio_provider::open(const char* file_name, int flag, int pmode)
+		{
+			return (handle_t)::open(file_name, flag, pmode);
+		}
 
-        error_code native_linux_aio_provider::close(handle_t hFile)
-        {
-            // TODO: handle failure
-            ::close(static_cast<int>(hFile));
-            return ERR_SUCCESS;
-        }
+		error_code native_linux_aio_provider::close(handle_t hFile)
+		{
+			// TODO: handle failure
+			::close(static_cast<int>(hFile));
+			return ERR_SUCCESS;
+		}
 
-       	disk_aio_ptr native_linux_aio_provider::prepare_aio_context(aio_task* tsk)
-        {
-            auto r = new linux_disk_aio_context;
-            bzero((char*)&r->cb, sizeof(r->cb));
-            r->tsk = tsk;
-            r->evt = nullptr;
-            return disk_aio_ptr(r);
-        }
+		disk_aio_ptr native_linux_aio_provider::prepare_aio_context(aio_task* tsk)
+		{
+			auto r = new linux_disk_aio_context;
+			bzero((char*)&r->cb, sizeof(r->cb));
+			r->tsk = tsk;
+			r->evt = nullptr;
+			return disk_aio_ptr(r);
+		}
 
-        void native_linux_aio_provider::aio(aio_task_ptr& aio_tsk)
-        {
-            aio_internal(aio_tsk, true);
-        }
-        
+		void native_linux_aio_provider::aio(aio_task_ptr& aio_tsk)
+		{
+			aio_internal(aio_tsk, true);
+		}
+
 		void native_linux_aio_provider::get_event()
 		{
 			struct io_event events[1];
 			int ret;
 			io_context_t ctx;
 			linux_disk_aio_context * aio;
-			
-			while(true)
+
+			while (true)
 			{
 				{
 					::dsn::service::zauto_lock l(_lock);
-					if(_ctx_q.empty())
+					if (_ctx_q.empty())
 						continue;
 					ctx = _ctx_q.front();
 					aio = _aio_q.front();
@@ -92,11 +92,11 @@ namespace dsn {
 					_aio_q.pop();
 				}
 				ret = io_getevents(ctx, 1, 1, events, NULL);
-				if(ret > 0) // should be 1
+				if (ret > 0) // should be 1
 				{
-					for(int i = 0; i < ret; i++)
+					for (int i = 0; i < ret; i++)
 					{
-						io_callback_t cb = (io_callback_t) events[i].data;
+						io_callback_t cb = (io_callback_t)events[i].data;
 						cb(ctx, &aio->cb, events[i].res, events[i].res2);
 					}
 				}
@@ -109,16 +109,16 @@ namespace dsn {
 			linux_disk_aio_context * aio = container_of(this_cb, linux_disk_aio_context, cb);
 
 			size_t bytes = size_t(this_cb->u.c.nbytes); // from e.g., read or write
-			if(res2 != 0)
+			if (res2 != 0)
 			{
 				derror("aio error");
 			}
-			if(res != bytes)
+			if (res != bytes)
 			{
 				derror("aio bytes miss");
 			}
 
-			if(!aio->evt)
+			if (!aio->evt)
 			{
 				aio_task_ptr aio_ptr(aio->tsk);
 				aio->this_->complete_io(aio_ptr, (res2 == 0 && res == bytes) ? ERR_SUCCESS : ERR_FILE_OPERATION_FAILED, bytes);
@@ -131,41 +131,41 @@ namespace dsn {
 			}
 		}
 
-        error_code native_linux_aio_provider::aio_internal(aio_task_ptr& aio_tsk, bool async, __out_param uint32_t* pbytes /*= nullptr*/)
-        {
+		error_code native_linux_aio_provider::aio_internal(aio_task_ptr& aio_tsk, bool async, __out_param uint32_t* pbytes /*= nullptr*/)
+		{
 			struct iocb *cbs[1];
 			io_context_t ctx;
 			linux_disk_aio_context * aio;
 			int ret;
 
-			memset (&ctx, 0, sizeof(ctx));
-			
+			memset(&ctx, 0, sizeof(ctx));
+
 			ret = io_setup(128, &ctx);
-			
+
 			if (ret < 0)
 			{
 				derror("io_setep error!");
 			}
 
 			aio = (linux_disk_aio_context *)aio_tsk->aio().get();
-			
-			memset (&aio->cb, 0, sizeof(aio->cb));
+
+			memset(&aio->cb, 0, sizeof(aio->cb));
 
 			aio->this_ = this;
 			aio->cb.aio_fildes = static_cast<int>((ssize_t)aio->file);
-			
-			switch(aio->type)
+
+			switch (aio->type)
 			{
-				case AIO_Read:
-					io_prep_pread(&aio->cb, static_cast<int>((ssize_t)aio->file), aio->buffer, aio->buffer_size, aio->file_offset);
-					break;
-				case AIO_Write:
-					io_prep_pwrite(&aio->cb, static_cast<int>((ssize_t)aio->file), aio->buffer, aio->buffer_size, aio->file_offset);
-					break;
-				default:
-					derror("unknown aio type %u", static_cast<int>(aio->type));
+			case AIO_Read:
+				io_prep_pread(&aio->cb, static_cast<int>((ssize_t)aio->file), aio->buffer, aio->buffer_size, aio->file_offset);
+				break;
+			case AIO_Write:
+				io_prep_pwrite(&aio->cb, static_cast<int>((ssize_t)aio->file), aio->buffer, aio->buffer_size, aio->file_offset);
+				break;
+			default:
+				derror("unknown aio type %u", static_cast<int>(aio->type));
 			}
-			
+
 			if (!async)
 			{
 				aio->evt = new utils::notify_event();
@@ -174,7 +174,7 @@ namespace dsn {
 			}
 
 			io_set_callback(&aio->cb, aio_complete);
-			
+
 			cbs[0] = &aio->cb;
 
 			{
@@ -183,7 +183,7 @@ namespace dsn {
 				_aio_q.push(aio);
 			}
 			ret = io_submit(ctx, 1, cbs);
-			if (ret != 1) 
+			if (ret != 1)
 			{
 				if (ret < 0)
 					derror("io_submit error");
@@ -192,18 +192,18 @@ namespace dsn {
 			}
 
 			if (async)
-            {
-                return ERR_IO_PENDING;
-            }
-            else
-            {
-                aio->evt->wait();
-                delete aio->evt;
-                aio->evt = nullptr;
-                *pbytes = aio->bytes;
-                return aio->err;
-            }
-        }
-    }
+			{
+				return ERR_IO_PENDING;
+			}
+			else
+			{
+				aio->evt->wait();
+				delete aio->evt;
+				aio->evt = nullptr;
+				*pbytes = aio->bytes;
+				return aio->err;
+			}
+		}
+	}
 } // end namespace dsn::tools
 #endif
--- src/tools/common/native_aio_provider.linux.h
+++ src/tools/common/native_aio_provider.linux.h
@@ -1,28 +1,28 @@
 /*
- * The MIT License (MIT)
- *
- * Copyright (c) 2015 Microsoft Corporation
- * 
- * -=- Robust Distributed System Nucleus (rDSN) -=- 
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
+* The MIT License (MIT)
+*
+* Copyright (c) 2015 Microsoft Corporation
+*
+* -=- Robust Distributed System Nucleus (rDSN) -=-
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+*/
 #pragma once
 
 # include <dsn/tool_api.h>
@@ -38,25 +38,25 @@
 # include <inttypes.h>	/* uint64_t */
 
 namespace dsn {
-    namespace tools {
+	namespace tools {
 
-		#define offsetof2(type, member) (size_t)&(((type*)0)->member)
+#define offsetof2(type, member) (size_t)&(((type*)0)->member)
 
-		#define container_of(ptr, type, member) ({                      \
-				        const decltype( ((type *)0)->member ) *__mptr = (ptr);    \
-				        (type *)( (char *)__mptr - offsetof2(type,member) );})
+#define container_of(ptr, type, member) ({                      \
+	const decltype(((type *)0)->member) *__mptr = (ptr);    \
+	(type *)((char *)__mptr - offsetof2(type, member)); })
 
-        class native_linux_aio_provider : public aio_provider
-        {
-        public:
-            native_linux_aio_provider(disk_engine* disk, aio_provider* inner_provider);
-            ~native_linux_aio_provider();
+		class native_linux_aio_provider : public aio_provider
+		{
+		public:
+			native_linux_aio_provider(disk_engine* disk, aio_provider* inner_provider);
+			~native_linux_aio_provider();
 
-            virtual handle_t open(const char* file_name, int flag, int pmode);
-            virtual error_code close(handle_t hFile);
-            virtual void    aio(aio_task_ptr& aio);            
-            virtual disk_aio_ptr prepare_aio_context(aio_task* tsk);
-			
+			virtual handle_t open(const char* file_name, int flag, int pmode);
+			virtual error_code close(handle_t hFile);
+			virtual void    aio(aio_task_ptr& aio);
+			virtual disk_aio_ptr prepare_aio_context(aio_task* tsk);
+
 			struct linux_disk_aio_context : public disk_aio
 			{
 				struct iocb cb;
@@ -67,20 +67,20 @@ namespace dsn {
 				uint32_t bytes;
 			};
 
-        protected:
-            
+		protected:
+
 			error_code aio_internal(aio_task_ptr& aio, bool async, __out_param uint32_t* pbytes = nullptr);
 
 			static void aio_complete(io_context_t ctx, struct iocb *iocb, long res1, long res2);
 
 			void get_event();
 
-        private:
+		private:
 			::dsn::service::zlock _lock;
 			std::queue<io_context_t> _ctx_q;
 			std::queue<linux_disk_aio_context *> _aio_q;
-        };
-    }
+		};
+	}
 }
 # endif
 
