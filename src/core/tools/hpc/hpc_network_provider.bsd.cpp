61f226786d6e8e30e7610c099079d972c524edd9
Haoxiang Lin <linmajia@live.com>
2015-10-29 13:34:07 +0800

    Check whether _socket has been closed before unbind it.

--- src/core/tools/hpc/hpc_network_provider.bsd.cpp
+++ src/core/tools/hpc/hpc_network_provider.bsd.cpp
@@ -511,7 +511,10 @@ namespace dsn
 
         void hpc_rpc_session::on_failure()
         {
-            _looper->unbind_io_handle((dsn_handle_t)(intptr_t)_socket, &_ready_event);
+            if (_socket != -1)
+            {
+                _looper->unbind_io_handle((dsn_handle_t)(intptr_t)_socket, &_ready_event);
+            }
             if (on_disconnected())
                 close();            
         }
