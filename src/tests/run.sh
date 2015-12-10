93af45b9dbd17b9926982ec5afe5a26a85f55548
qinzuoyan <qinzuoyan@gmail.com>
2015-12-10 14:15:43 +0800

    improve travis.yml to print gdb trace info

--- src/tests/run.sh
+++ src/tests/run.sh
@@ -2,4 +2,10 @@
 
 ./clear.sh
 ./dsn.tests config-test.ini
+if [ $? -ne 0 ]; then
+    if ls core.*; then
+        gdb ./dsn.tests core.* -ex "thread apply all bt" -ex "set pagination 0" -batch
+    fi
+    exit -1
+fi
 
