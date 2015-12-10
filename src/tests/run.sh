7cb9a92952f9e6db0a6519d400c74ec2729c1554
qinzuoyan <qinzuoyan@gmail.com>
2015-12-10 14:19:54 +0800

    smallfix

--- src/tests/run.sh
+++ src/tests/run.sh
@@ -3,8 +3,8 @@
 ./clear.sh
 ./dsn.tests config-test.ini
 if [ $? -ne 0 ]; then
-    if ls core.*; then
-        gdb ./dsn.tests core.* -ex "thread apply all bt" -ex "set pagination 0" -batch
+    if ls core*; then
+        gdb ./dsn.tests core* -ex "thread apply all bt" -ex "set pagination 0" -batch
     fi
     exit -1
 fi
