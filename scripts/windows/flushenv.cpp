f7ce0fe967eef530b039d5fe9c0aec797b42ef5d
imzhenyu <zhenyu.guo@microsoft.com>
2015-12-15 10:48:29 +0800

    fix format of flushenv

--- scripts/windows/flushenv.cpp
+++ scripts/windows/flushenv.cpp
@@ -3,7 +3,7 @@
 
 int main(int argc, char* argv[])
 {
-	DWORD_PTR dwReturnValue;
-	SendMessageTimeoutA(HWND_BROADCAST, WM_SETTINGCHANGE, 0, (LPARAM)("Environment"), SMTO_ABORTIFHUNG, 5000, &dwReturnValue);
-	return 0;
+    DWORD_PTR dwReturnValue;
+    SendMessageTimeoutA(HWND_BROADCAST, WM_SETTINGCHANGE, 0, (LPARAM)("Environment"), SMTO_ABORTIFHUNG, 5000, &dwReturnValue);
+    return 0;
 }
