050e9a4d24f093997f7f329185281f45659ce0fa
imzhenyu <zhenyu.guo@microsoft.com>
2016-01-14 14:39:18 +0800

    disable inline when the callback is not enqueued from io-thread, as in this case, a dead lock may happen when the enqueue is inside a lock

--- src/apps/replication/exe/perf-test.cmd
+++ src/apps/replication/exe/perf-test.cmd
@@ -1,6 +1,7 @@
-@ECHO ON
-
+@ECHO OFF
 @MKDIR perf-result
+@RMDIR /Q /S data
+
 :: %replica_count% - how many replica servers we want in this test
 FOR %%R IN (1,2,3) DO (
     :: %tcp_network_provider% - what kind of tcp network providers we use
@@ -8,14 +9,15 @@ FOR %%R IN (1,2,3) DO (
         :: %udp_network_provider% - what kind of udp network providers we use
         FOR %%U IN (dsn::tools::sim_network_provider dsn::tools::asio_udp_provider) DO (
             :: %aio_provider% - what kind of aio provider we use
-            FOR %%A IN (dsn::tools::empty_aio_provider dsn::tools::native_aio_provider) DO (             
-                rmdir /Q /S data
+            FOR %%A IN (dsn::tools::empty_aio_provider dsn::tools::native_aio_provider) DO (
                 CALL dsn.replication.simple_kv perf-config.ini -cargs replica_count=%%R;tcp_network_provider=%%T;udp_network_provider=%%U;aio_provider=%%A
-                SET last_error=%ERRORLEVEL%
-                XCOPY /Y data\client.perf.test\perf-result-*.txt .\perf-result\
-                IF "%last_error%" NEQ "0" exit
+                XCOPY /Y data\client.perf.test\perf-result-* .\perf-result\
+                @RMDIR /Q /S data
             )
         )
     )
 )
+
+:exit
+
 
