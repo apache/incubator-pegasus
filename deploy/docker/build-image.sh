d0ab5155ab1b9f95500a589851edbe848e617783
goksyli <goksyli1990@gmail.com>
2016-01-20 18:59:09 +0800

    small fix

--- deploy/docker/build-image.sh
+++ deploy/docker/build-image.sh
@@ -5,11 +5,12 @@ SOURCE_DIR=$(dirname ${BASH_SOURCE})
 
 function prepare_file()
 {
-    wget https://github.com/mcfatealan/rDSN.Python/tree/master/release/linux/MonitorPack.7z
+#    wget https://github.com/mcfatealan/rDSN.Python/tree/master/release/linux/MonitorPack.7z
+    tar -cvzf ${SOURCE_DIR}/MonitorPack.tar.gz -C /home/guoxi/rdsn-python/src apps/rDSN.monitor dev setup.py
     tar -cvzf ${SOURCE_DIR}/rdsn-release.tar.gz -C $DSN_ROOT include lib bin
 }
 
 
 prepare_file
-docker build -t ${REPO}/rdsn ${SOURCE_DIR}
-docker push ${REPO}/rdsn
+docker build -t ${DOCKER_REPO}/rdsn ${SOURCE_DIR}
+docker push ${DOCKER_REPO}/rdsn
