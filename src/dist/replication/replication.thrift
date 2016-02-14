5e3e27339f66489f0fda87118cbe190067e2733e
imzhenyu <zhenyu.guo@microsoft.com>
2016-02-14 15:26:49 +0800

    add is-stateful in create-app-opts to prepare for support of stateless apps; remove unused proposal structure

--- src/dist/replication/replication.thrift
+++ src/dist/replication/replication.thrift
@@ -224,11 +224,13 @@ struct meta_response_header
 }
 
 // primary | secondary(upgrading) (w/ new config) => meta server
+// also served as proposals from meta server to replica servers
 struct configuration_update_request
 {
     1:partition_configuration  config;
     2:config_type              type = config_type.CT_INVALID;
     3:dsn.rpc_address          node;
+    4:bool                     is_stateful;
 }
 
 // meta server (config mgr) => primary | secondary (downgrade) (w/ new config)
@@ -236,16 +238,6 @@ struct configuration_update_response
 {
     1:dsn.error_code           err;
     2:partition_configuration  config;
-}
-
-// proposal:  meta server(LBM) => primary  (w/ current config)
-struct configuration_proposal_request
-{
-    1:partition_configuration  config;
-    2:config_type              type = config_type.CT_INVALID;
-    3:dsn.rpc_address          node;
-    4:bool                     is_clean_data = false;
-    5:bool                     is_upgrade = false;
 }
 
 // client => meta server
@@ -260,6 +252,7 @@ struct create_app_options
     2:i32              replica_count;
     3:bool             success_if_exist;
     4:string           app_type;
+    5:bool             is_stateful;
 }
 
 struct configuration_create_app_request
