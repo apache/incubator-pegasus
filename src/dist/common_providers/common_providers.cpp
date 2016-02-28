f6a00d4ce502274b8545f1626265c16629b68f23
Zhenyu Guo <imzhenyu@outlook.com>
2016-02-28 14:31:10 +0800

    index on layer2: 56888bc add service API interface change due to layer 2 requirement

---
+++ src/dist/common_providers/common_providers.cpp
@@ -0,0 +1,63 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Microsoft Corporation
+ * 
+ * -=- Robust Distributed System Nucleus (rDSN) -=- 
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+/*
+ * Description:
+ *     partition resolver providers
+ *
+ * Revision history:
+ *     Feb., 2016, @imzhenyu (Zhenyu Guo), first draft
+ *     xxxx-xx-xx, author, fix bug about xxx
+ */
+
+# pragma once
+
+# include "partition_resolver_simple.h"
+# include <dsn/internal/factory_store.h>
+
+namespace dsn
+{
+    namespace dist
+    {
+        static bool register_component_provider(
+            const char* name,
+            ::dsn::dist::partition_resolver::factory f)
+        {
+            return dsn::utils::factory_store< ::dsn::dist::partition_resolver>::register_factory(
+                name,
+                f,
+                PROVIDER_TYPE_MAIN);
+        }
+
+        void register_common_providers()
+        {
+            register_component_provider(
+                "partition_resolver_simple",
+                partition_resolver::create< partition_resolver_simple>
+                );
+        }
+    }
+}
