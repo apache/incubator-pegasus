91b6019b97ee576c31cd5267c3b4278ce945e050
mcfatealan <louchang_new@163.com>
2016-03-15 19:40:19 +0800

    Refactored store, now it's in Vuejs.

--- app_package/static/js/store.js
+++ app_package/static/js/store.js
@@ -1,9 +1,11 @@
-function AutoFill() {
-    if ($('#packname').val() == '')
-    {
-        $('#packname').val(document.forms["fileForm"]["fileToUpload"].value.replace(/^.*[\\\/]/, '').slice(0, -3));
-    }
-}
+document.getElementById("iconToUpload").onchange = function () {
+    document.getElementById("iconpath").value = this.value.replace(/^.*[\\\/]/, '');
+};
+
+document.getElementById("fileToUpload").onchange = function () {
+    $('#packname').val(document.forms["fileForm"]["fileToUpload"].value.replace(/^.*[\\\/]/, '').slice(0, -3));
+    document.getElementById("filepath").value = this.value.replace(/^.*[\\\/]/, '');
+};
 
 function validateForm() {
     var fileToUpload = document.forms["fileForm"]["fileToUpload"].value;
@@ -30,238 +32,168 @@ function validateForm() {
     document.forms["fileForm"]["server_type"].value = $('#server_type_in').val();
     document.forms["fileForm"]["if_stateful"].value = $('#if_stateful').val();
 
-    var param_table = document.getElementById('param_input_list');
-    var row_len = param_table.rows.length;
-    var params_map = {};
-    for (i = 1; i < row_len; i++)
+    var clusterTypeList = [];
+    for(index in vm.$data.clusterList)
     {
-        var cells = param_table.rows.item(i).cells;
-        var key= cells.item(0).lastElementChild.value;
-        var value= cells.item(1).lastElementChild.value;
-        if (key != "" && value != "")
-        {
-            params_map[key] = value;
-        }
+        var cluster = vm.$data.clusterList[index];
+        if(cluster.active)
+            clusterTypeList.push(cluster.name);
+    }
+    document.forms["fileForm"]["cluster_type"].value = JSON.stringify(clusterTypeList);
+
+    var params_map = {};
+    for(index in vm.$data.paramList)
+    {
+        var kv = vm.$data.paramList[index];
+        params_map[kv.key] = kv.value;
     }
     document.forms["fileForm"]["parameters"].value = JSON.stringify(params_map);
 
     return true;
 }
 
-loadPackages();
+var vm = new Vue({
+    el: '#app',
+    data:{
+        metaServer: '',
+        commonPort: '',
 
-function loadPackageDetail(id) {
-    $.post('/api/pack/detail', {id: id
-        }, function(data) {
-            data = JSON.parse(data);
-            $('#detail_app_name').text(data['name']);
-            $('#detail_schema_info').text(data['schema_info']);
-            $('#detail_schema_type').text(data['schema_type']);
-            $('#detail_server_type').text(data['server_type']);
-            params = data['parameters'];
-            params = JSON.parse(params);
-            var tableHTML = "";
-            for (var key in params)
-            {
-                tableHTML += '<tr>'
-                          + '<td>' + key + '</td>'
-                          + '<td>' + params[key] + ' </td>'
-                          + '</tr>';
+        packList: [],
+
+        detail_schema_info: '',
+        detail_schema_type: '',
+        detail_server_type: '',
+        detail_params: {},
+
+        app_name: '',
+        partition_count: 0,
+        replica_count: 0,
+        success_if_exist: true,
+        package_id_to_deploy: '',
+        if_stateful_to_deploy: true,
+
+        newKey: '',
+        newValue: '',
+        paramList: [],
+
+        clusterList: [
+            {name:'kubernetes', active:false},
+            {name:'docker', active:false},
+            {name:'bare_medal_linux', active:false},
+            {name:'bare_medal_windows', active:false},
+            {name:'rdsn_linux', active:false},
+            {name:'rdsn_windows', active:false},
+        ],
+    },
+    components: {
+    },
+    methods: {
+        gotoFile: function(packname)
+        {
+            window.location.href = 'fileview.html?working_dir=pack/' + packname + '&root_dir=local';
+        },
+        showDetail: function(packname)
+        {
+            var self = this;
+            $.post('/api/pack/detail', {id: packname
+                }, function(data) {
+                    data = JSON.parse(data);
+                    self.detail_schema_info = data.schema_info;
+                    self.detail_schema_type = data.schema_type;
+                    self.detail_server_type = data.server_type;
+                    self.detail_params = JSON.parse(data.parameters);
+                }
+            );
+            $('#detail_modal').modal('show');
+        },
+        gotoInstance: function(packname)
+        {
+            window.location.href = 'service_meta.html?filterKey=' + packname;
+        },
+        predeploy: function(packname, if_stateful)
+        {
+            this.package_id_to_deploy = packname;
+            this.if_stateful_to_deploy = if_stateful;
+            $('#deploypack').modal('show');
+        },
+        deploy: function()
+        {
+            var self = this;
+            var command = "meta.create_app ";
+            var jsObj = JSON.stringify({
+                req: {
+                    app_name: self.app_name,
+                    options: {
+                        partition_count: parseInt(self.partition_count),
+                        replica_count: parseInt(self.replica_count),
+                        success_if_exist: (self.success_if_exist=='true')?true:false,
+                        app_type: self.package_id_to_deploy,
+                        is_stateful: (self.if_stateful_to_deploy=='true')?true:false,
+                        package_id: self.package_id_to_deploy
+                    }
+                }
+
+            });
+            command += jsObj;
+            $.post("http://" + self.metaServer + ":" + self.commonPort + "/api/cli", { 
+                command: command
+                }, function(data){ 
+                    console.log(data);
+                    window.location.href = 'service_meta.html';
+                }
+            );
+        },
+        remove: function(packname)
+        {
+            $.post("/api/pack/del", { name: packname
+                }, function(data){ 
+                   location.reload(false); 
+                }
+            );
+        },
+
+        addKV: function()
+        {
+            var key = this.newKey && this.newKey.trim();
+            var value = this.newValue && this.newValue.trim();
+            if (!key) {
+                return;
             }
-            $("#detail_parameters tbody tr").remove();
-            $("#detail_parameters").append(tableHTML);
-        }
-    )
-}
+            this.paramList.push({key: key, value:value});
+            this.newKey = '';        
+            this.newValue = '';        
+        },
+        removeKV: function(kv)
+        {
+            this.paramList.$remove(kv);   
+        },
 
-function loadPackages() {
-    $.post("/api/pack/load", {
-        }, function(data){
-            var tableHTML = ""; 
-
-            var message = JSON.parse(data);
-	        for (var i = 0; i < message.length; i++) {
-                tableHTML = tableHTML + '<tr id=' + message[i].name + '>' 
-                + '<td><img src="local/pack/' + message[i].uuid + '.jpg" width="50" height="50" ></td>' 
-                + '<td>' + message[i].name + '</td>' 
-                + '<td>' + message[i].author + '</td>'
-                + '<td>' + message[i].description + '</td>'
-                + '<td><span class="glyphicon glyphicon-file" aria-hidden="true" onclick="window.location.href = \'fileview.html?working_dir=pack/' + message[i].uuid + '&root_dir=local\';"></span></td>'
-                + '<td><span class="glyphicon glyphicon-list-alt" aria-hidden="true"' + 'onclick="loadPackageDetail(\'' + message[i].uuid + '\');' + '$(\'#detail_modal\').modal(\'show\');"></span></td>'
-                + '<td><a href="local/pack/' + message[i].uuid +'/' + message[i].name + '.Tron.Composition.dll" download><span class="glyphicon glyphicon-save-file" aria-hidden="true"></span></a></td>'
-                + '<td><span class="glyphicon glyphicon-flash" aria-hidden="true" onclick="window.location.href = \'service_meta.html?filterKey=' + message[i].uuid +'\';"></span></td>'
-                + '<td><span class="glyphicon glyphicon-send" aria-hidden="true" onclick="SetPackageToDeploy(\'' + message[i].uuid + '\',\''+message[i].if_stateful +'\');$(\'#deploypack\').modal(\'show\');"></span></td>'
-                + '<td><span class="glyphicon glyphicon-remove" aria-hidden="true" onclick="RemovePackage(\'' + message[i].name + '\');"></span></td>'
-                + '</tr>';
-                
+        changeClusterState: function(index)
+        {
+            this.clusterList[index].active = !(this.clusterList[index].active);
+        },
+     
+    },
+    ready: function ()
+    {
+        var self = this;
+        $.post("/api/metaserverquery", { 
+            }, function(data){ 
+                self.metaServer = data.split(":")[0];
+                self.commonPort = window.location.href.split("/")[2].split(":")[1];
             }
-            $("#packList tbody > tr").empty();
-            $("#packList tbody").append(tableHTML);
-        }
-    )
-    .fail(function() {
-        alert( "Error: lost connection to the server" );
-        return;
-    });
-}
+        );
 
-function RemovePackage(name) {
-    $.post("/api/pack/del", { name: name
-        }, function(data){ 
-           location.reload(false); 
-        }
-    )
-    .fail(function() {
-        alert( "Error: lost connection to the server" );
-        return;
-    });
-}
-
-function DeployPackage() {
-    var command = "meta.create_app ";
-    var jsObj = JSON.stringify({
-        req: {
-            app_name: document.getElementById("app_name").value,
-            options: {
-                partition_count: parseInt(document.getElementById("partition_count").value),
-                replica_count: parseInt(document.getElementById("replica_count").value),
-                success_if_exist: ($('#success_if_exist').val()=='true')?true:false,
-                app_type: $('#package_id_to_deploy').val(),
-                is_stateful: ($('#if_stateful_to_deploy').val()=='true')?true:false,
-                package_id: $('#package_id_to_deploy').val()
-            }
-        }
-
-    });
-    command += jsObj;
-    $.post("http://" + metaServer + ":" + commonPort + "/api/cli", { 
-        command: command
-        }, function(data){ 
-            console.log(data);
-        }
-    )
-    .fail(function() {
-        alert( "Error: lost connection to the server" );
-        return;
-    });
-}
-
-function SetPackageToDeploy(name,if_stateful) {
-    $('#package_id_to_deploy').val(name);
-    $('#if_stateful_to_deploy').val(if_stateful);
-}
-
-document.getElementById("iconToUpload").onchange = function () {
-    document.getElementById("iconpath").value = this.value.replace(/^.*[\\\/]/, '');
-};
-
-document.getElementById("fileToUpload").onchange = function () {
-    AutoFill();
-    document.getElementById("filepath").value = this.value.replace(/^.*[\\\/]/, '');
-};
-
-document.getElementById("add_param_btn").onclick = function () {
-    var tableHTML = '<tr>'
-                  + '<td> <input> </td>'
-                  + '<td> <input> </td>'
-                  + '<td> <span class="glyphicon glyphicon-remove rm_param_icon" aria-hidden="true"></span></td>'
-                  + '</tr>';
-    $("#param_input_list").append(tableHTML);
-};
-
-
-//check-box-list
-$(function () {
-    $('#param_input_list')
-        .on('click', '.rm_param_icon', function () {
-            $(this).closest("tr").remove();
-
-    });
-
-    $('.list-group.checked-list-box .list-group-item').each(function () {
-        // Settings
-        var $widget = $(this),
-        $checkbox = $('<input type="checkbox" class="hidden" />'),
-        color = ($widget.data('color') ? $widget.data('color') : "primary"),
-        style = ($widget.data('style') == "button" ? "btn-" : "list-group-item-"),
-        settings = {
-            on: {
-                icon: 'glyphicon glyphicon-check'
-            },
-        off: {
-            icon: 'glyphicon glyphicon-unchecked'
-        }
-        };
-
-    $widget.css('cursor', 'pointer')
-        $widget.append($checkbox);
-
-    // Event Handlers
-    $widget.on('click', function () {
-        $checkbox.prop('checked', !$checkbox.is(':checked'));
-        $checkbox.triggerHandler('change');
-        updateDisplay();
-    });
-
-    $checkbox.on('change', function () {
-        updateDisplay();
-    });
-
-
-    // Actions
-    function updateDisplay() {
-        var isChecked = $checkbox.is(':checked');
-
-        // Set the button's state
-        $widget.data('state', (isChecked) ? "on" : "off");
-
-        // Set the button's icon
-        $widget.find('.state-icon')
-            .removeClass()
-            .addClass('state-icon ' + settings[$widget.data('state')].icon);
-
-        // Update the button's color
-        if (isChecked) {
-            $widget.addClass(style + color + ' active');
-        } else {
-            $widget.removeClass(style + color + ' active');
-        }
+        $.post("/api/pack/load", {
+            }, function(data){
+                self.packList = JSON.parse(data);
+                for(index in self.packList)
+                {
+                    self.packList[index].imgsrc = "local/pack/" + self.packList[index].name + ".jpg";
+                }
+            }   
+        );
     }
-
-    // Initialization
-    function init() {
-
-        if ($widget.data('checked') == true) {
-            $checkbox.prop('checked', !$checkbox.is(':checked'));
-        }
-
-        updateDisplay();
-
-        // Inject the icon if applicable
-        if ($widget.find('.state-icon').length == 0) {
-            $widget.prepend('<span class="state-icon ' + settings[$widget.data('state')].icon + '"></span>');
-        }
-    }
-    init();
-    });
 });
 
 
-function getClusterType(){
-    var clusterTypeList = [];
-    $("#check-list-box li.active").each(function(idx, li) {
-            clusterTypeList.push($(li).attr('index'));
-        });
-    document.forms["fileForm"]["cluster_type"].value = JSON.stringify(clusterTypeList);
-    
-}
-
-
-var metaServer,commonPort;
-$.post("/api/metaserverquery", { 
-    }, function(data){ 
-        console.log(data);
-        metaServer = data.split(":")[0];
-        commonPort = window.location.href.split("/")[2].split(":")[1];
-    }
-);
