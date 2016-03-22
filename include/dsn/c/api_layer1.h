d5449708b3933f44311744a566be6603c359b6b0
WeijieSun <luckyweijie@gmail.com>
2016-03-22 18:10:25 +0800

    add comment for dsn_message_context_t

--- include/dsn/c/api_layer1.h
+++ include/dsn/c/api_layer1.h
@@ -536,7 +536,26 @@ typedef union dsn_msg_context_t
         uint64_t is_request : 1;           ///< whether the RPC message is a request or response
         uint64_t is_forwarded : 1;         ///< whether the msg is forwarded or not
         uint64_t is_replication_needed: 1; ///< whether state replication is needed for this request
-        uint64_t is_response_in_piece: 1;  ///< set when a response is marshalled in different place
+
+        /// When we send a reply to client, the reply result may have several fields.
+        /// For some serialization protocols like thrift, this is done by replying a
+        /// user defined struct:
+        /// struct {
+        ///     1: string error_code_field;
+        ///     2: blob some_data;
+        /// }.
+        /// And each field is assigned with an "id".
+        ///
+        /// But in rDSN, we can simply set fill these fields one by one by calling their "marshall"s:
+        /// marshall(msg, error_code_field);
+        /// marshall(msg, some_data);
+        ///
+        /// This is flexible but may not compatible with other serialization protocols, coz we don't know
+        /// the "id" for these fields. In order to make our system more friendly to other protocols, we may
+        /// need to set the flag before marshalling the replies. Thus the "marshall" implementation can calculate
+        /// the id for every fields and reconstruct a complete message from these fields.
+        uint64_t is_response_in_piece: 1;
+
         uint64_t is_forward_disabled: 1;   ///< whether forward a message to real leader
         uint64_t unused : 6;               ///< not used yet
         uint64_t parameter_type : 3;       ///< type of the parameter next, see  \ref dsn_msg_parameter_type_t        
