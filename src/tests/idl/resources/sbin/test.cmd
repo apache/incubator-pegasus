ec7e9fe371a7a84f48cdf203e1cefb462a7e9037
ykwd <oneday117@qq.com>
2016-03-29 19:56:33 +0800

    1. allow msbuild to run parallelly; 2. download thrift.exe on appveyor 3. modify .cmd file for changed interface of idl tests 4. specify DSN_CMAKE_BUILD_TYPE explicitly on appveyor 5. add php location to PATH on appveyor

--- src/tests/idl/resources/sbin/test.cmd
+++ src/tests/idl/resources/sbin/test.cmd
@@ -4,4 +4,4 @@ SET CMAKE_PATH=%~f2
 SET DSN_ROOT=%MOCK_ROOT%\rdsn
 SET PATH=%CMAKE_PATH%;%DSN_ROOT%\lib;%DSN_ROOT%\bin;%PATH%
 CALL  %bin_dir%\echoc.exe 2  %build_type%\dsn.idl.tests.exe %MOCK_ROOT%
-CALL  %build_type%\dsn.idl.tests.exe %MOCK_ROOT%
+CALL  %build_type%\dsn.idl.tests.exe %DSN_ROOT% %MOCK_ROOT%
