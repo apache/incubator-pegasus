2c93494b387cb7c9da071286707f861eb36d2d60
mcfatealan <louchang_new@163.com>
2016-06-02 20:03:29 +0800

    webstudio: fix saving view bug and improve display

--- src/tools/webstudio/app_package/static/js/setting.js
+++ src/tools/webstudio/app_package/static/js/setting.js
@@ -2,9 +2,9 @@ var vm = new Vue({
     el: '#app',
     data:{
         setting_list: [
-            {'name':'dsn_rpc_address','label':'DSN RPC Address','description':'','value':''},
-            {'name':'meta_server_address','label':'Meta Server Address','description':'','value':''}
-        ] 
+            {'name':'dsn_rpc_address','label':'DSN RPC Address','description':'the machine address you use when do single-node profiling','value':''},
+            {'name':'meta_server_address','label':'Meta Server Address','description':'the meta server address for app store','value':''}
+        ],
     },
     watch: {
         'setting_list': {
