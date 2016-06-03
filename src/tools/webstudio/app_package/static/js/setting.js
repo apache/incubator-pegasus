7f5ccaaf11311aac37b7b21043a2a355ce494e01
mcfatealan <louchang_new@163.com>
2016-06-03 13:09:10 +0800

    webstudio: improve hint information for setting and multicmd

--- src/tools/webstudio/app_package/static/js/setting.js
+++ src/tools/webstudio/app_package/static/js/setting.js
@@ -5,14 +5,18 @@ var vm = new Vue({
             {'name':'dsn_rpc_address','label':'DSN RPC Address','description':'the machine address you use when do single-node profiling','value':''},
             {'name':'meta_server_address','label':'Meta Server Address','description':'the meta server address for app store','value':''}
         ],
+        state: ''
     },
     watch: {
         'setting_list': {
             handler: function (lst, oldLst) {
+                var self = this;
+                self.state = 'saving';
                 for(item in lst)
                 {
                     localStorage.setItem(lst[item].name, lst[item].value);
                 }
+                setTimeout(function(){ self.state = ''; }, 700);
             },
             deep: true
         },
