e0920b1fb094fc1a5ce77cc14ab6f5a0a3ce9780
WeijieSun <luckyweijie@gmail.com>
2016-07-11 16:35:44 +0800

    fix invalid memory access in zookeeper_session::visit

--- src/tests/dsn/distributed_lock_zookeeper.cpp
+++ src/tests/dsn/distributed_lock_zookeeper.cpp
@@ -92,27 +92,27 @@ private:
 
 TEST(distributed_lock_service_zookeeper, simple_lock_unlock)
 {
-    pos = 0; 
+    pos = 0;
     result = 0;
     ss_start = false;
     ss_finish = false;
     q.clear();
-    
+
     srand( time(0) );
     q.reserve(100000);
     for (int i=0; i!=100000; ++i) {
         int64_t rand1 = rand()%10000;
-        int64_t rand2 = rand()%10000; 
+        int64_t rand2 = rand()%10000;
         q.push_back( rand1*rand2 );
     }
-    
+
     int64_t expect_reuslt = 0;
     for (int64_t i: q) expect_reuslt += i;
-    
+
     ss_start = true;
-    while ( !ss_finish ) 
+    while ( !ss_finish )
         std::this_thread::sleep_for( std::chrono::seconds(1) );
-    
+
     ddebug("actual result: %lld, expect_result:%lld", result, expect_reuslt);
     EXPECT_TRUE(result==expect_reuslt);
 }
@@ -154,7 +154,7 @@ TEST(distributed_lock_service_zookeeper, abnormal_api_call)
     ASSERT_TRUE(cb_pair.first!=nullptr && cb_pair.second!=nullptr);
     cb_pair.first->wait();
     
-    /* recursive lock */
+    // recursive lock
     std::pair<task_ptr, task_ptr> cb_pair2 = dlock_svc->lock(lock_id, my_id, 
         DLOCK_CALLBACK, 
         [](error_code ec, const std::string&, int)
@@ -170,7 +170,7 @@ TEST(distributed_lock_service_zookeeper, abnormal_api_call)
     cb_pair2.second->cancel(false);
     
     cb_pair.first->wait();
-    /* try to cancel an locked lock*/
+    // try to cancel an locked lock
     task_ptr tsk = dlock_svc->cancel_pending_lock(lock_id, my_id, 
         DLOCK_CALLBACK, [](error_code ec, const std::string&, int){
             ASSERT_TRUE(ec == ERR_INVALID_PARAMETERS);
@@ -178,7 +178,7 @@ TEST(distributed_lock_service_zookeeper, abnormal_api_call)
     );
     tsk->wait();
     
-    /* try to cancel an non-exist lock*/
+    // try to cancel an non-exist lock
     tsk = dlock_svc->cancel_pending_lock(lock_id, "non-exist-myself", 
         DLOCK_CALLBACK, [](error_code ec, const std::string&, int) { ASSERT_TRUE(ec==ERR_OBJECT_NOT_FOUND); }
     );
