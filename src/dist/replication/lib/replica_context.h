25bc90ff90a6952240d26a06f8ad27b4636cee9f
qinzuoyan <qinzuoyan@xiaomi.com>
2016-07-19 10:33:21 +0800

    fix primary_context init bug

--- src/dist/replication/lib/replica_context.h
+++ src/dist/replication/lib/replica_context.h
@@ -52,8 +52,10 @@ class primary_context
 {
 public:
     primary_context(gpid gpid, int max_concurrent_2pc_count = 1, bool batch_write_disabled = false)
-        : next_learning_version(0), write_queue(gpid, max_concurrent_2pc_count, batch_write_disabled)
-        , last_prepare_ts_ms(0)
+        : next_learning_version(0),
+          write_queue(gpid, max_concurrent_2pc_count, batch_write_disabled),
+          last_prepare_decree_on_new_primary(0),
+          last_prepare_ts_ms(0)
     {}
 
     void cleanup(bool clean_pending_mutations = true);
