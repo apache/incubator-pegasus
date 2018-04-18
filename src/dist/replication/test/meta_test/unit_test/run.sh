af9aaee6ea91f1ec553248d5689270913086b5ae
WeijieSun <luckyweijie@gmail.com>
2018-04-18 16:20:53 +0800

    meta_server_test: print more correct message when running unit test failed (#17)

--- src/dist/replication/test/meta_test/unit_test/run.sh
+++ src/dist/replication/test/meta_test/unit_test/run.sh
@@ -8,9 +8,10 @@ fi
 output_xml="${REPORT_DIR}/dsn.meta.test.1.xml"
 GTEST_OUTPUT="xml:${output_xml}" GTEST_FILTER="-meta.data_definition:meta.apply_balancer" ./dsn.meta.test
 if [ $? -ne 0 ]; then
+    tail -n 100 data/log/log.1.txt
     if [ -f core ]; then
-        echo "---- gdb ./dsn.rep_tests.simple_kv core ----"
-        gdb ./dsn.rep_tests.simple_kv core -ex "thread apply all bt" -ex "set pagination 0" -batch
+        echo "---- gdb ./dsn.meta.test core ----"
+        gdb ./dsn.meta.test core -ex "thread apply all bt" -ex "set pagination 0" -batch
     fi
     exit -1
 fi
@@ -19,9 +20,10 @@ fi
 output_xml="${REPORT_DIR}/dsn.meta.test.2.xml"
 GTEST_OUTPUT="xml:${output_xml}" GTEST_FILTER="meta.data_definition" ./dsn.meta.test config-ddl-test.ini
 if [ $? -ne 0 ]; then
+    tail -n 100 data/log/log.1.txt
     if [ -f core ]; then
-        echo "---- gdb ./dsn.rep_tests.simple_kv core ----"
-        gdb ./dsn.rep_tests.simple_kv core -ex "thread apply all bt" -ex "set pagination 0" -batch
+        echo "---- gdb ./dsn.meta.test core ----"
+        gdb ./dsn.meta.test core -ex "thread apply all bt" -ex "set pagination 0" -batch
     fi
     exit -1
 fi
@@ -30,9 +32,10 @@ fi
 output_xml="${REPORT_DIR}/dsn.meta.test.3.xml"
 GTEST_OUTPUT="xml:${output_xml}" GTEST_FILTER="meta.apply_balancer" ./dsn.meta.test
 if [ $? -ne 0 ]; then
+    tail -n 100 data/log/log.1.txt
     if [ -f core ]; then
-        echo "---- gdb ./dsn.rep_tests.simple_kv core ----"
-        gdb ./dsn.rep_tests.simple_kv core -ex "thread apply all bt" -ex "set pagination 0" -batch
+        echo "---- gdb ./dsn.meta.test core ----"
+        gdb ./dsn.meta.test core -ex "thread apply all bt" -ex "set pagination 0" -batch
     fi
     exit -1
 fi
