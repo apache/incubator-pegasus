d4ab667e7a7bb945b88fc31656111ce83f48a133
heyuchen <heyuchen@xiaomi.com>
2018-04-23 19:50:52 +0800

    update log libary from bunyan to log4js
    Summary: change log libary to log4js
    
    Reviewers: sunweijie
    
    Reviewed By: sunweijie
    
    Subscribers: #pegasus
    
    Differential Revision: https://phabricator.d.xiaomi.net/D92230

--- log.js
+++ log.js
@@ -1,44 +1,11 @@
 /**
- * Created by heyuchen on 18-2-8
+ * Created by hyc on 18-4-23
  */
-
-const bunyan = require('bunyan');
-
-const log = bunyan.createLogger({
-    name: 'pegasus-node-client',
-    streams: [
-        {
-            level: 'info',
-            stream: process.stdout,
-        },
-        {
-            // type: 'rotating-file',
-            // level: 'error',
-            // period : '1d',
-            // count : 3,
-            path: 'error.log',
-            level : 'error',
-        }
-    ],
-    src : true,
-    serializers: {err: bunyan.stdSerializers.err},
+const log4js = require('log4js');
+log4js.configure({
+    appenders: { err: { type: 'stderr' } },
+    categories: { default: { appenders: ['err'], level: 'WARN' } }
 });
-
-// const log = bunyan.createLogger({
-//     "name": "pegasus-node-client",
-//     "src": "true",
-//     "streams": [{
-//         "level": "info",
-//         // "type": "rotating-file",
-//         "path": "error.log",
-//         // "period": 'daily',
-//         // "count" : 5,
-//         // "size": 10,
-//     },
-//     {
-//         level: 'info',
-//         stream: process.stdout,
-//     }]
-// });
+let log = log4js.getLogger();
 
 module.exports = log;
