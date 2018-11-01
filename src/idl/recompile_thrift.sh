e6d8e1e0694d8912b809848c30d3d77610636a85
huangwei5 <huangwei5@xiaomi.com>
2018-11-01 15:56:48 +0800

    fix

--- src/idl/recompile_thrift.sh
+++ src/idl/recompile_thrift.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # recommand thrift-0.9.3
 
+cd `dirname $0`
+DSN_ROOT=../../rdsn
+
 if [ ! -d "$DSN_ROOT" ]; then
   echo "ERROR: DSN_ROOT not set"
   exit 1
@@ -10,11 +13,8 @@ TMP_DIR=./tmp
 rm -rf $TMP_DIR
 
 mkdir -p $TMP_DIR
-sh $DSN_ROOT/bin/dsn.cg.sh rrdb.thrift cpp $TMP_DIR
+$DSN_ROOT/bin/Linux/thrift --gen cpp:moveable_types -out $TMP_DIR rrdb.thrift
 cp -v $TMP_DIR/rrdb.types.h ../include/rrdb/
-#cp -v $TMP_DIR/rrdb.code.definition.h ../include/rrdb/
-#cp -v $TMP_DIR/rrdb.client.h ../include/rrdb/
-#sed 's/# include "rrdb.code.definition.h"/# include <rrdb\/rrdb.code.definition.h>/' $TMP_DIR/rrdb.server.h > ../include/rrdb/rrdb.server.h
 sed 's/#include "dsn_types.h"/#include <dsn\/service_api_cpp.h>/' $TMP_DIR/rrdb_types.h > ../include/rrdb/rrdb_types.h
 sed 's/#include "rrdb_types.h"/#include <rrdb\/rrdb_types.h>/' $TMP_DIR/rrdb_types.cpp > ../base/rrdb_types.cpp
 
