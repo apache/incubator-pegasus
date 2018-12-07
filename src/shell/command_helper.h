31feb3c091dcb4a9d1d04681f82cc709fce178d3
qinzuoyan <qinzuoyan@xiaomi.com>
2018-12-07 16:17:12 +0800

    update falcon_screen.json

--- src/shell/command_helper.h
+++ src/shell/command_helper.h
@@ -487,12 +487,12 @@ get_app_stat(shell_context *sc, const std::string &app_name, std::vector<row_dat
     }
 
     ::dsn::command command;
-    command.cmd = "perf-counters-by-substr";
+    command.cmd = "perf-counters";
     char tmp[256];
     if (app_name.empty()) {
-        sprintf(tmp, "@");
+        sprintf(tmp, ".*@.*");
     } else {
-        sprintf(tmp, "@%d.", app_info->app_id);
+        sprintf(tmp, ".*@%d\\..*", app_info->app_id);
     }
     command.arguments.push_back(tmp);
     std::vector<std::pair<bool, std::string>> results;
