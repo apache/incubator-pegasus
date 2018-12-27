ab049392bda46cb5f52dcf9b853cfa3239ae9a92
neverchanje <wutao1@xiaomi.com>
2018-12-27 14:40:31 +0800

    add usage

--- scripts/pegasus_check_ports.py
+++ scripts/pegasus_check_ports.py
@@ -3,6 +3,16 @@
 # Copyright (c) 2018, Xiaomi, Inc.  All rights reserved.
 # This source code is licensed under the Apache License Version 2.0, which
 # can be found in the LICENSE file in the root directory of this source tree.
+"""
+Basic usage:
+
+> vim ~/.bashrc
+export PYTHONPATH=$PYTHONPATH:$HOME/.local/lib/python2.7/site-packages/ 
+export PEGASUS_CONFIG_PATH=$HOME/work/conf_pegasus
+export PEGASUS_SHELL_PATH=$HOME/work/pegasus
+> pip install --user click
+> ./pegasus_check_posts.py --env c3srv
+"""
 
 import os
 import click
