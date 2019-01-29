ef1550aad1df4efa5888daf66a4c821575ff4b28
QinZuoyan <qinzuoyan@gmail.com>
2019-01-29 10:14:28 +0800

    update rdsn: add cluster balance indicator (#269)

--- scripts/pegasus_stat_available.sh
+++ scripts/pegasus_stat_available.sh
@@ -6,7 +6,7 @@ if [ $# -lt 2 ]; then
   echo "USGAE: $0 <cluster> <filter1> [<filter2> ...]"
   echo "   eg: $0 onebox 2017-07 2017-08 2017-09"
   echo
-  echo "Result: <cluster> <serve_days> <available>"
+  echo "Result: <cluster> <serve_minutes> <available> <app_count> <data_size>"
   exit 1
 fi
 
@@ -15,27 +15,29 @@ shift
 
 pwd="$( cd "$( dirname "$0"  )" && pwd )"
 shell_dir="$( cd $pwd/.. && pwd )"
-minos_config_dir=$(dirname $MINOS_CONFIG_FILE)/xiaomi-config/conf/pegasus
-minos_config=$minos_config_dir/pegasus-${cluster}.cfg
 cd $shell_dir
 
-if [ ! -f $minos_config ]; then
-  echo "ERROR: minos config \"$minos_config\" not found"
+detect_table="test"
+app_stat_result="/tmp/$UID.$PID.pegasus.stat_available.app_stat_result"
+tmp_file="/tmp/$UID.$PID.pegasus.stat_available.app_stat"
+echo "app_stat -o $app_stat_result" | ./run.sh shell -n $cluster &>$tmp_file
+app_stat_ok=`grep "succeed" $tmp_file | wc -l`
+if [ $app_stat_ok -ne 1 ]; then
+  echo "ERROR: app stat failed, refer error to $tmp_file"
   exit 1
 fi
 
-detect_table=`grep '^ *available_detect_app = ' $minos_config | awk '{print $3}'`
-if [ "$detect_table" == "" ]; then
-  echo "ERROR: get detect table from $minos_config failed"
-  exit 1
-fi
+app_count=`cat $app_stat_result | wc -l`
+app_count=$((app_count-2))
+data_size=`cat $app_stat_result | tail -n 1 | awk '{print $(NF-3)}' | sed 's/\.00$//'`
+data_size=$(((data_size+1024)/1024))
 
-all_result="pegasus.stat_available.all_result"
+all_result="/tmp/$UID.$PID.pegasus.stat_available.all_result"
 rm -f $all_result
 for filter in $*; do
-  result_file="pegasus.stat_available.scan_result.$filter"
+  result_file="/tmp/$UID.$PID.pegasus.stat_available.scan_result.$filter"
   tmp_file="/tmp/$UID.$PID.pegasus.stat_available.scan"
-  echo -e "use $detect_table\nhash_scan detect_available_day '' '' -s prefix -y \"$filter\" -o $result_file" | ./run.sh shell -n $cluster &>$tmp_file
+  echo -e "use $detect_table\nhash_scan detect_available_minute '' '' -s prefix -y \"$filter\" -o $result_file" | ./run.sh shell -n $cluster &>$tmp_file
   scan_ok=`grep 'key-value pairs got' $tmp_file | wc -l`
   if [ $scan_ok -ne 1 ]; then
     echo "ERROR: scan detect table failed, refer error to $tmp_file"
@@ -45,13 +47,14 @@ for filter in $*; do
   cat $result_file >>$all_result
 done
 
-days=`cat $all_result | wc -l`
-if [ $days -eq 0 ]; then
+minutes=`cat $all_result | wc -l`
+if [ $minutes -eq 0 ]; then
   available="0.000000"
 else
   available=`cat $all_result | grep -o '[0-9]*,[0-9]*,[0-9]*' | awk -F, '{a+=$1;b+=$2}END{printf("%f\n",(double)b/a);}'`
 fi
-echo "$cluster $days $available"
+echo "$cluster $minutes $available $app_count $data_size"
 
-rm -f pegasus.stat_available.scan_result.* $all_result
+cat $all_result >>/tmp/pegasus.stat_available.all_result
+
 rm -f /tmp/$UID.$PID.pegasus.* &>/dev/null
