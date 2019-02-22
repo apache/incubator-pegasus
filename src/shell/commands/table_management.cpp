ec2dcf2c533667dba39e67f869f2da9f72165a05
qinzuoyan <qinzuoyan@xiaomi.com>
2019-02-22 10:50:49 +0800

    smallfix

--- src/shell/commands/table_management.cpp
+++ src/shell/commands/table_management.cpp
@@ -428,6 +428,7 @@ bool app_stat(command_executor *e, shell_context *sc, arguments args)
 
     rows.resize(rows.size() + 1);
     row_data &sum = rows.back();
+    sum.row_name = "(sum)";
     for (int i = 0; i < rows.size() - 1; ++i) {
         row_data &row = rows[i];
         sum.partition_count += row.partition_count;
