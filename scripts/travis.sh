0d72a823a72175f0afc0f52c2dc3e0680aa99b1a
neverchanje <wutao1@xiaomi.com>
2019-07-22 18:10:26 +0800

    fix travis.sh

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -1,6 +1,13 @@
+#!/bin/bash
+
+# to project root directory
+cd "$(dirname "${BASH_SOURCE[0]}")" || exit 1
+cd "$(dirname "$(pwd)")" || exit 1
+
 # ensure source files are well formatted
 ./scripts/format_files.sh
 
+# ignore updates of submodules
 modified=$(git status -s --ignore-submodules)
 if [ "$modified" ]; then
     echo "$modified"
