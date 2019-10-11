3f23ae1285cf58b9b7a6aeb1af9b92dc57958202
zhaoliwei <zhaoliwei@xiaomi.com>
2019-10-11 19:11:15 +0800

    slow query threshold

--- src/server/pegasus_server_impl.cpp
+++ src/server/pegasus_server_impl.cpp
@@ -69,6 +69,7 @@ pegasus_server_impl::pegasus_server_impl(dsn::replication::replica *r)
         "rocksdb_slow_query_threshold_ns",
         100000000,
         "get/multi-get operation duration exceed this threshold will be logged");
+    dassert(_slow_query_threshold_ns > 0, "slow query threshold must be greater than 0");
     _abnormal_get_size_threshold = dsn_config_get_value_uint64(
         "pegasus.server",
         "rocksdb_abnormal_get_size_threshold",
