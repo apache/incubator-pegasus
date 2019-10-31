fa8cf67cc40bcd02f98186cfbe965d0df855a762
neverchanje <wutao1@xiaomi.com>
2019-10-31 18:58:45 +0800

    add travis

--- scripts/format-all.sh
+++ scripts/format-all.sh
@@ -6,17 +6,9 @@ cd "${PROJECT_DIR}" || exit 1
 
 SRC_FILES=(src/main/java/com/xiaomi/infra/common/*.java
            src/main/java/com/xiaomi/infra/config/*.java
-           src/main/java/com/xiaomi/infra/sample/local/*.java
-           src/main/java/com/xiaomi/infra/sample/spark/*.java
            src/main/java/com/xiaomi/infra/service/db/*.java
            src/main/java/com/xiaomi/infra/service/*.java
            src/main/java/com/xiaomi/infra/*.java
-
-           src/test/java/com/xiaomi/infra/pegasus/client/*.java
-           src/test/java/com/xiaomi/infra/pegasus/metrics/*.java
-           src/test/java/com/xiaomi/infra/pegasus/rpc/async/*.java
-           src/test/java/com/xiaomi/infra/pegasus/tools/*.java
-           src/test/java/com/xiaomi/infra/pegasus/base/*.java
            )
 
 if [ ! -f "${PROJECT_DIR}"/google-java-format-1.7-all-deps.jar ]; then
