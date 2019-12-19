2a59fd636f84ffa9113423aa01264ad0289d2c73
JiaShuo <js982986555@live.com>
2019-12-19 13:39:14 +0800

    add sanitizer

--- run.sh
+++ run.sh
@@ -78,6 +78,7 @@ function usage_build()
 function run_build()
 {
     # Note(jiashuo1): No "memory" check mode, because MemorySanitizer is only available in Clang for Linux x86_64 targets
+    # # https://www.jetbrains.com/help/clion/google-sanitizers.html
     SANITIZERS=("address" "leak" "thread" "undefined")
 
     C_COMPILER="gcc"
