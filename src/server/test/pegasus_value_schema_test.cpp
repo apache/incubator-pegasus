b936822e32954fe9e15b14aece9d7e5897ccecc7
smityz <smityz@qq.com>
2020-02-06 19:49:43 +0800

    1

--- src/server/test/pegasus_value_schema_test.cpp
+++ src/server/test/pegasus_value_schema_test.cpp
@@ -1,7 +1,6 @@
 // Copyright (c) 2017, Xiaomi, Inc.  All rights reserved.
 // This source code is licensed under the Apache License Version 2.0, which
 // can be found in the LICENSE file in the root directory of this source tree.
-#pragma once
 
 #include "base/pegasus_value_schema.h"
 
