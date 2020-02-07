56c9de7d22920e8fef777b3643384568e35537ec
smityz <smityz@qq.com>
2020-02-07 11:32:29 +0800

    change app_name_out

--- src/server/data_store.h
+++ src/server/data_store.h
@@ -1,3 +1,7 @@
+// Copyright (c) 2017, Xiaomi, Inc.  All rights reserved.
+// This source code is licensed under the Apache License Version 2.0, which
+// can be found in the LICENSE file in the root directory of this source tree.
+
 #pragma once
 
 #include "shell/commands.h"
@@ -7,8 +11,8 @@ namespace server {
 
 struct data_store
 {
-    data_store(const row_data &row, const std::string app_name_out)
-        : app_name(app_name_out),
+    data_store(const row_data &row, const std::string name)
+        : app_name(name),
           total_qps(row.get_total_qps()),
           total_cu(row.get_total_cu()),
           partition_name(row.row_name){};
