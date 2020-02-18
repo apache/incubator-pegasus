a85072b41329a10b6946735290130e40ac6a8e39
Smilencer <527646889@qq.com>
2020-02-18 18:21:20 +0800

    new alg

--- src/server/table_hotspot_policy.h
+++ src/server/table_hotspot_policy.h
@@ -66,7 +66,6 @@ public:
                 if (partition_data.total_qps > 1.00) {
                     data_samples.push_back(partition_data.total_qps);
                     avg += partition_data.total_qps;
-                    std::cout << "data_samples" << partition_data.total_qps << std::endl;
                     n++;
                 }
             }
@@ -79,7 +78,7 @@ public:
         avg /= n;
         for (auto data_sample : data_samples) {
             sd += pow((data_sample - avg), 2);
-            std::cout << "pow" << pow((data_sample - avg), 2) << std::endl;
+            std::cout << "pow" << data_sample - avg << std::endl;
         }
         sd = sqrt(sd / n);
         std::cout << "avg:" << avg << "sd:" << sd << "n:" << n << std::endl;
