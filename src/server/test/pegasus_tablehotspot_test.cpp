3a90d4bbb44b4bd9982952a1ff698ad57b579733
tangyanzhao <smityz@qq.com>
2020-02-21 15:57:43 +0800

    format

--- src/server/test/pegasus_tablehotspot_test.cpp
+++ src/server/test/pegasus_tablehotspot_test.cpp
@@ -8,23 +8,6 @@
 
 namespace pegasus {
 namespace server {
-/*
-TEST(table_hotspot_policy, hotspot_algo_qps_skew)
-{
-    std::vector<row_data> test_rows(2);
-    test_rows[0].get_qps = 1234.0;
-    test_rows[1].get_qps = 4321.0;
-    hotspot_calculator test_hotspot_calculator("TEST", 2);
-    test_hotspot_calculator.aggregate(test_rows);
-    test_hotspot_calculator.start_alg();
-    std::vector<double> result(2);
-    for (int i = 0; i < test_hotspot_calculator._points.size(); i++) {
-        result[i] = test_hotspot_calculator._points[i]->get_value();
-    }
-    std::vector<double> expect_vector{1, 3};
-    ASSERT_EQ(expect_vector, result);
-}
-*/
 
 TEST(table_hotspot_policy, hotspot_algo_qps_variance)
 {
