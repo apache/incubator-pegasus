2ccd646a669ba3462fbf60cb4b8204e9ccae5a86
neverchanje <wutao1@xiaomi.com>
2020-03-02 09:37:44 +0800

    feat(shell): support hint in add_dup

--- src/shell/commands/duplication.cpp
+++ src/shell/commands/duplication.cpp
@@ -52,8 +52,10 @@ bool add_dup(command_executor *e, shell_context *sc, arguments args)
 
     auto err_resp = sc->ddl_client->add_dup(app_name, remote_cluster_name, freeze);
     dsn::error_s err = err_resp.get_error();
+    std::string hint;
     if (err.is_ok()) {
         err = dsn::error_s::make(err_resp.get_value().err);
+        hint = err_resp.get_value().hint;
     }
     if (!err.is_ok()) {
         fmt::print(stderr,
@@ -62,6 +64,9 @@ bool add_dup(command_executor *e, shell_context *sc, arguments args)
                    remote_cluster_name,
                    freeze,
                    err.description());
+        if (!hint.empty()) {
+            fmt::print(stderr, "detail:\n  {}\n", hint);
+        }
     } else {
         const auto &resp = err_resp.get_value();
         fmt::print("adding duplication succeed [app: {}, remote: {}, appid: {}, dupid: "
