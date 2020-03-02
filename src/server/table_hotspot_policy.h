5875ccc690ad4296c7b2c69ba367f0cde4e8b789
Smilencer <527646889@qq.com>
2020-03-02 16:35:06 +0800

    1

--- src/server/table_hotspot_policy.h
+++ src/server/table_hotspot_policy.h
@@ -22,14 +22,14 @@ public:
     // vector is used to save the partitions' data of this app
     // hotspot_partition_data is used to save data of one partition
     virtual void analysis(const std::queue<std::vector<hotspot_partition_data>> &hotspot_app_data,
-                          std::vector<::dsn::perf_counter_wrapper> &hot_points) = 0;
+                          std::vector<::dsn::perf_counter_wrapper> &perf_counters) = 0;
 };
 
 class hotspot_algo_qps_skew : public hotspot_policy
 {
 public:
     void analysis(const std::queue<std::vector<hotspot_partition_data>> &hotspot_app_data,
-                  std::vector<::dsn::perf_counter_wrapper> &hot_points)
+                  std::vector<::dsn::perf_counter_wrapper> &perf_counters)
     {
         const auto &anly_data = hotspot_app_data.back();
         double min_total_qps = INT_MAX;
@@ -37,9 +37,9 @@ public:
             min_total_qps = std::min(min_total_qps, partition_anly_data.total_qps);
         }
         min_total_qps = std::max(1.0, min_total_qps);
-        dassert(anly_data.size() == hot_points.size(), "partition counts error, please check");
-        for (int i = 0; i < hot_points.size(); i++) {
-            hot_points[i]->set(anly_data[i].total_qps / min_total_qps);
+        dassert(anly_data.size() == perf_counters.size(), "partition counts error, please check");
+        for (int i = 0; i < perf_counters.size(); i++) {
+            perf_counters[i]->set(anly_data[i].total_qps / min_total_qps);
         }
     }
 };
@@ -49,15 +49,14 @@ class hotspot_algo_qps_variance : public hotspot_policy
 {
 public:
     void analysis(const std::queue<std::vector<hotspot_partition_data>> &hotspot_app_data,
-                  std::vector<::dsn::perf_counter_wrapper> &hot_points)
+                  std::vector<::dsn::perf_counter_wrapper> &perf_counters)
     {
-        dassert(hotspot_app_data.back().size() == hot_points.size(),
+        dassert(hotspot_app_data.back().size() == perf_counters.size(),
                 "partition counts error, please check");
         std::vector<double> data_samples;
-        data_samples.reserve(hotspot_app_data.size() * hot_points.size());
-        const auto &anly_data = hotspot_app_data.back();
+        data_samples.reserve(hotspot_app_data.size() * perf_counters.size());
         auto temp_data = hotspot_app_data;
-        double avg = 0, sd = 0;
+        double total = 0, sd = 0;
         int n = 0;
         // avg: Average number
         // sd: Standard deviation
@@ -66,7 +65,7 @@ public:
             for (auto partition_data : temp_data.front()) {
                 if (partition_data.total_qps - 1.00 > 0) {
                     data_samples.push_back(partition_data.total_qps);
-                    avg += partition_data.total_qps;
+                    total += partition_data.total_qps;
                     n++;
                 }
             }
@@ -76,17 +75,18 @@ public:
             ddebug("hotspot_app_data size == 0");
             return;
         }
-        avg /= n;
+        avg = total / n;
         for (auto data_sample : data_samples) {
             sd += pow((data_sample - avg), 2);
         }
         sd = sqrt(sd / n);
-        for (int i = 0; i < hot_points.size(); i++) {
+        const auto &anly_data = hotspot_app_data.back();
+        for (int i = 0; i < perf_counters.size(); i++) {
             double hot_point = (anly_data[i].total_qps - avg) / sd;
             // perf_counter->set can only be unsigned __int64
             // use ceil to guarantee conversion results
             hot_point = ceil(std::max(hot_point, double(0)));
-            hot_points[i]->set(hot_point);
+            perf_counters[i]->set(hot_point);
         }
     }
 };
