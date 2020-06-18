b90c5ee07c2360681f15c7f6935b81fa7d90227b
Smilencer <527646889@qq.com>
2020-06-18 15:41:38 +0800

    Update info_collector.cpp

--- src/server/info_collector.cpp
+++ src/server/info_collector.cpp
@@ -162,7 +162,7 @@ void info_collector::on_app_stat()
     }
     get_app_counters(all_stats.app_name)->set(all_stats);
 
-    ddebug("stat apps succeed, app_count = %d, total_read_qps = %.2f, total_write_qps = %.2f",
+    ddebug_f("stat apps succeed, app_count = {}, total_read_qps = {}, total_write_qps = {}",
            (int)(all_rows.size()),
            all_stats.get_total_read_qps(),
            all_stats.get_total_write_qps());
