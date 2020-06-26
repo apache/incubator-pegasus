332a53a445f0dbd00b0af1c08d78a35f0d0d4c8e
neverchanje <wutao1@xiaomi.com>
2020-06-27 00:55:32 +0800

    support rolling log

--- go.mod
+++ go.mod
@@ -7,4 +7,5 @@ require (
 	github.com/rcrowley/go-metrics v0.0.0-20200313005456-10cdbea86bc0
 	github.com/sirupsen/logrus v1.4.2
 	github.com/spf13/viper v1.7.0
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0
 )
--- main.go
+++ main.go
@@ -1,11 +1,39 @@
 package main
 
 import (
+	"fmt"
+	"runtime"
+	"strings"
+
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
+	"gopkg.in/natefinch/lumberjack.v2"
 )
 
+// callerPrettifier simplifies the caller info
+func callerPrettifier(f *runtime.Frame) (function string, file string) {
+	function = f.Function[strings.LastIndex(f.Function, "/")+1:]
+	file = fmt.Sprint(f.File[strings.LastIndex(f.File, "/")+1:], ":", f.Line)
+	return function, file
+}
+
 func main() {
+	// initialize logging
+	log.SetFormatter(&log.TextFormatter{
+		DisableColors:    true,
+		FullTimestamp:    true,
+		CallerPrettyfier: callerPrettifier,
+	})
+	log.SetOutput(&lumberjack.Logger{
+		Filename:  "./pegasus.log",
+		MaxSize:   500, // MegaBytes
+		MaxAge:    5,   // days
+		LocalTime: true,
+	})
+	log.SetReportCaller(true)
+
+	// TODO(wutao1): use args[1] as config path
+	viper.AddConfigPath("config.yaml")
 	viper.SetConfigType("yaml")
 	if err := viper.ReadInConfig(); err != nil {
 		log.Fatal("failed to read config: ", err)
