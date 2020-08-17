16001a57d07555bfad8afcd1ef9f39fc0609a6b5
JiaShuo <js982986555@live.com>
2020-08-17 12:04:20 +0800

    fix

--- scripts/pegasus_rebalance_cluster.sh
+++ scripts/pegasus_rebalance_cluster.sh
@@ -79,11 +79,11 @@ while true; do
         break
     fi
 
-    if [ $op_count -eq 0 && op_count_check_time -eq 0 ]; then
+    if [ $op_count -eq 0 -a op_count_check_time -eq 0 ]; then
         break
     fi
 
-    if [ $op_count -eq 0 && op_count_check_time -gt 0 ]; then
+    if [ $op_count -eq 0 -a op_count_check_time -gt 0 ]; then
         echo "Cluster may be balanced, try wait 30 seconds..."
         $op_count_check_time--
         sleep 30
