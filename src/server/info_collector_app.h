a1bdfeaf0ae08a5861518b256573e71822e2b64e
Wu Tao <wutao163.com@163.com>
2020-08-25 09:51:35 +0800

    fix: add library dependencies to fix compiler error (#584)

--- src/server/info_collector_app.h
+++ src/server/info_collector_app.h
@@ -5,7 +5,7 @@
 #pragma once
 
 #include <dsn/cpp/service_app.h>
-#include <dsn/tool-api/http_server.h>
+#include <dsn/http/http_server.h>
 #include "info_collector.h"
 #include "available_detector.h"
 
