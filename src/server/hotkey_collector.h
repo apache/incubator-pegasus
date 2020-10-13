96a0e7f582bef54fef229aeae85b4788ed211157
tangyanzhao <smityz@qq.com>
2020-10-13 14:41:15 +0800

    delete headers

--- src/server/hotkey_collector.h
+++ src/server/hotkey_collector.h
@@ -19,8 +19,6 @@
 
 #include <dsn/utility/string_view.h>
 #include <dsn/dist/replication/replication_types.h>
-#include <dsn/dist/replication/replica_base.h>
-#include <dsn/dist/fmt_logging.h>
 #include "hotkey_state_machine.h"
 
 namespace pegasus {
