8810e4231c37263d2c7e9baae4e7e55fb9bd2a76
neverchanje <wutao.as.neverchanje@gmail.com>
2020-10-30 07:26:07 +0800

    fix

--- src/test/bench_test/config.h
+++ src/test/bench_test/config.h
@@ -17,10 +17,6 @@
  * under the License.
  */
 
-// Copyright (c) 2018, Xiaomi, Inc.  All rights reserved.
-// This source code is licensed under the Apache License Version 2.0, which
-// can be found in the LICENSE file in the root directory of this source tree.
-
 #pragma once
 
 #include <string>
