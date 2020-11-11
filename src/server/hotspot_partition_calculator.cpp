1570c38f0f81caaadd85451be59f57f79c740994
tangyanzhao <tangyanzhao@xiaomi.com>
2020-11-11 17:16:25 +0800

    format

--- src/server/hotspot_partition_calculator.cpp
+++ src/server/hotspot_partition_calculator.cpp
@@ -182,8 +182,8 @@ void hotspot_partition_calculator::detect_hotkey_in_hotpartition(int data_type)
     int partition_count;
     std::vector<dsn::partition_configuration> partitions;
     _shell_context->ddl_client->list_app(app_name, app_id, partition_count, partitions);
-    auto target_address = partitions[partition_index].primary;
 
+    auto target_address = partitions[partition_index].primary;
     dsn::replication::detect_hotkey_response resp;
     dsn::replication::detect_hotkey_request req;
     req.type = hotkey_type;
