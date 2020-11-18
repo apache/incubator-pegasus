f8829668ef9f2de78577abc815e0767e0daf02b5
neverchanje <wutao.as.neverchanje@gmail.com>
2020-11-19 04:26:22 +0800

    feat: support formatter for stat value

--- tabular/perf.go
+++ tabular/perf.go
@@ -4,59 +4,85 @@ import (
 	"fmt"
 	"io"
 
-	jsoniter "github.com/json-iterator/go"
+	"github.com/dustin/go-humanize"
 	"github.com/olekukonko/tablewriter"
 	"github.com/pegasus-kv/collector/aggregate"
+	"gopkg.in/yaml.v2"
 )
 
-var tableStatsTemplate string = `{
-	"Write": [
-		"put_qps",
-		"multi_put_qps",
-		"incr_qps",
-		"check_and_set_qps",
-		"check_and_mutate_qps",
-		"write_bytes"
-	],
-	"Read": [
-		"get_qps",
-		"multi_get_qps",
-		"scan_qps",
-		"read_bytes"
-	],
-	"Storage": [
-		"sst_storage_mb",
-		"rdb_estimate_num_keys"
-	],
-	"Memory": [
-		"rdb_memtable_mem_usage",
-		"rdb_index_and_filter_blocks_mem_usage"
-	],
-	"Peformance": [
-		"recent_expire_count",
-		"recent_filter_count",
-		"recent_abnormal_count"
-	]
-}`
+var tableStatsTemplate string = `--- 
+Memory: 
+  rdb_index_and_filter_blocks_mem_usage: 
+    unit: size
+  rdb_memtable_mem_usage: 
+    unit: size
+Peformance: 
+  recent_abnormal_count: ~
+  recent_expire_count: ~
+  recent_filter_count: ~
+Read: 
+  get_qps: ~
+  multi_get_qps: ~
+  read_bytes: 
+    unit: size
+  scan_qps: ~
+Storage: 
+  rdb_estimate_num_keys: ~
+  sst_storage_mb: ~
+Write: 
+  check_and_mutate_qps: ~
+  check_and_set_qps: ~
+  incr_qps: ~
+  multi_put_qps: ~
+  put_qps: ~
+  write_bytes: 
+    unit: size
+`
+
+// The default statFormatter if no unit is specified
+func defaultStatFormatter(v float64) string {
+	return humanize.SI(v, "")
+}
+
+// Used for counter with `"unit" : "size"`.
+func sizeStatFormatter(v float64) string {
+	return humanize.Bytes(uint64(v))
+}
+
+type statFormatter func(float64) string
 
 // PrintTableStatsTabular prints table stats in a number of sections,
 // according to the predefined template.
 func PrintTableStatsTabular(writer io.Writer, tables map[int32]*aggregate.TableStats) {
-	iter := jsoniter.ParseString(jsoniter.ConfigDefault, tableStatsTemplate)
-	iter.ReadMapCB(func(i *jsoniter.Iterator, section string) bool {
+	var sections map[string]interface{}
+	yaml.Unmarshal([]byte(tableStatsTemplate), &sections)
+
+	for sect, columns := range sections {
 		// print section
 		table := tablewriter.NewWriter(writer)
 		table.SetBorders(tablewriter.Border{Left: false, Right: false, Top: true, Bottom: false})
 		table.SetRowSeparator("=")
-		table.SetHeader([]string{section})
+		table.SetHeader([]string{sect})
 		table.Render()
 
-		// print table
 		header := append([]string{"AppID", "Name", "Partitions"})
-		i.ReadArrayCB(func(i *jsoniter.Iterator) bool {
-			header = append(header, i.ReadString())
-			return true
-		})
+		var formatters []statFormatter
+		for columnName, attrs := range columns.(map[interface{}]interface{}) {
+			header = append(header, columnName.(string))
+
+			if attrs == nil {
+				formatters = append(formatters, defaultStatFormatter)
+			} else {
+				attrsMap := attrs.(map[interface{}]interface{})
+				unit := attrsMap["unit"]
+				if unit == "size" {
+					formatters = append(formatters, sizeStatFormatter)
+				} else {
+					panic(fmt.Sprintf("invalid unit %s in template", unit))
+				}
+			}
+		}
+
 		tabWriter := tablewriter.NewWriter(writer)
 		tabWriter.SetBorder(false)
 		tabWriter.SetHeader(header)
@@ -64,13 +90,12 @@ func PrintTableStatsTabular(writer io.Writer, tables map[int32]*aggregate.TableS
 			// each table displays as a row
 			var row []string
 			row = append(row, fmt.Sprintf("%d", tb.AppID), tb.TableName, fmt.Sprintf("%d", len(tb.Partitions)))
-			for _, key := range header[3:] {
-				row = append(row, fmt.Sprintf("%v", tb.Stats[key]))
+			for i, key := range header[3:] {
+				row = append(row, formatters[i](tb.Stats[key]))
 			}
 			tabWriter.Append(row)
 		}
 		tabWriter.Render()
 		fmt.Fprintln(writer)
-		return true
-	})
+	}
 }
