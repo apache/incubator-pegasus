a6ba3839ecef95b654693cb3b7a388b2d1dd890c
neverchanje <wutao.as.neverchanje@gmail.com>
2020-11-21 12:57:17 +0800

    feat: implement meta level update and auto-complete

--- cmd/meta_level.go
+++ cmd/meta_level.go
@@ -3,9 +3,15 @@ package cmd
 import (
 	"admin-cli/executor"
 	"admin-cli/shell"
+	"fmt"
 
 	"github.com/desertbit/grumble"
 )
+
+var predefinedMetaLevel = []string{
+	"lively",
+	"steady",
+}
 
 func init() {
 	rootCmd := &grumble.Command{
@@ -15,5 +21,24 @@ func init() {
 			return executor.GetMetaLevel(pegasusClient)
 		},
 	}
+	rootCmd.AddCommand(
+		&grumble.Command{
+			Name: "set",
+			Help: "reset the meta function level",
+			Run: func(c *grumble.Context) error {
+				if len(c.Args) != 1 {
+					return fmt.Errorf("invalid number (%d) of arguments for `meta-level set`", len(c.Args))
+				}
+				return executor.SetMetaLevel(pegasusClient, c.Args[0])
+			},
+			Completer: func(prefix string, args []string) []string {
+				if len(args) == 0 {
+					return filterStringWithPrefix(predefinedMetaLevel, prefix)
+				}
+				return []string{}
+			},
+			AllowArgs: true,
+		},
+	)
 	shell.App.AddCommand(rootCmd)
 }
