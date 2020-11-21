9ae478e1682d8c4ae204549d5cc59d29f2f6ecba
neverchanje <wutao.as.neverchanje@gmail.com>
2020-11-21 09:54:49 +0800

    feat: implement del and clear app_envs, and auto-complete of set app envs

--- cmd/app_env.go
+++ cmd/app_env.go
@@ -8,6 +8,26 @@ import (
 	"github.com/desertbit/grumble"
 )
 
+// NOTE: some old-version servers may not support some of the keys.
+var predefinedAppEnvKeys = []string{
+	"rocksdb.usage_scenario",
+	"replica.deny_client_write",
+	"replica.write_throttling",
+	"replica.write_throttling_by_size",
+	"default_ttl",
+	"manual_compact.disabled",
+	"manual_compact.max_concurrent_running_count",
+	"manual_compact.once.trigger_time",
+	"manual_compact.once.target_level",
+	"manual_compact.once.bottommost_level_compaction",
+	"manual_compact.periodic.trigger_time",
+	"manual_compact.periodic.target_level",
+	"manual_compact.periodic.bottommost_level_compaction",
+	"rocksdb.checkpoint.reserve_min_count",
+	"rocksdb.checkpoint.reserve_time_seconds",
+	"replica.slow_query_threshold",
+}
+
 func init() {
 	rootCmd := &grumble.Command{
 		Name: "app-env",
@@ -32,6 +52,35 @@ func init() {
 			return executor.SetAppEnv(pegasusClient, useTable, c.Args[0], c.Args[1])
 		},
 		AllowArgs: true,
+		Completer: func(prefix string, args []string) []string {
+			/* fill with predefined app-envs */
+			if len(args) == 0 {
+				return filterStringWithPrefix(predefinedAppEnvKeys, prefix)
+			}
+			return []string{}
+		},
+	})
+	rootCmd.AddCommand(&grumble.Command{
+		Name:    "delete",
+		Aliases: []string{"del"},
+		Help:    "delete app-envs with specified key or key prefix",
+		Run: func(c *grumble.Context) error {
+			if len(c.Args) != 1 {
+				return fmt.Errorf("invalid number (%d) of arguments for `app-env delete`", len(c.Args))
+			}
+			return executor.DelAppEnv(pegasusClient, useTable, c.Args[0], c.Flags.Bool("prefix"))
+		},
+		AllowArgs: true,
+		Flags: func(f *grumble.Flags) {
+			f.BoolL("prefix", false, "to delete with key prefix")
+		},
+	})
+	rootCmd.AddCommand(&grumble.Command{
+		Name: "clear",
+		Help: "clear all app-envs",
+		Run: func(c *grumble.Context) error {
+			return executor.ClearAppEnv(pegasusClient, useTable)
+		},
 	})
 	shell.AddCommand(rootCmd)
 }
--- executor/app_env.go
+++ executor/app_env.go
@@ -32,17 +32,47 @@ func ListAppEnvs(c *Client, useTable string) error {
 
 // SetAppEnv command
 func SetAppEnv(c *Client, useTable string, key, value string) error {
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
-	defer cancel()
-	resp, err := c.Meta.UpdateAppEnv(ctx, &admin.UpdateAppEnvRequest{
+	return updateAppEnv(c, &admin.UpdateAppEnvRequest{
 		Keys:    []string{key},
 		Values:  []string{value},
 		Op:      admin.AppEnvOperation_APP_ENV_OP_SET,
 		AppName: useTable,
 	})
+}
+
+// ClearAppEnv command
+func ClearAppEnv(c *Client, useTable string) error {
+	return updateAppEnv(c, &admin.UpdateAppEnvRequest{
+		Op:          admin.AppEnvOperation_APP_ENV_OP_CLEAR,
+		AppName:     useTable,
+		ClearPrefix: new(string), /*empty prefix means clear of all*/
+	})
+}
+
+// DelAppEnv command
+// TODO(wutao): deleting a non-existed key returns "OK" now.
+func DelAppEnv(c *Client, useTable string, key string, deletePrefix bool) error {
+	if deletePrefix {
+		return updateAppEnv(c, &admin.UpdateAppEnvRequest{
+			Op:          admin.AppEnvOperation_APP_ENV_OP_CLEAR,
+			AppName:     useTable,
+			ClearPrefix: &key,
+		})
+	}
+	return updateAppEnv(c, &admin.UpdateAppEnvRequest{
+		Op:      admin.AppEnvOperation_APP_ENV_OP_DEL,
+		Keys:    []string{key},
+		AppName: useTable,
+	})
+}
+
+func updateAppEnv(c *Client, req *admin.UpdateAppEnvRequest) error {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+	resp, err := c.Meta.UpdateAppEnv(ctx, req)
 	if err != nil {
 		if resp != nil {
-			return fmt.Errorf("failed to set app (\"%s\") with env \"%s\" => \"%s\" [%s]: %s", useTable, key, value, resp.Err.Errno, resp.HintMessage)
+			return fmt.Errorf("failed to update app envs:\n%s\nErrno:%s\nHint message:%s", req.String(), resp.Err.Errno, resp.HintMessage)
 		}
 		return err
 	}
