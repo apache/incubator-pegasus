b92ba6d5b3a9a15370b85f82814d94a746ace132
Zhang Yifan <chinazhangyifan@163.com>
2020-12-28 11:33:07 +0800

    fix(test): add gtest framework to meta tests (#709)

--- src/meta/test/main.cpp
+++ src/meta/test/main.cpp
@@ -1,12 +1,27 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
 #include <cmath>
+#include <dsn/service_api_cpp.h>
 #include <fstream>
+#include <gtest/gtest.h>
 #include <iostream>
 
-#include <gtest/gtest.h>
-#include <dsn/service_api_cpp.h>
-
 #include "meta/meta_data.h"
-
 #include "meta_service_test_app.h"
 
 int gtest_flags = 0;
@@ -82,12 +97,10 @@ dsn::error_code meta_service_test_app::start(const std::vector<std::string> &arg
 
 GTEST_API_ int main(int argc, char **argv)
 {
+    testing::InitGoogleTest(&argc, argv);
     dsn::service_app::register_factory<dsn::replication::meta_service_test_app>("test_meta");
     dsn::service::meta_service_app::register_all();
-    if (argc < 2)
-        dassert(dsn_run_config("config-test.ini", false), "");
-    else
-        dassert(dsn_run_config(argv[1], false), "");
+    dsn_run_config("config-test.ini", false);
     while (gtest_flags == 0) {
         std::this_thread::sleep_for(std::chrono::seconds(1));
     }
