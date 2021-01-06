a5a4f67ed36c852bfd2f6870945b5d04088be049
levy <zlw5307@163.com>
2021-01-06 11:19:56 +0800

    fix

--- src/server/pegasus_write_service_impl.h
+++ src/server/pegasus_write_service_impl.h
@@ -104,7 +104,8 @@ public:
 
     int empty_put(int64_t decree)
     {
-        int err = _rocksdb_wrapper->write_batch_put(decree, dsn::string_view(), dsn::string_view(), 0);
+        int err =
+            _rocksdb_wrapper->write_batch_put(decree, dsn::string_view(), dsn::string_view(), 0);
         auto cleanup = dsn::defer([this]() { _rocksdb_wrapper->clear_up_write_batch(); });
         if (err) {
             return err;
