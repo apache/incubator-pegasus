b8f07fb7ee7fc075c0a6de4773aa78ac4f159702
zhao liwei <zlw5307@163.com>
2021-01-15 15:10:10 +0800

    Update src/server/pegasus_server_write.cpp
    Co-authored-by: Wu Tao <wutao1@xiaomi.com>

--- src/server/pegasus_server_write.cpp
+++ src/server/pegasus_server_write.cpp
@@ -54,9 +54,8 @@ int pegasus_server_write::on_batched_write_requests(dsn::message_ex **requests,
     if (iter != _single_put_methods.end()) {
         dcheck_eq(count, 1);
         return iter->second(this, requests[0]);
-    } else {
-        return on_batched_writes(requests, count);
     }
+    return on_batched_writes(requests, count);
 }
 
 void pegasus_server_write::set_default_ttl(uint32_t ttl) { _write_svc->set_default_ttl(ttl); }
