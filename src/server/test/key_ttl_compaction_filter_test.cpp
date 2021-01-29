baf56b3cf8144cd073ae478c1629f958422b4b55
levy <zlw5307@163.com>
2021-01-29 21:07:11 +0800

    fix

--- src/server/test/key_ttl_compaction_filter_test.cpp
+++ src/server/test/key_ttl_compaction_filter_test.cpp
@@ -31,27 +31,35 @@ TEST(key_ttl_compaction_filter_test, need_clean_key)
     {
         std::string hash_key;
         int32_t expire_sec_from_now;
+        bool clear_ttl = false;
         bool need_clean;
-    } tests[] = {{"raw_tts_audio:", 100, false},
-                 {"raw_tts_audio:xxx", 100, false},
-                 {"raw_tts_audio:xxx", 3 * oneday_sec - 1, false},
-                 {"raw_tts_audio:xxx", 3 * oneday_sec, true},
-                 {"raw_tts_audio", 4 * oneday_sec, false},
-                 {"stored_tts_url_info:", 100, false},
-                 {"stored_tts_url_info:xxx", 100, false},
-                 {"stored_tts_url_info:xxx", 3 * oneday_sec - 1, false},
-                 {"stored_tts_url_info:xxx", 3 * oneday_sec, true},
-                 {"stored_tts_url_info", 4 * oneday_sec, false},
-                 {"donot_clean_key", 100, false},
-                 {"donot_clean_key", 4 * oneday_sec, false}};
+    } tests[] = {{"raw_tts_audio:", 100, false, false},
+                 {"raw_tts_audio:xxx", 100, false, false},
+                 {"raw_tts_audio:xxx", 3 * oneday_sec - 1, false, false},
+                 {"raw_tts_audio:xxx", 3 * oneday_sec, false, true},
+                 {"raw_tts_audio:no_ttl", 3 * oneday_sec, true, false},
+                 {"raw_tts_audio", 4 * oneday_sec, false, false},
+                 {"stored_tts_url_info:", 100, false, false},
+                 {"stored_tts_url_info:xxx", 100, false, false},
+                 {"stored_tts_url_info:xxx", 3 * oneday_sec - 1, false, false},
+                 {"stored_tts_url_info:xxx", 3 * oneday_sec, false, true},
+                 {"stored_tts_url_info:no_ttl", 3 * oneday_sec, true, false},
+                 {"stored_tts_url_info", 4 * oneday_sec, false, false},
+                 {"donot_clean_key", 100, false, false},
+                 {"donot_clean_key", 4 * oneday_sec, false, false},
+                 {"donot_clean_key_no_ttl", 4 * oneday_sec, true, false}};
 
     for (auto const &test : tests) {
         uint32_t now_ts = utils::epoch_now();
 
         dsn::blob raw_key;
         pegasus_generate_key(raw_key, test.hash_key, std::string("sort"));
+        uint32_t expire_ts = test.expire_sec_from_now + now_ts;
+        if (test.clear_ttl) {
+            expire_ts = 0;
+        }
         bool need_clean = need_clean_key(
-            utils::to_rocksdb_slice(raw_key), test.expire_sec_from_now + now_ts, now_ts);
+            utils::to_rocksdb_slice(raw_key), expire_ts, now_ts);
         ASSERT_EQ(need_clean, test.need_clean);
     }
 }
