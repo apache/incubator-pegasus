88783e1ec28c326974f808d91c1531391d38acb5
zhao liwei <zlw5307@163.com>
2021-04-20 10:32:54 +0800

    refactor: add some base classes to refactor pegasus_value_schema (#709)

---
+++ src/base/value_schema_manager.cpp
@@ -0,0 +1,62 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+#include "value_schema_manager.h"
+
+namespace pegasus {
+
+void value_schema_manager::register_schema(value_schema *schema)
+{
+    /// TBD(zlw)
+}
+
+value_schema *value_schema_manager::get_value_schema(uint32_t meta_cf_data_version,
+                                                     dsn::string_view value) const
+{
+    /// TBD(zlw)
+    return nullptr;
+}
+
+value_schema *value_schema_manager::get_value_schema(uint32_t version) const
+{
+    /// TBD(zlw)
+    return nullptr;
+}
+
+value_schema *value_schema_manager::get_latest_value_schema() const
+{
+    /// TBD(zlw)
+    return nullptr;
+}
+
+/**
+ * If someone wants to add a new data version, he only need to implement the new value schema,
+ * and register it here.
+ */
+void register_value_schemas()
+{
+    /// TBD(zlw)
+}
+
+struct value_schemas_registerer
+{
+    value_schemas_registerer() { register_value_schemas(); }
+};
+static value_schemas_registerer value_schemas_reg;
+} // namespace pegasus
---
+++ src/base/value_schema_manager.h
@@ -0,0 +1,43 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+#pragma once
+
+#include "pegasus_value_schema.h"
+#include <dsn/utility/singleton.h>
+
+namespace pegasus {
+
+class value_schema_manager : public dsn::utils::singleton<value_schema_manager>
+{
+public:
+    void register_schema(value_schema *schema);
+    /// using the raw value in rocksdb and data version stored in meta column family to get data
+    /// version
+    value_schema *get_value_schema(uint32_t meta_cf_data_version, dsn::string_view value) const;
+    value_schema *get_value_schema(uint32_t version) const;
+    value_schema *get_latest_value_schema() const;
+
+private:
+    value_schema_manager() = default;
+    friend class dsn::utils::singleton<value_schema_manager>;
+
+    std::array<value_schema *, 2> _schemas;
+};
+} // namespace pegasus
