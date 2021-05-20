9e37a45f8b38d61191fb6aa35df5be6fac1f2d36
levy <zlw5307@163.com>
2021-05-20 16:36:57 +0800

    feat: add base classes for user-specified compaction

---
+++ src/server/compaction_filter_operation.cpp
@@ -0,0 +1,43 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+#include "compaction_filter_operation.h"
+
+namespace pegasus {
+namespace server {
+compaction_filter_operation::~compaction_filter_operation() = default;
+
+bool compaction_filter_operation::all_rules_match(const std::string &hash_key,
+                                                  const std::string &sort_key,
+                                                  const rocksdb::Slice &existing_value) const
+{
+    if (rules.size() == 0) {
+        return false;
+    }
+
+    for (const auto &rule : rules) {
+        if (!rule->match(hash_key, sort_key, existing_value)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+} // namespace server
+} // namespace pegasus
