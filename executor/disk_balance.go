19335d6f8392005c02888967a501a8702fc4f68f
Jiashuo <js982986555@live.com>
2021-06-09 14:35:46 +0800

    feat(disk-balance): complete framework for adding disk balance automatically(3/3) (#39)

--- executor/disk_balance.go
+++ executor/disk_balance.go
@@ -22,6 +22,7 @@ package executor
 import (
 	"context"
 	"fmt"
+	"math"
 	"strings"
 	"time"
 
@@ -111,7 +112,6 @@ func DiskBalance(client *Client, replicaServer string, minSize int64, auto bool)
 					time.Sleep(WaitRunning)
 					continue
 				}
-
 				if strings.Contains(err.Error(), base.ERR_BUSY.String()) {
 					fmt.Printf("migrate(%s) is running, msg=%s, wait complete...\n", action.toString(), err.Error())
 					time.Sleep(WaitRunning)
@@ -129,19 +129,25 @@ func DiskBalance(client *Client, replicaServer string, minSize int64, auto bool)
 		}
 		break
 	}
-
 	return nil
 }
 
 type DiskStats struct {
-	DiskCapacity    DiskCapacityStruct
-	ReplicaCapacity []ReplicaCapacityStruct
+	diskCapacity    DiskCapacityStruct
+	replicaCapacity []ReplicaCapacityStruct
 }
 
 type MigrateDisk struct {
-	AverageUsage int64
-	HighDisk     DiskStats
-	LowDisk      DiskStats
+	averageUsage int64
+	currentNode  string
+	highDisk     DiskStats
+	lowDisk      DiskStats
+}
+
+func (m *MigrateDisk) toString() string {
+	return fmt.Sprintf("Node=%s, highDisk=%s[%dMB(%d%%)]=>lowDisk=%s[%dMB(%d%%)]", m.currentNode,
+		m.highDisk.diskCapacity.Disk, m.highDisk.diskCapacity.Usage, m.highDisk.diskCapacity.Ratio,
+		m.lowDisk.diskCapacity.Disk, m.lowDisk.diskCapacity.Usage, m.lowDisk.diskCapacity.Ratio)
 }
 
 type MigrateAction struct {
@@ -196,23 +202,176 @@ func forceAssignReplicaToSecondary(client *Client, replicaServer string, gpid st
 		util.NewNodeFromTCPAddr(replicaServer, session.NodeTypeReplica), secondaryNode)
 }
 
-// todo(jiashuo1) next pr
 func getReplicaSecondaryNode(client *Client, gpid string) (*util.PegasusNode, error) {
-	return nil, nil
+	replica, err := util.Str2Gpid(gpid)
+	if err != nil {
+		return nil, err
+	}
+	tables, err := client.Meta.ListApps(admin.AppStatus_AS_AVAILABLE)
+	if err != nil {
+		return nil, fmt.Errorf("can't get the table name of replica %s when migrate the replica", gpid)
+	}
+	var tableName string
+	for _, tb := range tables {
+		if tb.AppID == replica.Appid {
+			tableName = tb.AppName
+			break
+		}
+	}
+	if tableName == "" {
+		return nil, fmt.Errorf("can't find the table for %s when migrate the replica", gpid)
+	}
+
+	resp, err := client.Meta.QueryConfig(tableName)
+	if err != nil {
+		return nil, fmt.Errorf("can't get the table %s configuration when migrate the replica(%s): %s",
+			tableName, gpid, err)
+	}
+
+	var secondaryNode *util.PegasusNode
+	for _, partition := range resp.Partitions {
+		if partition.Pid.String() == replica.String() {
+			secondaryNode = util.NewNodeFromTCPAddr(partition.Secondaries[0].GetAddress(), session.NodeTypeReplica)
+		}
+	}
+
+	if secondaryNode == nil {
+		return nil, fmt.Errorf("can't get the replica %s secondary node", gpid)
+	}
+	return secondaryNode, nil
 }
 
-// todo(jiashuo1) next pr
 func queryDiskCapacityInfo(client *Client, replicaServer string) ([]DiskCapacityStruct, int64, int64, error) {
-	return nil, 0, 0, nil
+	diskCapacityOnNode, err := queryDiskInfo(client, CapacitySize, replicaServer, "", "", false)
+	if err != nil {
+		return nil, 0, 0, err
+	}
+	util.SortStructsByField(diskCapacityOnNode, "Usage")
+	var disks []DiskCapacityStruct
+	var totalUsage int64
+	var totalCapacity int64
+	for _, disk := range diskCapacityOnNode {
+		if s, ok := disk.(DiskCapacityStruct); ok {
+			disks = append(disks, s)
+			totalUsage += s.Usage
+			totalCapacity += s.Capacity
+		} else {
+			return nil, 0, 0, fmt.Errorf("can't covert to DiskCapacityStruct")
+		}
+	}
+
+	if disks == nil {
+		return nil, 0, 0, fmt.Errorf("the node(%s) has no ssd", replicaServer)
+	}
+	if len(disks) == 1 {
+		return nil, 0, 0, fmt.Errorf("the node(%s) only has one disk, can't balance", replicaServer)
+	}
+
+	return disks, totalUsage, totalCapacity, nil
 }
 
-// todo(jiashuo1) next pr
 func getMigrateDiskInfo(client *Client, replicaServer string, disks []DiskCapacityStruct,
 	totalUsage int64, totalCapacity int64) (*MigrateDisk, error) {
-	return nil, nil
+	highUsageDisk := disks[len(disks)-1]
+	highDiskInfo, err := queryDiskInfo(client, CapacitySize, replicaServer, "", highUsageDisk.Disk, false)
+	if err != nil {
+		return nil, err
+	}
+	lowUsageDisk := disks[0]
+	lowDiskInfo, err := queryDiskInfo(client, CapacitySize, replicaServer, "", lowUsageDisk.Disk, false)
+	if err != nil {
+		return nil, err
+	}
+
+	if highUsageDisk.Ratio < 10 {
+		return nil, fmt.Errorf("no need balance since the high disk still enough capacity(balance threshold=10%%): "+
+			"high(%s): %dMB(%d%%); low(%s): %dMB(%d%%)", highUsageDisk.Disk, highUsageDisk.Usage,
+			highUsageDisk.Ratio, lowUsageDisk.Disk, lowUsageDisk.Usage, lowUsageDisk.Ratio)
+	}
+
+	averageUsage := totalUsage / int64(len(disks))
+	averageRatio := totalUsage * 100 / totalCapacity
+	if highUsageDisk.Ratio-lowUsageDisk.Ratio < 5 {
+		return nil, fmt.Errorf("no need balance since the disk is balanced:"+
+			" high(%s): %dMB(%d%%); low(%s): %dMB(%d%%); average: %dMB(%d%%)",
+			highUsageDisk.Disk, highUsageDisk.Usage, highUsageDisk.Ratio, lowUsageDisk.Disk,
+			lowUsageDisk.Usage, lowUsageDisk.Ratio, averageUsage, averageRatio)
+	}
+
+	replicaCapacityOnHighDisk, err := convertReplicaCapacityStruct(highDiskInfo)
+	if err != nil { // we need migrate replica from high disk, so the convert must be successful
+		return nil, fmt.Errorf("parse replica info on high disk(%s) failed: %s", highUsageDisk.Disk, err.Error())
+	}
+	replicaCapacityOnLowDisk, err := convertReplicaCapacityStruct(lowDiskInfo)
+	if err != nil { // we don't care the replica capacity info on low disk, if convert failed, we only log warning and know the result=nil
+		fmt.Printf("WARNING: parse replica info on low disk(%s) failed: %s", lowUsageDisk.Disk, err.Error())
+	}
+	return &MigrateDisk{
+		averageUsage: averageUsage,
+		currentNode:  replicaServer,
+		highDisk: DiskStats{
+			diskCapacity:    highUsageDisk,
+			replicaCapacity: replicaCapacityOnHighDisk,
+		},
+		lowDisk: DiskStats{
+			diskCapacity:    lowUsageDisk,
+			replicaCapacity: replicaCapacityOnLowDisk,
+		},
+	}, nil
 }
 
-// todo(jiashuo1) next pr
 func computeMigrateAction(migrate *MigrateDisk, minSize int64) (*MigrateAction, error) {
-	return nil, nil
+	lowDiskCanReceiveMax := migrate.averageUsage - migrate.lowDisk.diskCapacity.Usage
+	highDiskCanSendMax := migrate.highDisk.diskCapacity.Usage - migrate.averageUsage
+	sizeNeedMove := int64(math.Min(float64(lowDiskCanReceiveMax), float64(highDiskCanSendMax)))
+
+	var selectReplica *ReplicaCapacityStruct
+	for i := len(migrate.highDisk.replicaCapacity) - 1; i >= 0; i-- {
+		if migrate.highDisk.replicaCapacity[i].Size > sizeNeedMove {
+			continue
+		} else {
+			selectReplica = &migrate.highDisk.replicaCapacity[i]
+			break
+		}
+	}
+
+	if selectReplica == nil {
+		return nil, fmt.Errorf("can't balance(%s): high disk min replica(%s) size(%dMB) must <= sizeNeedMove(%dMB)",
+			migrate.toString(),
+			migrate.highDisk.replicaCapacity[0].Gpid,
+			migrate.highDisk.replicaCapacity[0].Size,
+			sizeNeedMove)
+	}
+
+	// if select replica size is too small, it will need migrate many replica and result in `replica count not balance` among disk
+	if selectReplica.Size < minSize {
+		return nil, fmt.Errorf("not suggest balance(%s): the qualified(must<=sizeNeedMove(%dMB)) replica size(%s=%dMB) must >= minSize(%dMB))",
+			migrate.toString(), sizeNeedMove, selectReplica.Gpid, selectReplica.Size, minSize)
+	}
+
+	fmt.Printf("ACTION:disk migrate(sizeNeedMove=%dMB): %s, gpid(%s)=%s(%dMB)\n",
+		sizeNeedMove, migrate.toString(), selectReplica.Status, selectReplica.Gpid, selectReplica.Size)
+
+	return &MigrateAction{
+		node:    migrate.currentNode,
+		replica: *selectReplica,
+		from:    migrate.highDisk.diskCapacity.Disk,
+		to:      migrate.lowDisk.diskCapacity.Disk,
+	}, nil
+}
+
+func convertReplicaCapacityStruct(replicaCapacityInfos []interface{}) ([]ReplicaCapacityStruct, error) {
+	util.SortStructsByField(replicaCapacityInfos, "Size")
+	var replicas []ReplicaCapacityStruct
+	for _, replica := range replicaCapacityInfos {
+		if r, ok := replica.(ReplicaCapacityStruct); ok {
+			replicas = append(replicas, r)
+		} else {
+			return nil, fmt.Errorf("can't covert to ReplicaCapacityStruct")
+		}
+	}
+	if replicas == nil {
+		return nil, fmt.Errorf("the disk has no replica")
+	}
+	return replicas, nil
 }
