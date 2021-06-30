46281dad19ffb8b6da94ab5b460ed87f386395ce
HeYuchen <377710264@qq.com>
2021-06-30 18:03:35 +0800

    feat(split): add split function test (#733)

--- src/test/function_test/run.sh
+++ src/test/function_test/run.sh
@@ -75,4 +75,6 @@ if [ $on_travis == "NO" ]; then
     exit_if_fail $? "run test test_detect_hotspot load failed: $test_case $config_file $table_name"
     GTEST_OUTPUT="xml:$REPORT_DIR/backup_restore_test.xml" GTEST_FILTER="backup_restore_test.*" ./$test_case $config_file $table_name
     exit_if_fail $? "run test backup_restore_test load failed: $test_case $config_file $table_name"
+    GTEST_OUTPUT="xml:$REPORT_DIR/split.xml" GTEST_FILTER="partition_split_test.*" ./$test_case $config_file $table_name
+    exit_if_fail $? "run test split failed: $test_case $config_file $table_name"
 fi
