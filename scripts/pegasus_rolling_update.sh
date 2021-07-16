44645f58b30168e138f5c59e176ae82957b36b37
jiashuo <js982986555@live.com>
2021-07-16 13:50:51 +0800

    fix

--- scripts/pegasus_rolling_update.sh
+++ scripts/pegasus_rolling_update.sh
@@ -124,7 +124,7 @@ if [ $set_ok -ne 1 ]; then
 fi
 
 echo "Set lb.assign_delay_ms to 60min..."
-echo "remote_command -l $pmeta meta.lb.assign_delay_ms 3600000" | ./run.sh shell --cluster $meta_list &>/tmp/$UID.$PID.pegasus.rolling_node.assign_delay_ms
+echo "remote_command -l $pmeta meta.lb.assign_delay_ms 1800000" | ./run.sh shell --cluster $meta_list &>/tmp/$UID.$PID.pegasus.rolling_node.assign_delay_ms
 set_ok=`grep OK /tmp/$UID.$PID.pegasus.rolling_node.assign_delay_ms | wc -l`
 if [ $set_ok -ne 1 ]; then
   echo "ERROR: set lb.assign_delay_ms to 60min failed"
