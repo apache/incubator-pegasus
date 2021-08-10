b02cfaa8a1ade3578a0f0a61a02ab70d97625f3d
jiashuo <js982986555@live.com>
2021-08-10 19:15:18 +0800

    merge to master

--- src/server/info_collector.h
+++ src/server/info_collector.h
@@ -111,11 +111,16 @@ public:
             read_bytes->set(row_stats.get_total_read_bytes());
             write_bytes->set(row_stats.get_total_write_bytes());
 
-            int64_t total_rocksdb_block_cache_miss_hit_count = row_stats.rdb_block_cache_total_count - row_stats.rdb_block_cache_hit_count;
-            rdb_read_l2andup_hit_rate->set(convert_to_1M_ratio(row_stats.rdb_read_l2andup_hit_count, total_rocksdb_block_cache_miss_hit_count));
-            rdb_read_l1_hit_rate->set(convert_to_1M_ratio(row_stats.rdb_read_l1_hit_count, total_rocksdb_block_cache_miss_hit_count));
-            rdb_read_l0_hit_rate->set(convert_to_1M_ratio(row_stats.rdb_read_l0_hit_count,total_rocksdb_block_cache_miss_hit_count));
-            rdb_read_memtable_hit_rate->set(convert_to_1M_ratio(row_stats.rdb_read_memtable_hit_count, total_rocksdb_block_cache_miss_hit_count));
+            int64_t total_rocksdb_block_cache_miss_hit_count =
+                row_stats.rdb_block_cache_total_count - row_stats.rdb_block_cache_hit_count;
+            rdb_read_l2andup_hit_rate->set(convert_to_1M_ratio(
+                row_stats.rdb_read_l2andup_hit_count, total_rocksdb_block_cache_miss_hit_count));
+            rdb_read_l1_hit_rate->set(convert_to_1M_ratio(
+                row_stats.rdb_read_l1_hit_count, total_rocksdb_block_cache_miss_hit_count));
+            rdb_read_l0_hit_rate->set(convert_to_1M_ratio(
+                row_stats.rdb_read_l0_hit_count, total_rocksdb_block_cache_miss_hit_count));
+            rdb_read_memtable_hit_rate->set(convert_to_1M_ratio(
+                row_stats.rdb_read_memtable_hit_count, total_rocksdb_block_cache_miss_hit_count));
             rdb_write_amplification->set(row_stats.rdb_write_amplification);
             rdb_read_amplification->set(row_stats.rdb_read_amplification);
         }
