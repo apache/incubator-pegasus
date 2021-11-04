85ef9b118f23f7c901aa9aec46b11b976e0f78b1
jiashuo <js982986555@live.com>
2021-11-04 11:20:12 +0800

    fix

--- src/server/pegasus_server_write.cpp
+++ src/server/pegasus_server_write.cpp
@@ -84,6 +84,7 @@ int pegasus_server_write::on_batched_writes(dsn::message_ex **requests, int coun
         _write_svc->batch_prepare(_decree);
 
         for (int i = 0; i < count; ++i) {
+            //todo(jiashuo1)  filter timeout request
             dassert(requests[i] != nullptr, "request[%d] is null", i);
 
             // Make sure all writes are batched even if they are failed,
