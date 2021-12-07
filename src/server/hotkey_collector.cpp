bfa2ee6e1549d5850a720f96786d18aa4966b472
tang yanzhao <smityz@qq.com>
2021-12-07 17:08:42 +0800

    Improved algorithm

--- src/server/hotkey_collector.cpp
+++ src/server/hotkey_collector.cpp
@@ -84,6 +84,7 @@ find_outlier_index(const std::vector<uint64_t> &captured_keys, int threshold, in
             hot_value = captured_keys[i];
         }
     }
+    std::cout << std::endl;
     // TODO: (Tangyanzhao) increase a judgment of table_captured_key_sum
     double captured_keys_avg_count =
         (table_captured_key_sum - captured_keys[hot_index]) / (data_size - 1);
@@ -94,9 +95,16 @@ find_outlier_index(const std::vector<uint64_t> &captured_keys, int threshold, in
         }
     }
     standard_deviation = sqrt(standard_deviation / (data_size - 2));
-    if (0 == standard_deviation) {
-        return false;
+
+    // There are two cases will lead standard_deviation = 0
+    // Case 1: have hotspot [1, 1, 300, 1]
+    // Case 2: not have hotspot [1, 1, 1, 1]
+    // In both case 1 and case 2, we select [1, 1, 1] to calculate standard_deviation, so it equals
+    // to 1. In these scenes, we simply compare the size of outliers with the average
+    if (standard_deviation == 0) {
+        return hot_value > captured_keys_avg_count;
     }
+
     double hot_point = (hot_value - captured_keys_avg_count) / standard_deviation;
     if (hot_point >= threshold) {
         return true;
