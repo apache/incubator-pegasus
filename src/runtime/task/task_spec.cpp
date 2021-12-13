58e0627a510050c69379e61673df43c80a689349
Dan Wang <empiredan@126.com>
2021-12-13 11:48:49 +0800

    fix: the join point name of on_rpc_create_response (#984)

--- src/runtime/task/task_spec.cpp
+++ src/runtime/task/task_spec.cpp
@@ -165,7 +165,7 @@ task_spec::task_spec(int code,
       on_rpc_task_dropped((std::string(name) + std::string(".dropped")).c_str()),
       on_rpc_reply((std::string(name) + std::string(".rpc.reply")).c_str()),
       on_rpc_response_enqueue((std::string(name) + std::string(".rpc.response.enqueue")).c_str()),
-      on_rpc_create_response((std::string(name) + std::string("rpc.create.response")).c_str())
+      on_rpc_create_response((std::string(name) + std::string(".rpc.response.create")).c_str())
 {
     dassert(strlen(name) < DSN_MAX_TASK_CODE_NAME_LENGTH,
             "task code name '%s' is too long: length must be smaller than "
