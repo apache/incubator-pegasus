fd653155850851c5175aaf0baaee1e0daa5d571a
GiantKing <giantkingww@163.com>
2022-01-11 15:15:03 +0800

    Fix code review issues

--- src/shell/main.cpp
+++ src/shell/main.cpp
@@ -494,10 +494,7 @@ static command_executor commands[] = {
         detect_hotkey,
     },
     {
-        "unlock_meta_op_status",
-        "set op_status of meta to FREE",
-        "",
-        unlock_meta_op_status,
+        "unlock_meta_op_status", "set op_status of meta to FREE", "", unlock_meta_op_status,
     },
     {
         "exit", "exit shell", "", exit_shell,
