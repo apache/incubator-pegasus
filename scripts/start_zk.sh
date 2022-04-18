45ba13cb1285f5fe86932cf7d3804e2ae4c3f1cc
liguohao <948193394@qq.com>
2022-04-18 22:11:40 +0800

    feat: zookeeper client support kerberos

--- scripts/start_zk.sh
+++ scripts/start_zk.sh
@@ -40,32 +40,43 @@ fi
 
 cd "$INSTALL_DIR" || exit
 
-if [ ! -f zookeeper-3.4.6.tar.gz ]; then
+ZOOKEEPER_ROOT=apache-zookeeper-3.7.0-bin
+ZOOKEEPER_TAR_NAME=${ZOOKEEPER_ROOT}.tar.gz
+ZOOKEEPER_TAR_MD5_VALUE="8ffa97e7e6b0b2cf1d022e5156a7561a"
+
+if [ ! -f $ZOOKEEPER_TAR_NAME ]; then
     echo "Downloading zookeeper..."
-    download_url="https://github.com/XiaoMi/pegasus-common/releases/download/deps/zookeeper-3.4.6.tar.gz"
+    download_url="http://pegasus-thirdparty-package.oss-cn-beijing.aliyuncs.com/apache-zookeeper-3.7.0-bin.tar.gz"
     if ! wget -T 5 -t 1 $download_url; then
         echo "ERROR: download zookeeper failed"
         exit 1
     fi
+    if [ `md5sum $ZOOKEEPER_TAR_NAME | awk '{print$1}'` != $ZOOKEEPER_TAR_MD5_VALUE ]; then
+        echo "check file $ZOOKEEPER_TAR_NAME md5sum failed!"
+        exit 1
+    fi
 fi
 
-if [ ! -d zookeeper-3.4.6 ]; then
+if [ ! -d $ZOOKEEPER_ROOT ]; then
     echo "Decompressing zookeeper..."
-    if ! tar xf zookeeper-3.4.6.tar.gz; then
+    if ! tar xf $ZOOKEEPER_TAR_NAME; then
         echo "ERROR: decompress zookeeper failed"
         exit 1
     fi
 fi
 
-ZOOKEEPER_HOME=$(pwd)/zookeeper-3.4.6
+ZOOKEEPER_HOME=`pwd`/$ZOOKEEPER_ROOT
 ZOOKEEPER_PORT=$PORT
 
-cp "$ZOOKEEPER_HOME"/conf/zoo_sample.cfg "$ZOOKEEPER_HOME"/conf/zoo.cfg
-sed -i "s@dataDir=/tmp/zookeeper@dataDir=$ZOOKEEPER_HOME/data@" "$ZOOKEEPER_HOME"/conf/zoo.cfg
-sed -i "s@clientPort=2181@clientPort=$ZOOKEEPER_PORT@" "$ZOOKEEPER_HOME"/conf/zoo.cfg
+cp $ZOOKEEPER_HOME/conf/zoo_sample.cfg $ZOOKEEPER_HOME/conf/zoo.cfg
+sed -i "s@dataDir=/tmp/zookeeper@dataDir=$ZOOKEEPER_HOME/data@" $ZOOKEEPER_HOME/conf/zoo.cfg
+sed -i "s@clientPort=2181@clientPort=$ZOOKEEPER_PORT@" $ZOOKEEPER_HOME/conf/zoo.cfg
+echo "admin.enableServer=false" >> $ZOOKEEPER_HOME/conf/zoo.cfg
+echo "4lw.commands.whitelist=ruok" >> $ZOOKEEPER_HOME/conf/zoo.cfg
 
-mkdir -p "$ZOOKEEPER_HOME"/data
-"$ZOOKEEPER_HOME"/bin/zkServer.sh start
+mkdir -p $ZOOKEEPER_HOME/data
+$ZOOKEEPER_HOME/bin/zkServer.sh start
+sleep 1
 
 zk_check_count=0
 while true; do
