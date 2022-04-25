acb6d13f7273b72c587871b28599cac2ff57c692
jiashuo <js982986555@live.com>
2022-04-25 16:14:57 +0800

    update

--- scripts/pegasus_command_version.sh
+++ scripts/pegasus_command_version.sh
@@ -45,85 +45,28 @@ if [ "$version" = "" ]; then
   echo "current cluster version is $version"
 fi
 
-# list all remote command, default `server-info` that is supported by all version to avoid some version set no supported command and return error
-meta_lb_assign_secondary_black_list="server-info"
-meta_live_percentage="server-info"
-meta_lb_assign_delay_ms="server-info"
-meta_lb_only_move_primary="server-info"
-meta_lb_add_secondary_max_count_for_one_node="server-info"
+meta_lb_assign_secondary_black_list="meta.lb.assign_secondary_black_list"
+meta_live_percentage="meta.live_percentage"
+meta_lb_assign_delay_ms="meta.lb.assign_delay_ms"
+meta_lb_only_move_primary="meta.lb.only_move_primary"
+meta_lb_add_secondary_max_count_for_one_node="meta.lb.add_secondary_max_count_for_one_node"
 
-replica_kill_partition="server-info"
-replica_query_app_envs="server-info"
-replica_query_compact="server-info"
-replica_query_compact="server-info"
+replica_kill_partition="replica.kill_partition"
+replica_query_app_envs="replica.query-app-envs"
+replica_query_compact="replica.query-compact"
+replica_query_compact="replica.trigger-checkpoint"
 
-nfs_max_copy_rate_megabytes="server-info"
-nfs_max_send_rate_megabytes="server-info"
+# default `server-info` that is supported by all version to avoid some version set no supported command and return error
+nfs_max_copy_rate_megabytes=""
+nfs_max_send_rate_megabytes=""
 
 # shellcheck disable=SC2076
-if [[ ($version =~ "1.1.6") || ($version =~ "1.12.2") || ($version =~ "1.12.3") || ($version =~ "2.0.0") || ($version =~ "2.0-write-optm") ]];then
-  meta_lb_assign_secondary_black_list="meta.lb.assign_secondary_black_list"
-  meta_live_percentage="meta.live_percentage"
-  meta_lb_assign_delay_ms="meta.lb.assign_delay_ms"
-  meta_lb_only_move_primary="meta.lb.only_move_primary"
-  meta_lb_add_secondary_max_count_for_one_node="meta.lb.add_secondary_max_count_for_one_node"
-
-  replica_kill_partition="replica.kill_partition"
-  replica_query_app_envs="replica.query-app-envs"
-  replica_query_compact="replica.query-compact"
-  replica_query_compact="replica.trigger-checkpoint"
-elif [[ ($version =~ "2.1.1") ]];then
-  meta_lb_assign_secondary_black_list="lb.assign_secondary_black_list"
-  meta_live_percentage="live_percentage"
-  meta_lb_assign_delay_ms="lb.assign_delay_ms"
-  meta_lb_only_move_primary="lb.only_move_primary"
-  meta_lb_add_secondary_max_count_for_one_node="lb.add_secondary_max_count_for_one_node"
-
-  replica_kill_partition="kill_partition"
-  replica_query_app_envs="query-app-envs"
-  replica_query_compact="query-compact"
-  replica_query_compact="trigger-checkpoint"
-
-  nfs_max_copy_rate_megabytes="nfs.max_copy_rate_megabytes"
-elif [[ ($version =~ "2.2.1") || ($version =~ "2.2.2") || ($version =~ "2.2.3") ]];then
-  meta_lb_assign_secondary_black_list="meta.lb.assign_secondary_black_list"
-  meta_live_percentage="meta.live_percentage"
-  meta_lb_assign_delay_ms="meta.lb.assign_delay_ms"
-  meta_lb_only_move_primary="meta.lb.only_move_primary"
-  meta_lb_add_secondary_max_count_for_one_node="meta.lb.add_secondary_max_count_for_one_node"
-
-  replica_kill_partition="replica.kill_partition"
-  replica_query_app_envs="replica.query-app-envs"
-  replica_query_compact="replica.query-compact"
-  replica_query_compact="replica.trigger-checkpoint"
-
+if [[ ($version =~ "2.1.1") || ($version =~ "2.2.1") || ($version =~ "2.2.2") || ($version =~ "2.2.3") ]];then
   nfs_max_copy_rate_megabytes="nfs.max_copy_rate_megabytes"
 elif [[ ($version =~ "2.3.0") || ($version =~ "2.3.1") || ($version =~ "2.3.2") || ($version =~ "2.3.3")]];then
-  meta_lb_assign_secondary_black_list="meta.lb.assign_secondary_black_list"
-  meta_live_percentage="meta.live_percentage"
-  meta_lb_assign_delay_ms="meta.lb.assign_delay_ms"
-  meta_lb_only_move_primary="meta.lb.only_move_primary"
-  meta_lb_add_secondary_max_count_for_one_node="meta.lb.add_secondary_max_count_for_one_node"
-
-  replica_kill_partition="replica.kill_partition"
-  replica_query_app_envs="replica.query-app-envs"
-  replica_query_compact="replica.query-compact"
-  replica_query_compact="replica.trigger-checkpoint"
-
   nfs_max_copy_rate_megabytes="nfs.max_copy_rate_megabytes"
   nfs_max_send_rate_megabytes="nfs.max_send_rate_megabytes"
 else
-  meta_lb_assign_secondary_black_list="meta.lb.assign_secondary_black_list"
-  meta_live_percentage="meta.live_percentage"
-  meta_lb_assign_delay_ms="meta.lb.assign_delay_ms"
-  meta_lb_only_move_primary="meta.lb.only_move_primary"
-  meta_lb_add_secondary_max_count_for_one_node="meta.lb.add_secondary_max_count_for_one_node"
-
-  replica_kill_partition="replica.kill_partition"
-  replica_query_app_envs="replica.query-app-envs"
-  replica_query_compact="replica.query-compact"
-  replica_query_compact="replica.trigger-checkpoint"
-
   nfs_max_copy_rate_megabytes="nfs.max_copy_rate_megabytes_per_disk"
   nfs_max_send_rate_megabytes="nfs.max_send_rate_megabytes_per_disk"
 fi
