007452b5ae518007bfd1414edd0c612b30ca731e
jiashuo <js982986555@live.com>
2022-05-12 18:20:14 +0800

    update

---
+++ admin-cli/cmd/nodes_balancer.go
@@ -0,0 +1,21 @@
+package cmd
+
+import (
+	"github.com/apache/incubator-pegasus/admin-cli/executor/toolkits/nodesbalancer"
+	"github.com/apache/incubator-pegasus/admin-cli/shell"
+	"github.com/desertbit/grumble"
+)
+
+func init() {
+	shell.AddCommand(&grumble.Command{
+		Name: "nodes-balancer",
+		Help: "balance nodes capacity",
+		Flags: func(a *grumble.Flags) {
+			a.BoolL("auto", false, "whether to migrate replica until all nodes is balanced")
+		},
+		Run: func(c *grumble.Context) error {
+			auto := c.Flags.Bool("auto")
+			return nodesbalancer.BalanceNodeCapacity(pegasusClient, auto)
+		},
+	})
+}
