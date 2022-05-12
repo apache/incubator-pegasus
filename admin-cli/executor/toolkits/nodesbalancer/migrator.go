62a1fb081a4eed958915658efff91cb5deddf332
jiashuo <js982986555@live.com>
2022-05-12 12:30:17 +0800

    update

--- admin-cli/executor/toolkits/nodesbalancer/migrator.go
+++ admin-cli/executor/toolkits/nodesbalancer/migrator.go
@@ -1,11 +1,16 @@
 package nodesbalancer
 
 import (
+	"fmt"
+	"github.com/XiaoMi/pegasus-go-client/idl/base"
+	"github.com/apache/incubator-pegasus/admin-cli/client"
+	migrator "github.com/apache/incubator-pegasus/admin-cli/client"
 	"github.com/apache/incubator-pegasus/admin-cli/executor"
+	"github.com/apache/incubator-pegasus/admin-cli/executor/toolkits"
 	"github.com/apache/incubator-pegasus/admin-cli/executor/toolkits/diskbalancer"
 	"github.com/apache/incubator-pegasus/admin-cli/util"
-	"golang.org/x/text/date"
 	"math"
+	"time"
 )
 
 type NodesCapacity struct {
@@ -16,29 +21,43 @@ type NodesCapacity struct {
 	Available int64
 }
 
+type NodesReplica struct {
+	Node     *util.PegasusNode
+	Replicas []*executor.ReplicaCapacityStruct
+}
+
 type Migrator struct {
-	CapacityLoad []*NodesCapacity
-	Total int64
-	Average int64
+	CapacityLoad  []*NodesCapacity
+	Total         int64
+	Average       int64
 	BalancedCount int64
 }
 
 func BalanceNodeCapacity(client *executor.Client, auto bool) error {
-	migrater := &Migrator{
-
-	}
+	migrater := &Migrator{}
 	for {
-		err, completed := migrater.updateNodesCapacityLoad(client)
+		err := migrater.updateNodesLoad(client)
 		if err != nil {
-			return err
+			toolkits.LogInfo("retry update load")
+			time.Sleep(time.Second * 10)
+			continue
 		}
-		if completed || auto {
-			return nil
+
+		err, action := migrater.selectNextAction(client)
+		err = client.Meta.Balance(action.replica.Gpid, action.replica.Status, action.from.Node, action.to.Node)
+		if err != nil {
+			return fmt.Errorf("migrate action[%s] now is invalid: %s", action.toString(), err.Error())
 		}
+		checkCompleted(client, action)
+		if auto {
+			break
+		}
+		time.Sleep(time.Second * 10)
 	}
+	return nil
 }
 
-func (m *Migrator) updateNodesCapacityLoad(client *executor.Client) (error, bool) {
+func (m *Migrator) updateNodesLoad(client *executor.Client) error {
 	nodes, err := client.Meta.ListNodes()
 	if err != nil {
 		return err
@@ -72,7 +91,7 @@ func (m *Migrator) updateNodesCapacityLoad(client *executor.Client) (error, bool
 	m.Average = m.Total / int64(len(nodesLoad))
 
 	for _, node := range m.CapacityLoad {
-		if int64(math.Abs(float64(node.Usage - m.Average))) * 100 / m.Total <= 5 {
+		if int64(math.Abs(float64(node.Usage-m.Average)))*100/m.Total <= 5 {
 			m.BalancedCount++
 		}
 	}
@@ -80,68 +99,100 @@ func (m *Migrator) updateNodesCapacityLoad(client *executor.Client) (error, bool
 	return nil
 }
 
-type ActionProposal struct {
-	replica executor.ReplicaCapacityStruct
-	from *util.PegasusNode
-	to *util.PegasusNode
+type partition struct {
+	Gpid   *base.Gpid
+	Status client.BalanceType
+	Size   int64
 }
 
-func (m *Migrator) selectNextAction(client *executor.Client) (error, *ActionProposal){
-	highNode := m.CapacityLoad[len(m.CapacityLoad) - 1]
+type ActionProposal struct {
+	replica *partition
+	from    *NodesCapacity
+	to      *NodesCapacity
+}
+
+func (act *ActionProposal) toString() string {
+	return fmt.Sprintf("[%s]%s:%s=>%s", act.replica.Status.String(), act.replica.Gpid.String(),
+		act.from.Node.String(), act.to.Node.String())
+}
+
+func (m *Migrator) selectNextAction(client *executor.Client) (error, *ActionProposal) {
+	highNode := m.CapacityLoad[len(m.CapacityLoad)-1]
 	lowNode := m.CapacityLoad[0]
 
 	averageCapacity := m.Total / int64(len(m.CapacityLoad))
-	sizeCanMoved := math.Min(float64(highNode.Total - averageCapacity), float64(averageCapacity - lowNode.Total))
-	if (lowNode.Available * 100 / lowNode.Total) - (highNode.Available * 100 / highNode.Total) <= 5 {
-		return nil,nil
-	}
-
-	highDisk := highNode.Disks[len(highNode.Disks) - 1]
-	highDiskInfo, err := executor.GetDiskInfo(client, executor.CapacitySize, highNode.Node.TCPAddr(), "", highDisk.Disk, false)
-	if err != nil {
-		return nil, nil
-	}
-	replicas, err := executor.ConvertReplicaCapacityStruct(highDiskInfo)
-	if err != nil {
+	sizeAllowMoved := math.Min(float64(highNode.Total-averageCapacity), float64(averageCapacity-lowNode.Total))
+	if (lowNode.Available*100/lowNode.Total)-(highNode.Available*100/highNode.Total) <= 5 {
 		return nil, nil
 	}
 
-	targetNodeReplicas, err := getNodeReplica(client, lowNode)
+	highDisk := highNode.Disks[len(highNode.Disks)-1]
+	originNodeHighDiskReplicas, err := getDiskReplicas(client, highNode, highDisk.Disk)
+	if err != nil {
+		return err, nil
+	}
+
+	targetNodeTotalReplicas, err := getNodeReplicas(client, lowNode)
 	if err != nil {
 		return nil, nil
 	}
 
 	var selectReplica executor.ReplicaCapacityStruct
-	for _, replica := range replicas {
-		if replica.Size > int64(sizeCanMoved) {
+	for _, replica := range originNodeHighDiskReplicas {
+		if replica.Size > int64(sizeAllowMoved) {
 			continue
 		}
 
-		if targetNodeReplicas.contain(replica) {
+		if targetNodeTotalReplicas.contain(replica.Gpid) {
 			continue
+		}
+
+		if selectReplica.Status == "primary" {
+			return fmt.Errorf("please downgrade primary"), nil
 		}
 
 		selectReplica = replica
 	}
 
+	gpid, err := util.Str2Gpid(selectReplica.Gpid)
+	if err != nil {
+		return err, nil
+	}
 	return nil, &ActionProposal{
-		replica: selectReplica,
-		from: highNode.Node,
-		to: lowNode.Node,
+		replica: &partition{
+			Gpid:   gpid,
+			Status: migrator.BalanceCopySec,
+		},
+		from: highNode,
+		to:   lowNode,
 	}
 }
 
 type replicas []executor.ReplicaCapacityStruct
-func (r replicas) contain(selectReplica executor.ReplicaCapacityStruct) bool {
+
+func (r replicas) contain(selectReplica string) bool {
 	for _, replica := range r {
-		if replica.Gpid == selectReplica.Gpid {
+		if replica.Gpid == selectReplica {
 			return true
 		}
 	}
 	return false
 }
 
-func getNodeReplica(client *executor.Client, replicaServer *NodesCapacity) (replicas, error) {
+func getDiskReplicas(client *executor.Client, replicaServer *NodesCapacity, diskTag string) (replicas, error) {
+	node := replicaServer.Node.TCPAddr()
+	diskInfo, err := executor.GetDiskInfo(client, executor.CapacitySize, node, "", diskTag, false)
+	if err != nil {
+		return nil, err
+	}
+	replicas, err := executor.ConvertReplicaCapacityStruct(diskInfo)
+	if err != nil {
+		return nil, err
+	}
+	return replicas, nil
+}
+
+func getNodeReplicas(client *executor.Client, replicaServer *NodesCapacity) (replicas, error) {
 	node := replicaServer.Node.TCPAddr()
 
 	var totalDiskInfo []interface{}
@@ -160,4 +211,21 @@ func getNodeReplica(client *executor.Client, replicaServer *NodesCapacity) (repl
 	return replicas, nil
 }
 
+func checkCompleted(client *executor.Client, action *ActionProposal) {
+	for {
+		replicas, err := getNodeReplicas(client, action.to)
+		if err != nil {
+			toolkits.LogInfo(err.Error())
+			time.Sleep(time.Second * 10)
+			continue
+		}
 
+		if !replicas.contain(action.replica.Gpid.String()) {
+			toolkits.LogInfo(fmt.Sprintf("%s is running", action.toString()))
+			time.Sleep(time.Second * 10)
+			continue
+		}
+		break
+	}
+
+}
