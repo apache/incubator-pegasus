0cba7eb227f064dcfcc92d0d8a5cfd4acfaf7858
Yingchun Lai <acelyc1112009@gmail.com>
2022-06-13 20:16:35 +0800

    Revert "chore: Add license header check action (#1111)" (#1117)

--- include/dsn/utility/TokenBucket.h
+++ include/dsn/utility/TokenBucket.h
@@ -14,6 +14,10 @@
  * limitations under the License.
  */
 
+// Copyright (c) 2017, Xiaomi, Inc.  All rights reserved.
+// This source code is licensed under the Apache License Version 2.0, which
+// can be found in the LICENSE file in the root directory of this source tree.
+
 #pragma once
 
 #include <algorithm>
--- include/dsn/utility/filesystem.h
+++ include/dsn/utility/filesystem.h
@@ -24,6 +24,10 @@
  * THE SOFTWARE.
  */
 
+// Copyright (c) 2017-present, Xiaomi, Inc.  All rights reserved.
+// This source code is licensed under the Apache License Version 2.0, which
+// can be found in the LICENSE file in the root directory of this source tree.
+
 #pragma once
 
 #include <string>
