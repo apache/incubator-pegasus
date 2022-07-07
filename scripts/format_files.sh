a4dd5d327e4deef247b801493b2a08333b1af9d3
Yingchun Lai <laiyingchun@apache.org>
2022-07-07 10:41:49 +0800

    chore(clang-format): unify rdsn and Pegasus clang-format (#1022)

--- scripts/format_files.sh
+++ scripts/format_files.sh
@@ -25,14 +25,14 @@ sds=./src/shell/sds
 
 if [ $# -eq 0 ]; then
   echo "formating all .h/.cpp files in $root_dir ..."
-  find . -type f -not \( -wholename "$linenoise/*" -o -wholename "$sds/*" -o -wholename "./rocksdb/*" -o -wholename "./rdsn/*" \) \
+  find . -type f -not \( -wholename "$linenoise/*" -o -wholename "$sds/*" \) \
       -regextype posix-egrep -regex ".*\.(cpp|h)" | xargs clang-format-3.9 -i -style=file
 elif [ $1 = "-h" ]; then
   echo "USAGE: ./format-files.sh [<relative_path>] -- format .h/.cpp files in $root_dir/relative_path"
   echo "       ./format-files.sh means format all .h/.cpp files in $root_dir"
 else
   echo "formating all .h/.cpp files in $root_dir/$1 ..."
-  find ./$1 -type f -not \( -wholename "$linenoise/*" -o -wholename "$sds/*" -o -wholename "./rocksdb/*" -o -wholename "./rdsn/*" \) \
+  find ./$1 -type f -not \( -wholename "$linenoise/*" -o -wholename "$sds/*" \) \
       -regextype posix-egrep -regex ".*\.(cpp|h)" | xargs clang-format-3.9 -i -style=file
 fi
 
