5d30aae3725c289b7aa920367533a5a5728c327b
Yingchun Lai <laiyingchun@apache.org>
2022-07-27 15:47:28 +0800

    rm

--- get_local_ip.py
+++
@@ -1,9 +0,0 @@
-import socket
-
-def get_host_ip():
-    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-        s.connect(('8.8.8.8', 80))
-        return s.getsockname()[0]
-
-
-print(get_host_ip())
