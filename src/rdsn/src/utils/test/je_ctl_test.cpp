1cea1d9bc1318af4b46f2ee5f525489dfb99de4e
Dan Wang <wangdan@apache.org>
2022-08-22 12:24:54 +0800

    feat: support to dump jemalloc stats

--- src/rdsn/src/utils/test/je_ctl_test.cpp
+++ src/rdsn/src/utils/test/je_ctl_test.cpp
@@ -25,10 +25,38 @@ namespace dsn {
 
 namespace {
 
-void check_stats_marks(const std::string &stats)
+// This function does not check "End" mark, since sometimes returned message is very long while
+// the buffer may not have enough space: the message will be truncated. If it is known that the
+// message can be held by the buffer completely, `check_base_stats_marks_with_end` should be used
+// instead.
+void check_base_stats_marks(const std::string &stats)
 {
     ASSERT_NE(stats.find("Begin jemalloc statistics"), std::string::npos);
+    ASSERT_NE(stats.find("Allocated:"), std::string::npos);
+    ASSERT_NE(stats.find("Background threads:"), std::string::npos);
+}
+
+void check_base_stats_marks_with_end(const std::string &stats)
+{
+    check_base_stats_marks(stats);
     ASSERT_NE(stats.find("End jemalloc statistics"), std::string::npos);
+}
+
+void check_configs_marks(const std::string &stats)
+{
+    ASSERT_NE(stats.find("Version:"), std::string::npos);
+    ASSERT_NE(stats.find("Build-time option settings"), std::string::npos);
+    ASSERT_NE(stats.find("Run-time option settings"), std::string::npos);
+    ASSERT_NE(stats.find("Profiling settings"), std::string::npos);
+}
+
+void check_arena_marks(const std::string &stats)
+{
+    // Marks for merged arenas.
+    ASSERT_NE(stats.find("Merged arenas stats:"), std::string::npos);
+
+    // Marks for each arena.
+    ASSERT_NE(stats.find("arenas[0]:"), std::string::npos);
 }
 
 } // anonymous namespace
@@ -38,7 +66,7 @@ TEST(je_ctl_test, dump_summary_stats)
     std::string stats;
     je_dump_stats(je_stats_type::SUMMARY_STATS, stats);
 
-    check_stats_marks(stats);
+    check_base_stats_marks_with_end(stats);
 }
 
 TEST(je_ctl_test, dump_configs)
@@ -46,10 +74,8 @@ TEST(je_ctl_test, dump_configs)
     std::string stats;
     je_dump_stats(je_stats_type::CONFIGS, stats);
 
-    check_stats_marks(stats);
-    ASSERT_NE(stats.find("Build-time option settings"), std::string::npos);
-    ASSERT_NE(stats.find("Run-time option settings"), std::string::npos);
-    ASSERT_NE(stats.find("Profiling settings"), std::string::npos);
+    check_base_stats_marks_with_end(stats);
+    check_configs_marks(stats);
 }
 
 TEST(je_ctl_test, dump_brief_arena_stats)
@@ -57,10 +83,24 @@ TEST(je_ctl_test, dump_brief_arena_stats)
     std::string stats;
     je_dump_stats(je_stats_type::BRIEF_ARENA_STATS, stats);
 
-    check_stats_marks(stats);
-    ASSERT_NE(stats.find("arenas[0]:"), std::string::npos);
-    ASSERT_NE(stats.find("assigned threads:"), std::string::npos);
-    ASSERT_NE(stats.find("decaying:"), std::string::npos);
+    // Since there may be many arenas, "End" mark is not required to be checked here.
+    check_base_stats_marks(stats);
+    check_arena_marks(stats);
+}
+
+TEST(je_ctl_test, dump_detailed_stats)
+{
+    std::string stats;
+    je_dump_stats(je_stats_type::DETAILED_STATS, stats);
+
+    // Since there may be many arenas, "End" mark is not required to be checked here.
+    check_base_stats_marks(stats);
+
+    // Detailed stats will contain all information, therefore everything should be checked.
+    check_configs_marks(stats);
+    check_arena_marks(stats);
+    ASSERT_NE(stats.find("bins:"), std::string::npos);
+    ASSERT_NE(stats.find("extents:"), std::string::npos);
 }
 
 } // namespace dsn
