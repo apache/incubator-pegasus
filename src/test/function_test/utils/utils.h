c4737a714fb4a99d574dded8185bf688d3e6da1b
Yingchun Lai <acelyc1112009@gmail.com>
2022-09-07 22:01:34 +0800

    Update src/test/function_test/utils/utils.h
    Co-authored-by: Dan Wang <wangdan@apache.org>

--- src/test/function_test/utils/utils.h
+++ src/test/function_test/utils/utils.h
@@ -157,7 +157,7 @@ inline void compare(const std::map<std::string, std::pair<std::string, uint32_t>
             << "Only in actual: hash_key=" << hash_key << ", sort_key=" << it1->first
             << ", value=" << it1->second.first << ", expire_ts_seconds=" << it1->second.second;
         ASSERT_EQ(it1->first, it2->first)
-            << "Diff sort_key: hash_key=" << hash_key << ", data_sort_key=" << it1->first
+            << "Diff sort_key: hash_key=" << hash_key << ", actual_sort_key=" << it1->first
             << ", data_value=" << it1->second.first
             << ", data_expire_ts_seconds=" << it1->second.second << ", base_sort_key=" << it2->first
             << ", base_value=" << it2->second.first
