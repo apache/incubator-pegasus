fe0e0284daab6afc544fa21e06ef95bb9291f88c
liguohao <48315319+GehaFearless@users.noreply.github.com>
2023-04-11 14:44:30 +0800

    feat(FQDN): Implement of struct rpc_host_port (#1430)
    https://github.com/apache/incubator-pegasus/issues/1404
    
    Implement of class `rpc_host_port` which is base on class `rpc_address`.
    
    The purpose is to replace address with host_port achieve to be able to
    communicate through fqdn not only ip.

---
+++ src/runtime/test/host_port_test.cpp
@@ -0,0 +1,77 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+#include <gtest/gtest.h>
+#include <gtest/gtest-message.h>
+#include <gtest/gtest-test-part.h>
+#include <string>
+
+#include "runtime/rpc/rpc_address.h"
+#include "runtime/rpc/rpc_host_port.h"
+
+namespace dsn {
+
+TEST(host_port_test, host_port_to_string)
+{
+    {
+        host_port hp = host_port("localhost", 8080);
+        ASSERT_EQ(std::string("localhost:8080"), hp.to_string());
+    }
+
+    {
+        host_port hp;
+        ASSERT_EQ(std::string("invalid address"), hp.to_string());
+    }
+}
+
+TEST(host_port_test, host_port_build)
+{
+    host_port hp = host_port("localhost", 8080);
+    ASSERT_EQ(HOST_TYPE_IPV4, hp.type());
+    ASSERT_EQ(8080, hp.port());
+    ASSERT_EQ("localhost", hp.host());
+
+    {
+        rpc_address addr = rpc_address("localhost", 8080);
+        host_port hp1(addr);
+        ASSERT_EQ(hp, hp1);
+    }
+}
+
+TEST(host_port_test, operators)
+{
+    host_port hp("localhost", 8080);
+    ASSERT_EQ(hp, hp);
+
+    {
+        host_port new_hp(hp);
+        ASSERT_EQ(hp, new_hp);
+    }
+
+    {
+        host_port new_hp("localhost", 8081);
+        ASSERT_NE(hp, new_hp);
+    }
+
+    host_port hp2;
+    ASSERT_NE(hp, hp2);
+    ASSERT_FALSE(hp.is_invalid());
+    ASSERT_TRUE(hp2.is_invalid());
+}
+} // namespace dsn
