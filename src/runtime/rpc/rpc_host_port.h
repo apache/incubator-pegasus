cf997de7fb010a183c941670e6700ad52dc73b40
WHBANG <38547944+WHBANG@users.noreply.github.com>
2023-04-13 00:16:00 +0800

    fix(IWYU): fix include errors for rpc_host_port reported by IWYU (#1434)
    https://github.com/apache/incubator-pegasus/issues/1349
    
    Fix IWYU issues in rpc_host_port.*

--- src/runtime/rpc/rpc_host_port.h
+++ src/runtime/rpc/rpc_host_port.h
@@ -19,9 +19,10 @@
 
 #pragma once
 
-// IWYU pragma: no_include <experimental/string_view>
-#include <stdint.h>
 #include <cstddef>
+#include <cstdint>
+// IWYU pragma: no_include <experimental/string_view>
+#include <functional>
 #include <iosfwd>
 #include <string>
 
