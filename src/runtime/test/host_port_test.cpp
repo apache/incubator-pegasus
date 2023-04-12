6277993f54d73c5002bd95dc81d62a54bb34ff8d
liguohao <948193394@qq.com>
2023-04-12 20:09:51 +0800

    add test

--- src/runtime/test/host_port_test.cpp
+++ src/runtime/test/host_port_test.cpp
@@ -22,6 +22,7 @@
 #include <gtest/gtest-test-part.h>
 #include <string>
 
+#include "runtime/rpc/group_host_port.h"
 #include "runtime/rpc/rpc_address.h"
 #include "runtime/rpc/rpc_host_port.h"
 
@@ -74,4 +75,72 @@ TEST(host_port_test, operators)
     ASSERT_FALSE(hp.is_invalid());
     ASSERT_TRUE(hp2.is_invalid());
 }
+
+TEST(host_port_test, rpc_group_host_port)
+{
+    host_port hp("localhost", 8080);
+    host_port hp2("localhost", 8081);
+    host_port invalid_hp;
+
+    host_port hp_grp;
+    hp_grp.assign_group("test_group");
+    ASSERT_EQ(HOST_TYPE_GROUP, hp_grp.type());
+    rpc_group_host_port *g = hp_grp.group_host_port();
+    ASSERT_EQ(std::string("test_group"), g->name());
+    ASSERT_EQ(1, g->get_count());
+
+    // invalid_hp
+    ASSERT_FALSE(g->remove(hp));
+    ASSERT_FALSE(g->contains(hp));
+    ASSERT_EQ(0u, g->members().size());
+    ASSERT_EQ(invalid_hp, g->random_member());
+    ASSERT_EQ(invalid_hp, g->next(hp));
+    ASSERT_EQ(invalid_hp, g->leader());
+    ASSERT_EQ(invalid_hp, g->possible_leader());
+
+    // hp
+    g->set_leader(hp);
+    ASSERT_TRUE(g->contains(hp));
+    ASSERT_EQ(1u, g->members().size());
+    ASSERT_EQ(hp, g->members().at(0));
+    ASSERT_EQ(hp, g->leader());
+    ASSERT_EQ(hp, g->possible_leader());
+
+    // hp2
+    g->set_leader(hp2);
+    ASSERT_TRUE(g->contains(hp));
+    ASSERT_TRUE(g->contains(hp2));
+    ASSERT_EQ(2u, g->members().size());
+    ASSERT_EQ(hp, g->members().at(0));
+    ASSERT_EQ(hp2, g->members().at(1));
+    ASSERT_EQ(hp2, g->leader());
+    ASSERT_EQ(hp2, g->possible_leader());
+    ASSERT_EQ(hp, g->next(hp2));
+    ASSERT_EQ(hp2, g->next(hp));
+
+    // change leader
+    g->set_leader(hp);
+    ASSERT_TRUE(g->contains(hp));
+    ASSERT_TRUE(g->contains(hp2));
+    ASSERT_EQ(2u, g->members().size());
+    ASSERT_EQ(hp, g->members().at(0));
+    ASSERT_EQ(hp2, g->members().at(1));
+    ASSERT_EQ(hp, g->leader());
+    g->leader_forward();
+    ASSERT_EQ(hp2, g->leader());
+
+    // del
+    ASSERT_TRUE(g->remove(hp));
+    ASSERT_FALSE(g->contains(hp));
+    ASSERT_TRUE(g->contains(hp2));
+    ASSERT_EQ(1u, g->members().size());
+    ASSERT_EQ(hp2, g->members().at(0));
+    ASSERT_EQ(invalid_hp, g->leader());
+
+    ASSERT_TRUE(g->remove(hp2));
+    ASSERT_FALSE(g->contains(hp2));
+    ASSERT_EQ(0u, g->members().size());
+    ASSERT_EQ(invalid_hp, g->leader());
+}
+
 } // namespace dsn
