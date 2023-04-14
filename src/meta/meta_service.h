fb5b5b017b864d79525e90d9da610a9c033c647b
Dan Wang <wangdan@apache.org>
2023-04-14 20:08:47 +0800

    feat(new_metrics): migrate server-level metrics for meta_service (#1437)
    https://github.com/apache/incubator-pegasus/issues/1331
    
    Migrate metrics to new framework for meta_service, including the number
    of disconnections with replica servers, and the number of unalive and alive
    replica servers. All of these metrics are server-level, maintained in meta
    server.
    
    The old type in perf counters of the number of disconnections is volatile
    counter, which would be changed to non-volatile, while another 2 metrics
    would keep the type of gauge.

--- src/meta/meta_service.h
+++ src/meta/meta_service.h
@@ -46,7 +46,6 @@
 #include "meta_options.h"
 #include "meta_rpc_types.h"
 #include "meta_server_failure_detector.h"
-#include "perf_counter/perf_counter_wrapper.h"
 #include "runtime/api_layer1.h"
 #include "runtime/rpc/network.h"
 #include "runtime/rpc/rpc_address.h"
@@ -61,6 +60,7 @@
 #include "utils/enum_helper.h"
 #include "utils/error_code.h"
 #include "utils/fmt_logging.h"
+#include "utils/metrics.h"
 #include "utils/threadpool_code.h"
 #include "utils/zlocks.h"
 
@@ -375,9 +375,9 @@ private:
 
     std::string _cluster_root;
 
-    perf_counter_wrapper _recent_disconnect_count;
-    perf_counter_wrapper _unalive_nodes_count;
-    perf_counter_wrapper _alive_nodes_count;
+    METRIC_VAR_DECLARE_counter(replica_server_disconnections);
+    METRIC_VAR_DECLARE_gauge_int64(unalive_replica_servers);
+    METRIC_VAR_DECLARE_gauge_int64(alive_replica_servers);
 
     dsn::task_tracker _tracker;
 
