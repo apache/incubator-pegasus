7d26c9da42f899e3c4c1f377cbf64ffab8f673ea
Dan Wang <wangdan@apache.org>
2023-04-23 19:26:56 +0800

    fix(ddl_client): sleep for a while before retry once meta server is busy

--- src/client/test/ddl_client_test.cpp
+++ src/client/test/ddl_client_test.cpp
@@ -19,6 +19,8 @@
 // IWYU pragma: no_include <gtest/gtest-message.h>
 // IWYU pragma: no_include <gtest/gtest-test-part.h>
 #include <gtest/gtest.h>
+#include <stdint.h>
+#include <algorithm>
 #include <deque>
 #include <iosfwd>
 #include <memory>
