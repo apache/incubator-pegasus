77a0fcd8aecc4c6a757a66ca879f3750a4ce8774
Dan Wang <wangdan@apache.org>
2023-04-27 00:26:50 +0800

    feat(new_metrics): migrate metrics for replica_stub (part 3)

--- src/replica/replica_chkpt.cpp
+++ src/replica/replica_chkpt.cpp
@@ -50,8 +50,6 @@
 #include "duplication/replica_duplicator_manager.h"
 #include "metadata_types.h"
 #include "mutation_log.h"
-#include "perf_counter/perf_counter.h"
-#include "perf_counter/perf_counter_wrapper.h"
 #include "replica.h"
 #include "replica/prepare_list.h"
 #include "replica/replica_context.h"
