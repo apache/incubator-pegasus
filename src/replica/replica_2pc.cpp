6004e1e004817f124b10e2c483d2566f2fe6baf2
Dan Wang <wangdan@apache.org>
2023-04-27 11:02:54 +0800

    feat(new_metrics): migrate metrics for replica_stub (part 3) (#1462)
    https://github.com/apache/incubator-pegasus/issues/1454
    
    This is the 3rd part of migrating metrics of replica_stub to new framework.
    
    During this migration, there are 3 metrics which are changed from server-level
    to replica-level, including the number of failed RPC_PREPARE requests, the
    number of failed RPC_GROUP_CHECK requests launched by primary replicas,
    the number of triggered emergency checkpoints.
    
    Another 7 metrics are still kept server-level, the number of replicas whose
    dirs are moved as error or garbage, the number of removed replica dirs, error
    replica dirs (*.err), garbage replica dirs (*.gar), tmp replica dirs (*.tmp) and origin
    replica dirs (*.ori) for disk migration.
    
    There are 2 metrics removed, since both are shared-log-related.

--- src/replica/replica_2pc.cpp
+++ src/replica/replica_2pc.cpp
@@ -765,7 +765,7 @@ void replica::on_prepare_reply(std::pair<mutation_ptr, partition_status::type> p
             }
         }
 
-        _stub->_counter_replicas_recent_prepare_fail_count->increment();
+        METRIC_VAR_INCREMENT(prepare_failed_requests);
 
         // make sure this is before any later commit ops
         // because now commit ops may lead to new prepare ops
