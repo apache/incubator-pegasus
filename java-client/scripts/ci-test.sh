9d295b07967fb6fae5a80366854f9404bb2c2859
Yingchun Lai <laiyingchun@apache.org>
2023-12-18 20:44:11 +0800

    chore(java-client-ci): build the server instead of download it from external site

--- java-client/scripts/ci-test.sh
+++ java-client/scripts/ci-test.sh
@@ -20,10 +20,6 @@
 
 set -e
 
-SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
-PROJECT_DIR=$(dirname "${SCRIPT_DIR}")
-cd "${PROJECT_DIR}" || exit 1
-
 # lint all scripts, abort if there's any warning.
 function shellcheck_must_pass()
 {
@@ -35,34 +31,10 @@ function shellcheck_must_pass()
 }
 shellcheck_must_pass ./scripts/ci-test.sh
 
-# The new version of pegasus client is not compatible with old version server which contains old rpc protocol,
-# So we use snapshot version of pegasus-tools, because we don`t have a new release version, which contains the new version of rpc protocol,
-# TODO: This is a temp url. We will change it later.
-PEGASUS_PKG="pegasus-tools-2.1.SNAPSHOT-dc4c710-glibc2.17-release"
-PEGASUS_PKG_URL="https://github.com/levy5307/pegasus-tools/raw/master/pegasus-tools-2.1.SNAPSHOT-dc4c710-glibc2.17-release.tar.gz"
-
-# check format
-if ! mvn spotless:check
-then
-    exit 1
-fi
-
-# start pegasus onebox environment
-if [ ! -f $PEGASUS_PKG.tar.gz ]; then
-    wget $PEGASUS_PKG_URL
-    tar xvf $PEGASUS_PKG.tar.gz
-fi
-cd $PEGASUS_PKG
-
-./run.sh start_onebox -w
-cd ../
-
-
 pushd scripts
 echo "run recompile_thrift.sh"
 ./recompile_thrift.sh
 popd
-mvn spotless:apply
 
 if ! mvn clean test
 then
