6569aa7c51bf49e3715cff3abdb257148f9846da
Dan Wang <wangdan@apache.org>
2024-03-05 21:41:45 +0800

    refactor(go-client): refactor RPC requests for meta by reflection

--- go-client/admin/client.go
+++ go-client/admin/client.go
@@ -22,6 +22,7 @@ package admin
 import (
 	"context"
 	"fmt"
+	"reflect"
 	"time"
 
 	"github.com/apache/incubator-pegasus/go-client/idl/admin"
@@ -33,10 +34,16 @@ import (
 // Client provides the administration API to a specific cluster.
 // Remember only the superusers configured to the cluster have the admin priviledges.
 type Client interface {
-	CreateTable(ctx context.Context, tableName string, partitionCount int, successIfExist_optional ...bool) error
+	Close() error
+
+	GetTimeout() time.Duration
+	SetTimeout(timeout time.Duration)
+
+	CreateTable(ctx context.Context, tableName string, partitionCount int, successIfExist_optional ...bool) (int32, error)
 
 	DropTable(ctx context.Context, tableName string) error
 
+	ListAvailTables() ([]*admin.AppInfo, error)
 	ListTables(ctx context.Context) ([]*TableInfo, error)
 }
 
@@ -50,24 +57,72 @@ type TableInfo struct {
 
 type Config struct {
 	MetaServers []string `json:"meta_servers"`
+	Timeout     time.Duration
 }
 
 // NewClient returns an instance of Client.
 func NewClient(cfg Config) Client {
 	return &rpcBasedClient{
-		metaManager: session.NewMetaManager(cfg.MetaServers, session.NewNodeSession),
+		meta:       session.NewMetaManager(cfg.MetaServers, session.NewNodeSession),
+		rpcTimeout: cfg.Timeout,
 	}
 }
 
 type rpcBasedClient struct {
-	metaManager *session.MetaManager
+	meta       *session.MetaManager
+	rpcTimeout time.Duration
+}
+
+func (c *rpcBasedClient) Close() error {
+	return c.meta.Close()
+}
+
+func (c *rpcBasedClient) GetTimeout() time.Duration {
+	return c.rpcTimeout
+}
+
+func (c *rpcBasedClient) SetTimeout(timeout time.Duration) {
+	c.rpcTimeout = timeout
+}
+
+// `callback` always accepts non-nil `resp`.
+func (m *rpcBasedMeta) callMeta(methodName string, req interface{}, callback func(resp interface{})) error {
+	ctx, cancel := context.WithTimeout(context.Background(), c.rpcTimeout)
+	defer cancel()
+
+	// There are 2 kinds of structs for the retured slice:
+	// * [response, error], or
+	// * error.
+	result := reflect.ValueOf(m.meta).MethodByName(methodName).Call([]reflect.Value{
+		reflect.ValueOf(ctx),
+		reflect.ValueOf(req),
+	})
+
+	// The last element must be error.
+	ierr := result[len(result)-1].Interface()
+
+	var err error
+	if ierr != nil {
+		err = ierr.(error)
+	}
+
+	if len(result) == 1 {
+		return err
+	}
+
+	// The struct of returned slice must be [response, error].
+	if !result[0].IsNil() {
+		callback(result[0].Interface())
+	}
+
+	return err
 }
 
 func (c *rpcBasedClient) waitTableReady(ctx context.Context, tableName string, partitionCount int) error {
 	const replicaCount int = 3
 
 	for {
-		resp, err := c.metaManager.QueryConfig(ctx, tableName)
+		resp, err := c.meta.QueryConfig(ctx, tableName)
 		if err != nil {
 			return err
 		}
@@ -94,7 +149,7 @@ func (c *rpcBasedClient) CreateTable(ctx context.Context, tableName string, part
 	if len(successIfExist_optional) > 0 {
 		successIfExist = successIfExist_optional[0]
 	}
-	_, err := c.metaManager.CreateApp(ctx, &admin.ConfigurationCreateAppRequest{
+	_, err := c.meta.CreateApp(ctx, &admin.ConfigurationCreateAppRequest{
 		AppName: tableName,
 		Options: &admin.CreateAppOptions{
 			PartitionCount: int32(partitionCount),
@@ -120,12 +175,12 @@ func (c *rpcBasedClient) DropTable(ctx context.Context, tableName string) error 
 		SuccessIfNotExist: true,
 		ReserveSeconds:    &reserveSeconds,
 	}
-	_, err := c.metaManager.DropApp(ctx, req)
+	_, err := c.meta.DropApp(ctx, req)
 	return err
 }
 
 func (c *rpcBasedClient) ListTables(ctx context.Context) ([]*TableInfo, error) {
-	resp, err := c.metaManager.ListApps(ctx, &admin.ConfigurationListAppsRequest{
+	resp, err := c.meta.ListApps(ctx, &admin.ConfigurationListAppsRequest{
 		Status: replication.AppStatus_AS_AVAILABLE,
 	})
 	if err != nil {
