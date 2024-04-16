a5da470017699a606e34358ec37972a0566c8c6a
yujingwei <yujingwei@sensorsdata.cn>
2024-04-16 17:38:45 +0800

    feat(java-client): java client add class host_port for fqdn compile

--- java-client/src/test/java/org/apache/pegasus/base/TestHostPort.java
+++ java-client/src/test/java/org/apache/pegasus/base/TestHostPort.java
@@ -16,6 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
 package org.apache.pegasus.base;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
