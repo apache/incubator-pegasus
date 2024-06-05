37b59d944748083f1de91e0a57a813aea5f1aba6
Samunroyu <36890229+Samunroyu@users.noreply.github.com>
2024-06-05 16:45:01 +0800

    fix(shell): Add PARSE_OPT_STRS marco for shell command execute  flag input mode (#2040)
    The marco `PARSE_STRS` execute strs with `param_index`，and it only
    execute the number of params_index of input strs.
    
    The marco `PARSE_OPT_STRS` can execute input strs with flag.
    
    The historical flag input mode should be continued.

--- src/shell/command_helper.h
+++ src/shell/command_helper.h
@@ -929,6 +929,12 @@ private:
         }                                                                                          \
     } while (false)
 
+#define PARSE_OPT_STRS(container, def_val, ...)                                                    \
+    do {                                                                                           \
+        const auto param = cmd(__VA_ARGS__, (def_val)).str();                                      \
+        ::dsn::utils::split_args(param.c_str(), container, ',');                                   \
+    } while (false)
+
 // A helper macro to parse command argument, the result is filled in an uint32_t variable named
 // 'value'.
 #define PARSE_UINT(value)                                                                          \
