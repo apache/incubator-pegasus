ef78b7ef277ceaf7f91af9dc1cc95d6f3817cddf
Yingchun Lai <laiyingchun@apache.org>
2024-07-03 17:03:30 +0800

    fix: Fix a typo in log

--- src/runtime/task/task_spec.cpp
+++ src/runtime/task/task_spec.cpp
@@ -231,8 +231,8 @@ bool task_spec::init()
         }
 
         if (spec->rpc_call_channel == RPC_CHANNEL_UDP && !FLAGS_enable_udp) {
-            LOG_ERROR("task rpc_call_channel RPC_CHANNEL_UCP need udp service, make sure "
-                      "[network].enable_udp");
+            LOG_ERROR("task rpc_call_channel RPC_CHANNEL_UDP need udp service, make sure "
+                      "[network].enable_udp is enabled");
             return false;
         }
     }
