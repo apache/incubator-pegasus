146b6b5c804c2b0af504f6f0bd0f212bf084ea7f
Dan Wang <wangdan@apache.org>
2024-09-05 15:04:43 +0800

    fix

--- src/utils/test/blob_test.cpp
+++ src/utils/test/blob_test.cpp
@@ -24,15 +24,24 @@
 
 namespace dsn {
 
-TEST(BlobTest, CreateFromNullptr)
+TEST(BlobTest, CreateFromZeroLengthNullptr)
 {
     const auto &obj = blob::create_from_bytes(nullptr, 0);
 
-    // TODO(wangdan): once create_from_bytes() could return an empty blob, add case
-    // to test if all pointers are null.
     EXPECT_EQ(0, obj.length());
     EXPECT_EQ(0, obj.size());
 }
+
+#ifndef NDEBUG
+
+TEST(BlobTest, CreateFromNonZeroLengthNullptr)
+{
+    ASSERT_DEATH({ const auto &obj = blob::create_from_bytes(nullptr, 1); },
+                 "null source pointer with non-zero length would lead to "
+                 "undefined behaviour");
+}
+
+#endif
 
 struct blob_base_case
 {
