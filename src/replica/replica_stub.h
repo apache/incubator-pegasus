39bb57dbe2628cd0bbe5f18210e8bde8aaa07ccd
Dan Wang <wangdan@apache.org>
2024-09-20 13:37:13 +0800

    add ParseReplicaDirNameTest, fix clang-tidy and IWYU

--- src/replica/replica_stub.h
+++ src/replica/replica_stub.h
@@ -486,6 +486,7 @@ private:
     friend class replica_http_service_test;
     friend class mock_load_replica;
     friend class GetReplicaDirNameTest;
+    friend class ParseReplicaDirNameTest;
     FRIEND_TEST(open_replica_test, open_replica_add_decree_and_ballot_check);
     FRIEND_TEST(replica_test, test_auto_trash_of_corruption);
     FRIEND_TEST(replica_test, test_clear_on_failure);
