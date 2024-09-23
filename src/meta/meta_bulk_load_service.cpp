acelyc111/pegasus

refactor(FQDN): feather refator on idl/dsn.layer2.thrift

src/meta/meta_bulk_load_service.cpp
@@ -356,7 +356,7 @@ bool bulk_load_service::check_partition_status(
     const gpid &pid,
     bool always_unhealthy_check,
     const std::function<void(const std::string &, const gpid &)> &retry_function,
-    /*out*/ partition_configuration &pconfig)
+    /*out*/ partition_configuration &pc)
 {
     std::shared_ptr<app_state> app = get_app(pid.get_app_id());
     if (app == nullptr || app->status != app_status::AS_AVAILABLE) {
@@ -369,8 +369,10 @@ bool bulk_load_service::check_partition_status(
         return false;
     }
 
-    pconfig = app->partitions[pid.get_partition_index()];
-    if (!pconfig.hp_primary) {
+    pc = app->pcs[pid.get_partition_index()];
+    host_port primary;
+    GET_HOST_PORT(pc, primary1, primary);
+    if (!primary) {
         LOG_WARNING("app({}) partition({}) primary is invalid, try it later", app_name, pid);
         tasking::enqueue(LPC_META_STATE_NORMAL,
                          _meta_svc->tracker(),
@@ -380,7 +382,9 @@ bool bulk_load_service::check_partition_status(
         return false;
     }
 
-    if (pconfig.hp_secondaries.size() < pconfig.max_replica_count - 1) {
+    std::vector<host_port> secondaries;
+    GET_HOST_PORTS(pc, secondaries1, secondaries);
+    if (secondaries.size() < pc.max_replica_count - 1) {
         bulk_load_status::type p_status;
         {
             zauto_read_lock l(_lock);
@@ -413,7 +417,7 @@ void bulk_load_service::partition_bulk_load(const std::string &app_name, const g
 {
     FAIL_POINT_INJECT_F("meta_bulk_load_partition_bulk_load", [](absl::string_view) {});
 
-    partition_configuration pconfig;
+    partition_configuration pc;
     if (!check_partition_status(app_name,
                                 pid,
                                 false,
@@ -421,7 +425,7 @@ void bulk_load_service::partition_bulk_load(const std::string &app_name, const g
                                           this,
                                           std::placeholders::_1,
                                           std::placeholders::_2),
-                                pconfig)) {
+                                pc)) {
         return;
     }
 
@@ -431,18 +435,18 @@ void bulk_load_service::partition_bulk_load(const std::string &app_name, const g
         const app_bulk_load_info &ainfo = _app_bulk_load_info[pid.get_app_id()];
         req->pid = pid;
         req->app_name = app_name;
-        SET_IP_AND_HOST_PORT(*req, primary, pconfig.primary, pconfig.hp_primary);
+        SET_OBJ_IP_AND_HOST_PORT(*req, primary, pc, primary1);
         req->remote_provider_name = ainfo.file_provider_type;
         req->cluster_name = ainfo.cluster_name;
         req->meta_bulk_load_status = get_partition_bulk_load_status_unlocked(pid);
-        req->ballot = pconfig.ballot;
+        req->ballot = pc.ballot;
         req->query_bulk_load_metadata = is_partition_metadata_not_updated_unlocked(pid);
         req->remote_root_path = ainfo.remote_root_path;
     }
 
     LOG_INFO("send bulk load request to node({}), app({}), partition({}), partition "
              "status = {}, remote provider = {}, cluster_name = {}, remote_root_path = {}",
-             FMT_HOST_PORT_AND_IP(pconfig, primary),
+             FMT_HOST_PORT_AND_IP(pc, primary1),
              app_name,
              pid,
              dsn::enum_to_string(req->meta_bulk_load_status),
@@ -451,17 +455,16 @@ void bulk_load_service::partition_bulk_load(const std::string &app_name, const g
              req->remote_root_path);
 
     bulk_load_rpc rpc(std::move(req), RPC_BULK_LOAD, 0_ms, 0, pid.thread_hash());
-    rpc.call(
-        pconfig.primary, _meta_svc->tracker(), [this, pid, rpc, pconfig](error_code err) mutable {
-            // The remote server may not support FQDN, but do not try to reverse resolve the
-            // IP addresses because they may be unresolved. Just warning and ignore this.
-            LOG_WARNING_IF(!rpc.response().__isset.hp_group_bulk_load_state,
-                           "The {} primary {} doesn't support FQDN, the response "
-                           "hp_group_bulk_load_state field is not set",
-                           pid,
-                           FMT_HOST_PORT_AND_IP(pconfig, primary));
-            on_partition_bulk_load_reply(err, rpc.request(), rpc.response());
-        });
+    rpc.call(pc.primary1, _meta_svc->tracker(), [this, pid, rpc, pc](error_code err) mutable {
+        // The remote server may not support FQDN, but do not try to reverse resolve the
+        // IP addresses because they may be unresolved. Just warning and ignore this.
+        LOG_WARNING_IF(!rpc.response().__isset.hp_group_bulk_load_state,
+                       "The {} primary {} doesn't support FQDN, the response "
+                       "hp_group_bulk_load_state field is not set",
+                       pid,
+                       FMT_HOST_PORT_AND_IP(pc, primary1));
+        on_partition_bulk_load_reply(err, rpc.request(), rpc.response());
+    });
 }
 
 // ThreadPool: THREAD_POOL_META_STATE
@@ -530,7 +533,7 @@ void bulk_load_service::on_partition_bulk_load_reply(error_code err,
         handle_app_unavailable(pid.get_app_id(), app_name);
         return;
     }
-    ballot current_ballot = app->partitions[pid.get_partition_index()].ballot;
+    ballot current_ballot = app->pcs[pid.get_partition_index()].ballot;
     if (request.ballot < current_ballot) {
         LOG_WARNING(
             "receive out-date response from node({}), app({}), partition({}), request ballot = "
@@ -1184,7 +1187,7 @@ void bulk_load_service::update_app_status_on_remote_storage_reply(const app_bulk
 }
 
 // ThreadPool: THREAD_POOL_META_STATE
-bool bulk_load_service::check_ever_ingestion_succeed(const partition_configuration &config,
+bool bulk_load_service::check_ever_ingestion_succeed(const partition_configuration &pc,
                                                      const std::string &app_name,
                                                      const gpid &pid)
 {
@@ -1199,8 +1202,12 @@ bool bulk_load_service::check_ever_ingestion_succeed(const partition_configurati
     }
 
     std::vector<host_port> current_nodes;
-    current_nodes.emplace_back(config.hp_primary);
-    for (const auto &secondary : config.hp_secondaries) {
+    dsn::host_port primary;
+    GET_HOST_PORT(pc, primary1, primary);
+    current_nodes.emplace_back(primary);
+    std::vector<host_port> secondaries;
+    GET_HOST_PORTS(pc, secondaries1, secondaries);
+    for (const auto &secondary : secondaries) {
         current_nodes.emplace_back(secondary);
     }
 
@@ -1240,7 +1247,7 @@ void bulk_load_service::partition_ingestion(const std::string &app_name, const g
         return;
     }
 
-    partition_configuration pconfig;
+    partition_configuration pc;
     if (!check_partition_status(app_name,
                                 pid,
                                 true,
@@ -1248,16 +1255,16 @@ void bulk_load_service::partition_ingestion(const std::string &app_name, const g
                                           this,
                                           std::placeholders::_1,
                                           std::placeholders::_2),
-                                pconfig)) {
+                                pc)) {
         return;
     }
 
-    if (check_ever_ingestion_succeed(pconfig, app_name, pid)) {
+    if (check_ever_ingestion_succeed(pc, app_name, pid)) {
         return;
     }
 
     auto app = get_app(pid.get_app_id());
-    if (!try_partition_ingestion(pconfig, app->helpers->contexts[pid.get_partition_index()])) {
+    if (!try_partition_ingestion(pc, app->helpers->contexts[pid.get_partition_index()])) {
         LOG_WARNING(
             "app({}) partition({}) couldn't execute ingestion, wait and try later", app_name, pid);
         tasking::enqueue(LPC_META_STATE_NORMAL,
@@ -1268,18 +1275,15 @@ void bulk_load_service::partition_ingestion(const std::string &app_name, const g
         return;
     }
 
-    const auto &primary_addr = pconfig.hp_primary;
-    ballot meta_ballot = pconfig.ballot;
-    tasking::enqueue(LPC_BULK_LOAD_INGESTION,
-                     _meta_svc->tracker(),
-                     std::bind(&bulk_load_service::send_ingestion_request,
-                               this,
-                               app_name,
-                               pid,
-                               primary_addr,
-                               meta_ballot),
-                     0,
-                     std::chrono::seconds(bulk_load_constant::BULK_LOAD_REQUEST_INTERVAL));
+    host_port primary;
+    GET_HOST_PORT(pc, primary1, primary);
+    tasking::enqueue(
+        LPC_BULK_LOAD_INGESTION,
+        _meta_svc->tracker(),
+        std::bind(
+            &bulk_load_service::send_ingestion_request, this, app_name, pid, primary, pc.ballot),
+        0,
+        std::chrono::seconds(bulk_load_constant::BULK_LOAD_REQUEST_INTERVAL));
 }
 
 // ThreadPool: THREAD_POOL_DEFAULT

src/shell/command_helper.h
@@ -1560,7 +1560,7 @@ inline stat_var_map create_rates(row_data &row)
 // Given all tables, create all aggregations needed for the table-level stats. All selected
 // partitions should have their primary replicas on this node.
 inline std::unique_ptr<aggregate_stats_calcs> create_table_aggregate_stats_calcs(
-    const std::map<int32_t, std::vector<dsn::partition_configuration>> &table_partitions,
+    const std::map<int32_t, std::vector<dsn::partition_configuration>> &pcs_by_appid,
     const dsn::host_port &node,
     const std::string &entity_type,
     std::vector<row_data> &rows)
@@ -1580,18 +1580,20 @@ inline std::unique_ptr<aggregate_stats_calcs> create_table_aggregate_stats_calcs
             processor.first->emplace(row.app_id, processor.second(row));
         }
 
-        const auto &table = table_partitions.find(row.app_id);
-        CHECK(table != table_partitions.end(),
-              "table could not be found in table_partitions: table_id={}",
+        const auto &iter = pcs_by_appid.find(row.app_id);
+        CHECK(iter != pcs_by_appid.end(),
+              "table could not be found in pcs_by_appid: table_id={}",
               row.app_id);
 
-        for (const auto &partition : table->second) {
-            if (partition.hp_primary != node) {
+        for (const auto &pc : iter->second) {
+            dsn::host_port primary;
+            GET_HOST_PORT(pc, primary1, primary);
+            if (primary != node) {
                 // Ignore once the replica of the metrics is not the primary of the partition.
                 continue;
             }
 
-            partitions.insert(partition.pid);
+            partitions.insert(pc.pid);
         }
     }
 
@@ -1606,18 +1608,20 @@ inline std::unique_ptr<aggregate_stats_calcs> create_table_aggregate_stats_calcs
 // stats. All selected partitions should have their primary replicas on this node.
 inline std::unique_ptr<aggregate_stats_calcs>
 create_partition_aggregate_stats_calcs(const int32_t table_id,
-                                       const std::vector<dsn::partition_configuration> &partitions,
+                                       const std::vector<dsn::partition_configuration> &pcs,
                                        const dsn::host_port &node,
                                        const std::string &entity_type,
                                        std::vector<row_data> &rows)
 {
-    CHECK_EQ(rows.size(), partitions.size());
+    CHECK_EQ(rows.size(), pcs.size());
 
     partition_stat_map sums;
     partition_stat_map increases;
     partition_stat_map rates;
     for (size_t i = 0; i < rows.size(); ++i) {
-        if (partitions[i].hp_primary != node) {
+        dsn::host_port primary;
+        GET_HOST_PORT(pcs[i], primary1, primary);
+        if (primary != node) {
             // Ignore once the replica of the metrics is not the primary of the partition.
             continue;
         }
@@ -1785,13 +1789,13 @@ inline bool get_apps_and_nodes(shell_context *sc,
 inline bool
 get_app_partitions(shell_context *sc,
                    const std::vector<::dsn::app_info> &apps,
-                   std::map<int32_t, std::vector<dsn::partition_configuration>> &app_partitions)
+                   std::map<int32_t, std::vector<dsn::partition_configuration>> &pcs_by_appid)
 {
     for (const ::dsn::app_info &app : apps) {
         int32_t app_id = 0;
         int32_t partition_count = 0;
         dsn::error_code err = sc->ddl_client->list_app(
-            app.app_name, app_id, partition_count, app_partitions[app.app_id]);
+            app.app_name, app_id, partition_count, pcs_by_appid[app.app_id]);
         if (err != ::dsn::ERR_OK) {
             LOG_ERROR("list app {} failed, error = {}", app.app_name, err);
             return false;
@@ -1844,8 +1848,8 @@ inline bool get_app_partition_stat(shell_context *sc,
     }
 
     // get app_id --> partitions
-    std::map<int32_t, std::vector<dsn::partition_configuration>> app_partitions;
-    if (!get_app_partitions(sc, apps, app_partitions)) {
+    std::map<int32_t, std::vector<dsn::partition_configuration>> pcs_by_appid;
+    if (!get_app_partitions(sc, apps, pcs_by_appid)) {
         return false;
     }
 
@@ -1869,13 +1873,16 @@ inline bool get_app_partition_stat(shell_context *sc,
             if (parse_app_pegasus_perf_counter_name(
                     m.name, app_id_x, partition_index_x, counter_name)) {
                 // only primary partition will be counted
-                auto find = app_partitions.find(app_id_x);
-                if (find != app_partitions.end() &&
-                    find->second[partition_index_x].hp_primary == nodes[i].hp) {
-                    row_data &row = rows[app_id_name[app_id_x]][partition_index_x];
-                    row.row_name = std::to_string(partition_index_x);
-                    row.app_id = app_id_x;
-                    update_app_pegasus_perf_counter(row, counter_name, m.value);
+                const auto find = pcs_by_appid.find(app_id_x);
+                if (find != pcs_by_appid.end()) {
+                    dsn::host_port primary;
+                    GET_HOST_PORT(find->second[partition_index_x], primary1, primary);
+                    if (primary == nodes[i].hp) {
+                        row_data &row = rows[app_id_name[app_id_x]][partition_index_x];
+                        row.row_name = std::to_string(partition_index_x);
+                        row.app_id = app_id_x;
+                        update_app_pegasus_perf_counter(row, counter_name, m.value);
+                    }
                 }
             } else if (parse_app_perf_counter_name(m.name, app_name, counter_name)) {
                 // if the app_name from perf-counter isn't existed(maybe the app was dropped), it
@@ -1908,8 +1915,8 @@ get_table_stats(shell_context *sc, uint32_t sample_interval_ms, std::vector<row_
     std::this_thread::sleep_for(std::chrono::milliseconds(sample_interval_ms));
     const auto &results_end = get_metrics(nodes, query_string);
 
-    std::map<int32_t, std::vector<dsn::partition_configuration>> table_partitions;
-    if (!get_app_partitions(sc, apps, table_partitions)) {
+    std::map<int32_t, std::vector<dsn::partition_configuration>> pcs_by_appid;
+    if (!get_app_partitions(sc, apps, pcs_by_appid)) {
         return false;
     }
 
@@ -1923,15 +1930,14 @@ get_table_stats(shell_context *sc, uint32_t sample_interval_ms, std::vector<row_
             row.partition_count = app.partition_count;
             return row;
         });
-    CHECK_EQ(rows.size(), table_partitions.size());
+    CHECK_EQ(rows.size(), pcs_by_appid.size());
 
     for (size_t i = 0; i < nodes.size(); ++i) {
         RETURN_SHELL_IF_GET_METRICS_FAILED(
             results_start[i], nodes[i], "starting row data requests");
         RETURN_SHELL_IF_GET_METRICS_FAILED(results_end[i], nodes[i], "ending row data requests");
 
-        auto calcs =
-            create_table_aggregate_stats_calcs(table_partitions, nodes[i].hp, "replica", rows);
+        auto calcs = create_table_aggregate_stats_calcs(pcs_by_appid, nodes[i].hp, "replica", rows);
         RETURN_SHELL_IF_PARSE_METRICS_FAILED(
             calcs->aggregate_metrics(results_start[i].body(), results_end[i].body()),
             nodes[i],
@@ -1955,13 +1961,13 @@ inline bool get_partition_stats(shell_context *sc,
 
     int32_t table_id = 0;
     int32_t partition_count = 0;
-    std::vector<dsn::partition_configuration> partitions;
-    const auto &err = sc->ddl_client->list_app(table_name, table_id, partition_count, partitions);
+    std::vector<dsn::partition_configuration> pcs;
+    const auto &err = sc->ddl_client->list_app(table_name, table_id, partition_count, pcs);
     if (err != ::dsn::ERR_OK) {
         LOG_ERROR("list app {} failed, error = {}", table_name, err);
         return false;
     }
-    CHECK_EQ(partitions.size(), partition_count);
+    CHECK_EQ(pcs.size(), partition_count);
 
     const auto &query_string = row_data_filters(table_id).to_query_string();
     const auto &results_start = get_metrics(nodes, query_string);
@@ -1980,8 +1986,8 @@ inline bool get_partition_stats(shell_context *sc,
         RETURN_SHELL_IF_GET_METRICS_FAILED(
             results_end[i], nodes[i], "ending row data requests for table(id={})", table_id);
 
-        auto calcs = create_partition_aggregate_stats_calcs(
-            table_id, partitions, nodes[i].hp, "replica", rows);
+        auto calcs =
+            create_partition_aggregate_stats_calcs(table_id, pcs, nodes[i].hp, "replica", rows);
         RETURN_SHELL_IF_PARSE_METRICS_FAILED(
             calcs->aggregate_metrics(results_start[i].body(), results_end[i].body()),
             nodes[i],
@@ -2091,17 +2097,16 @@ inline bool get_storage_size_stat(shell_context *sc, app_storage_size_stat &st_s
         return false;
     }
 
-    std::map<int32_t, std::vector<dsn::partition_configuration>> app_partitions;
-    if (!get_app_partitions(sc, apps, app_partitions)) {
+    std::map<int32_t, std::vector<dsn::partition_configuration>> pcs_by_appid;
+    if (!get_app_partitions(sc, apps, pcs_by_appid)) {
         LOG_ERROR("get app partitions failed");
         return false;
     }
-    for (auto &kv : app_partitions) {
-        auto &v = kv.second;
-        for (auto &c : v) {
+    for (auto & [ _, pcs ] : pcs_by_appid) {
+        for (auto &pc : pcs) {
             // use partition_flags to record if this partition's storage size is calculated,
-            // because `app_partitions' is a temporary variable, so we can re-use partition_flags.
-            c.partition_flags = 0;
+            // because `pcs_by_appid' is a temporary variable, so we can re-use partition_flags.
+            pc.partition_flags = 0;
         }
     }
 
@@ -2122,11 +2127,13 @@ inline bool get_storage_size_stat(shell_context *sc, app_storage_size_stat &st_s
             CHECK(parse_ret, "name = {}", m.name);
             if (counter_name != "disk.storage.sst(MB)")
                 continue;
-            auto find = app_partitions.find(app_id_x);
-            if (find == app_partitions.end()) // app id not found
+            auto find = pcs_by_appid.find(app_id_x);
+            if (find == pcs_by_appid.end()) // app id not found
                 continue;
-            dsn::partition_configuration &pc = find->second[partition_index_x];
-            if (pc.hp_primary != nodes[i].hp) // not primary replica
+            auto &pc = find->second[partition_index_x];
+            dsn::host_port primary;
+            GET_HOST_PORT(pc, primary1, primary);
+            if (primary != nodes[i].hp) // not primary replica
                 continue;
             if (pc.partition_flags != 0) // already calculated
                 continue;

apache/incubator-pegasus

src/meta/meta_bulk_load_service.cpp
@@ -371,7 +371,9 @@ bool bulk_load_service::check_partition_status(
     }
 
     pc = app->pcs[pid.get_partition_index()];
-    if (!pc.hp_primary) {
+    host_port primary;
+    GET_HOST_PORT(pc, primary, primary);
+    if (!primary) {
         LOG_WARNING("app({}) partition({}) primary is invalid, try it later", app_name, pid);
         tasking::enqueue(
             LPC_META_STATE_NORMAL,
@@ -382,7 +384,9 @@ bool bulk_load_service::check_partition_status(
         return false;
     }
 
-    if (pc.hp_secondaries.size() < pc.max_replica_count - 1) {
+    std::vector<host_port> secondaries;
+    GET_HOST_PORTS(pc, secondaries, secondaries);
+    if (secondaries.size() < pc.max_replica_count - 1) {
         bulk_load_status::type p_status;
         {
             zauto_read_lock l(_lock);
@@ -434,7 +438,7 @@ void bulk_load_service::partition_bulk_load(const std::string &app_name, const g
         const app_bulk_load_info &ainfo = _app_bulk_load_info[pid.get_app_id()];
         req->pid = pid;
         req->app_name = app_name;
-        SET_IP_AND_HOST_PORT(*req, primary, pc.primary, pc.hp_primary);
+        SET_OBJ_IP_AND_HOST_PORT(*req, primary, pc, primary);
         req->remote_provider_name = ainfo.file_provider_type;
         req->cluster_name = ainfo.cluster_name;
         req->meta_bulk_load_status = get_partition_bulk_load_status_unlocked(pid);
@@ -1201,8 +1205,12 @@ bool bulk_load_service::check_ever_ingestion_succeed(const partition_configurati
     }
 
     std::vector<host_port> current_nodes;
-    current_nodes.emplace_back(pc.hp_primary);
-    for (const auto &secondary : pc.hp_secondaries) {
+    dsn::host_port primary;
+    GET_HOST_PORT(pc, primary, primary);
+    current_nodes.emplace_back(primary);
+    std::vector<host_port> secondaries;
+    GET_HOST_PORTS(pc, secondaries, secondaries);
+    for (const auto &secondary : secondaries) {
         current_nodes.emplace_back(secondary);
     }
 
@@ -1270,13 +1278,13 @@ void bulk_load_service::partition_ingestion(const std::string &app_name, const g
         return;
     }
 
-    const auto &primary = pc.hp_primary;
-    ballot meta_ballot = pc.ballot;
+    host_port primary;
+    GET_HOST_PORT(pc, primary, primary);
     tasking::enqueue(
         LPC_BULK_LOAD_INGESTION,
         _meta_svc->tracker(),
         std::bind(
-            &bulk_load_service::send_ingestion_request, this, app_name, pid, primary, meta_ballot),
+            &bulk_load_service::send_ingestion_request, this, app_name, pid, primary, pc.ballot),
         0,
         std::chrono::seconds(bulk_load_constant::BULK_LOAD_REQUEST_INTERVAL));
 }

src/shell/command_helper.h
@@ -1652,7 +1652,9 @@ inline std::unique_ptr<aggregate_stats_calcs> create_table_aggregate_stats_calcs
               row.app_id);
 
         for (const auto &pc : iter->second) {
-            if (pc.hp_primary != node) {
+            dsn::host_port primary;
+            GET_HOST_PORT(pc, primary, primary);
+            if (primary != node) {
                 // Ignore once the replica of the metrics is not the primary of the partition.
                 continue;
             }
@@ -1683,7 +1685,9 @@ create_partition_aggregate_stats_calcs(const int32_t table_id,
     partition_stat_map increases;
     partition_stat_map rates;
     for (size_t i = 0; i < rows.size(); ++i) {
-        if (pcs[i].hp_primary != node) {
+        dsn::host_port primary;
+        GET_HOST_PORT(pcs[i], primary, primary);
+        if (primary != node) {
             // Ignore once the replica of the metrics is not the primary of the partition.
             continue;
         }
@@ -1938,12 +1942,15 @@ inline bool get_app_partition_stat(shell_context *sc,
                     m.name, app_id_x, partition_index_x, counter_name)) {
                 // only primary partition will be counted
                 const auto find = pcs_by_appid.find(app_id_x);
-                if (find != pcs_by_appid.end() &&
-                    find->second[partition_index_x].hp_primary == nodes[i].hp) {
-                    row_data &row = rows[app_id_name[app_id_x]][partition_index_x];
-                    row.row_name = std::to_string(partition_index_x);
-                    row.app_id = app_id_x;
-                    update_app_pegasus_perf_counter(row, counter_name, m.value);
+                if (find != pcs_by_appid.end()) {
+                    dsn::host_port primary;
+                    GET_HOST_PORT(find->second[partition_index_x], primary, primary);
+                    if (primary == nodes[i].hp) {
+                        row_data &row = rows[app_id_name[app_id_x]][partition_index_x];
+                        row.row_name = std::to_string(partition_index_x);
+                        row.app_id = app_id_x;
+                        update_app_pegasus_perf_counter(row, counter_name, m.value);
+                    }
                 }
             } else if (parse_app_perf_counter_name(m.name, app_name, counter_name)) {
                 // if the app_name from perf-counter isn't existed(maybe the app was dropped), it
@@ -2192,7 +2199,9 @@ inline bool get_storage_size_stat(shell_context *sc, app_storage_size_stat &st_s
             if (find == pcs_by_appid.end()) // app id not found
                 continue;
             auto &pc = find->second[partition_index_x];
-            if (pc.hp_primary != nodes[i].hp) // not primary replica
+            dsn::host_port primary;
+            GET_HOST_PORT(pc, primary, primary);
+            if (primary != nodes[i].hp) // not primary replica
                 continue;
             if (pc.partition_flags != 0) // already calculated
                 continue;