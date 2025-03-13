acelyc111/pegasus

refactor(FQDN): feather refator on idl/dsn.layer2.thrift

src/common/replication_other_types.h
@@ -36,6 +36,7 @@
 #include "consensus_types.h"
 #include "replica_admin_types.h"
 #include "common/replication_enums.h"
+#include "runtime/rpc/dns_resolver.h"
 #include "runtime/rpc/rpc_address.h"
 #include "runtime/rpc/rpc_host_port.h"
 
@@ -53,19 +54,19 @@ typedef int64_t decree;
 
 inline bool is_primary(const partition_configuration &pc, const host_port &node)
 {
-    return node && pc.hp_primary == node;
+    return node && pc.hp_primary1 == node;
 }
 inline bool is_primary(const partition_configuration &pc, const rpc_address &node)
 {
-    return node && pc.primary == node;
+    return node && pc.primary1 == node;
 }
 inline bool is_secondary(const partition_configuration &pc, const host_port &node)
 {
-    return node && utils::contains(pc.hp_secondaries, node);
+    return node && utils::contains(pc.hp_secondaries1, node);
 }
 inline bool is_secondary(const partition_configuration &pc, const rpc_address &node)
 {
-    return node && utils::contains(pc.secondaries, node);
+    return node && utils::contains(pc.secondaries1, node);
 }
 inline bool is_member(const partition_configuration &pc, const host_port &node)
 {
@@ -79,16 +80,24 @@ inline bool is_partition_config_equal(const partition_configuration &pc1,
                                       const partition_configuration &pc2)
 {
     // secondaries no need to be same order
-    for (const auto &hp : pc1.hp_secondaries) {
-        if (!is_secondary(pc2, hp)) {
+    std::vector<host_port> pc1_secondaries;
+    GET_HOST_PORTS(pc1, secondaries1, pc1_secondaries);
+    for (const auto &pc1_secondary : pc1_secondaries) {
+        if (!is_secondary(pc2, pc1_secondary)) {
             return false;
         }
     }
+
     // last_drops is not considered into equality check
+    host_port pc1_primary;
+    GET_HOST_PORT(pc1, primary1, pc1_primary);
+    host_port pc2_primary;
+    GET_HOST_PORT(pc2, primary1, pc2_primary);
+    std::vector<host_port> pc2_secondaries;
+    GET_HOST_PORTS(pc2, secondaries1, pc2_secondaries);
     return pc1.ballot == pc2.ballot && pc1.pid == pc2.pid &&
-           pc1.max_replica_count == pc2.max_replica_count && pc1.primary == pc2.primary &&
-           pc1.hp_primary == pc2.hp_primary && pc1.secondaries.size() == pc2.secondaries.size() &&
-           pc1.hp_secondaries.size() == pc2.hp_secondaries.size() &&
+           pc1.max_replica_count == pc2.max_replica_count && pc1_primary == pc2_primary &&
+           pc1_secondaries.size() == pc2_secondaries.size() &&
            pc1.last_committed_decree == pc2.last_committed_decree;
 }
 
@@ -106,9 +115,9 @@ public:
         }
         return false;
     }
-    static bool get_replica_config(const partition_configuration &partition_config,
+    static bool get_replica_config(const partition_configuration &pc,
                                    const ::dsn::host_port &node,
-                                   /*out*/ replica_configuration &replica_config);
+                                   /*out*/ replica_configuration &rc);
 
     // Return true if 'server_list' is a valid comma-separated list of servers, otherwise return
     // false. The result is filled into 'servers' if success.

src/replica/duplication/replica_follower.cpp
@@ -178,18 +178,20 @@ error_code replica_follower::update_master_replica_config(error_code err, query_
         return ERR_INCONSISTENT_STATE;
     }
 
-    if (dsn_unlikely(!resp.partitions[0].hp_primary)) {
+    dsn::host_port primary;
+    GET_HOST_PORT(resp.partitions[0], primary1, primary);
+    if (dsn_unlikely(!primary)) {
         LOG_ERROR_PREFIX("master[{}] partition address is invalid", master_replica_name());
         return ERR_INVALID_STATE;
     }
 
     // since the request just specify one partition, the result size is single
-    _master_replica_config = resp.partitions[0];
+    _pc = resp.partitions[0];
     LOG_INFO_PREFIX(
         "query master[{}] config successfully and update local config: remote={}, gpid={}",
         master_replica_name(),
-        FMT_HOST_PORT_AND_IP(_master_replica_config, primary),
-        _master_replica_config.pid);
+        FMT_HOST_PORT_AND_IP(_pc, primary1),
+        _pc.pid);
     return ERR_OK;
 }
 
@@ -199,11 +201,13 @@ void replica_follower::copy_master_replica_checkpoint()
     LOG_INFO_PREFIX("query master[{}] replica checkpoint info and start use nfs copy the data",
                     master_replica_name());
     learn_request request;
-    request.pid = _master_replica_config.pid;
-    dsn::message_ex *msg = dsn::message_ex::create_request(
-        RPC_QUERY_LAST_CHECKPOINT_INFO, 0, _master_replica_config.pid.thread_hash());
+    request.pid = _pc.pid;
+    dsn::message_ex *msg =
+        dsn::message_ex::create_request(RPC_QUERY_LAST_CHECKPOINT_INFO, 0, _pc.pid.thread_hash());
     dsn::marshall(msg, request);
-    rpc::call(_master_replica_config.primary,
+    dsn::host_port primary;
+    GET_HOST_PORT(_pc, primary1, primary);
+    rpc::call(dsn::dns_resolver::instance().resolve_address(primary),
               msg,
               &_tracker,
               [&](error_code err, learn_response &&resp) mutable {

src/shell/commands/data_operations.cpp
@@ -2231,16 +2231,18 @@ inline dsn::metric_filters rdb_estimated_keys_filters(int32_t table_id)
 // All selected partitions should have their primary replicas on this node.
 std::unique_ptr<aggregate_stats_calcs>
 create_rdb_estimated_keys_stats_calcs(const int32_t table_id,
-                                      const std::vector<dsn::partition_configuration> &partitions,
+                                      const std::vector<dsn::partition_configuration> &pcs,
                                       const dsn::host_port &node,
                                       const std::string &entity_type,
                                       std::vector<row_data> &rows)
 {
-    CHECK_EQ(rows.size(), partitions.size());
+    CHECK_EQ(rows.size(), pcs.size());
 
     partition_stat_map sums;
     for (size_t i = 0; i < rows.size(); ++i) {
-        if (partitions[i].hp_primary != node) {
+        dsn::host_port primary;
+        GET_HOST_PORT(pcs[i], primary1, primary);
+        if (primary != node) {
             // Ignore once the replica of the metrics is not the primary of the partition.
             continue;
         }
@@ -2268,13 +2270,13 @@ bool get_rdb_estimated_keys_stats(shell_context *sc,
 
     int32_t table_id = 0;
     int32_t partition_count = 0;
-    std::vector<dsn::partition_configuration> partitions;
-    const auto &err = sc->ddl_client->list_app(table_name, table_id, partition_count, partitions);
+    std::vector<dsn::partition_configuration> pcs;
+    const auto &err = sc->ddl_client->list_app(table_name, table_id, partition_count, pcs);
     if (err != ::dsn::ERR_OK) {
         LOG_ERROR("list app {} failed, error = {}", table_name, err);
         return false;
     }
-    CHECK_EQ(partitions.size(), partition_count);
+    CHECK_EQ(pcs.size(), partition_count);
 
     const auto &results =
         get_metrics(nodes, rdb_estimated_keys_filters(table_id).to_query_string());
@@ -2289,8 +2291,8 @@ bool get_rdb_estimated_keys_stats(shell_context *sc,
         RETURN_SHELL_IF_GET_METRICS_FAILED(
             results[i], nodes[i], "rdb_estimated_keys for table(id={})", table_id);
 
-        auto calcs = create_rdb_estimated_keys_stats_calcs(
-            table_id, partitions, nodes[i].hp, "replica", rows);
+        auto calcs =
+            create_rdb_estimated_keys_stats_calcs(table_id, pcs, nodes[i].hp, "replica", rows);
         RETURN_SHELL_IF_PARSE_METRICS_FAILED(calcs->aggregate_metrics(results[i].body()),
                                              nodes[i],
                                              "rdb_estimated_keys for table(id={})",
@@ -2870,9 +2872,9 @@ bool calculate_hash_value(command_executor *e, shell_context *sc, arguments args
     if (!sc->current_app_name.empty()) {
         int32_t app_id;
         int32_t partition_count;
-        std::vector<::dsn::partition_configuration> partitions;
+        std::vector<::dsn::partition_configuration> pcs;
         ::dsn::error_code err =
-            sc->ddl_client->list_app(sc->current_app_name, app_id, partition_count, partitions);
+            sc->ddl_client->list_app(sc->current_app_name, app_id, partition_count, pcs);
         if (err != ::dsn::ERR_OK) {
             std::cout << "list app [" << sc->current_app_name << "] failed, error=" << err
                       << std::endl;
@@ -2883,17 +2885,14 @@ bool calculate_hash_value(command_executor *e, shell_context *sc, arguments args
         tp.add_row_name_and_data("app_id", app_id);
         tp.add_row_name_and_data("partition_count", partition_count);
         tp.add_row_name_and_data("partition_index", partition_index);
-        if (partitions.size() > partition_index) {
-            ::dsn::partition_configuration &pc = partitions[partition_index];
-            tp.add_row_name_and_data("primary", pc.hp_primary.to_string());
-
-            std::ostringstream oss;
-            for (int i = 0; i < pc.hp_secondaries.size(); ++i) {
-                if (i != 0)
-                    oss << ",";
-                oss << pc.hp_secondaries[i];
-            }
-            tp.add_row_name_and_data("secondaries", oss.str());
+        if (pcs.size() > partition_index) {
+            const auto &pc = pcs[partition_index];
+            dsn::host_port primary;
+            GET_HOST_PORT(pc, primary1, primary);
+            std::vector<dsn::host_port> secondaries;
+            GET_HOST_PORTS(pc, secondaries1, secondaries);
+            tp.add_row_name_and_data("primary", primary);
+            tp.add_row_name_and_data("secondaries", fmt::format("{}", fmt::join(secondaries, ",")));
         }
     }
     tp.output(std::cout);

src/test/kill_test/kill_testor.cpp
@@ -102,38 +102,34 @@ dsn::error_code kill_testor::get_partition_info(bool debug_unhealthy,
     healthy_partition_cnt = 0, unhealthy_partition_cnt = 0;
     int32_t app_id;
     int32_t partition_count;
-    partitions.clear();
-    dsn::error_code err =
-        ddl_client->list_app(FLAGS_verify_app_name, app_id, partition_count, partitions);
+    pcs.clear();
+    dsn::error_code err = ddl_client->list_app(FLAGS_verify_app_name, app_id, partition_count, pcs);
 
     if (err == ::dsn::ERR_OK) {
         LOG_DEBUG("access meta and query partition status success");
-        for (int i = 0; i < partitions.size(); i++) {
-            const dsn::partition_configuration &p = partitions[i];
+        for (const auto &pc : pcs) {
             int replica_count = 0;
-            if (p.hp_primary) {
+            dsn::host_port primary;
+            GET_HOST_PORT(pc, primary1, primary);
+            if (primary) {
                 replica_count++;
             }
-            replica_count += p.hp_secondaries.size();
-            if (replica_count == p.max_replica_count) {
+            std::vector<dsn::host_port> secondaries;
+            GET_HOST_PORTS(pc, secondaries1, secondaries);
+            replica_count += secondaries.size();
+            if (replica_count == pc.max_replica_count) {
                 healthy_partition_cnt++;
             } else {
-                std::stringstream info;
-                info << "gpid=" << p.pid.get_app_id() << "." << p.pid.get_partition_index() << ", ";
-                info << "primay=" << p.hp_primary << ", ";
-                info << "secondaries=[";
-                for (int idx = 0; idx < p.hp_secondaries.size(); idx++) {
-                    if (idx != 0)
-                        info << "," << p.hp_secondaries[idx];
-                    else
-                        info << p.hp_secondaries[idx];
-                }
-                info << "], ";
-                info << "last_committed_decree=" << p.last_committed_decree;
+                const auto &info =
+                    fmt::format("gpid={}, primary={}, secondaries=[{}], last_committed_decree={}",
+                                pc.pid,
+                                primary,
+                                fmt::join(secondaries, ", "),
+                                pc.last_committed_decree);
                 if (debug_unhealthy) {
-                    LOG_INFO("found unhealthy partition, {}", info.str());
+                    LOG_INFO("found unhealthy partition, {}", info);
                 } else {
-                    LOG_DEBUG("found unhealthy partition, {}", info.str());
+                    LOG_DEBUG("found unhealthy partition, {}", info);
                 }
             }
         }

apache/incubator-pegasus

src/common/replication_other_types.h
@@ -36,6 +36,7 @@
 #include "consensus_types.h"
 #include "replica_admin_types.h"
 #include "common/replication_enums.h"
+#include "rpc/dns_resolver.h"
 #include "rpc/rpc_address.h"
 #include "rpc/rpc_host_port.h"
 
@@ -78,18 +79,33 @@ inline bool is_member(const partition_configuration &pc, const rpc_address &node
 inline bool is_partition_config_equal(const partition_configuration &pc1,
                                       const partition_configuration &pc2)
 {
-    // secondaries no need to be same order
-    for (const auto &pc1_secondary : pc1.hp_secondaries) {
+    if (pc1.ballot != pc2.ballot || pc1.pid != pc2.pid ||
+        pc1.max_replica_count != pc2.max_replica_count ||
+        pc1.last_committed_decree != pc2.last_committed_decree) {
+        return false;
+    }
+
+    host_port pc1_primary;
+    GET_HOST_PORT(pc1, primary, pc1_primary);
+    host_port pc2_primary;
+    GET_HOST_PORT(pc2, primary, pc2_primary);
+    if (pc1_primary != pc2_primary) {
+        return false;
+    }
+
+    // secondaries no need to be in the same order.
+    std::vector<host_port> pc1_secondaries;
+    GET_HOST_PORTS(pc1, secondaries, pc1_secondaries);
+    for (const auto &pc1_secondary : pc1_secondaries) {
         if (!is_secondary(pc2, pc1_secondary)) {
             return false;
         }
     }
+
+    std::vector<host_port> pc2_secondaries;
+    GET_HOST_PORTS(pc2, secondaries, pc2_secondaries);
     // last_drops is not considered into equality check
-    return pc1.ballot == pc2.ballot && pc1.pid == pc2.pid &&
-           pc1.max_replica_count == pc2.max_replica_count && pc1.primary == pc2.primary &&
-           pc1.hp_primary == pc2.hp_primary && pc1.secondaries.size() == pc2.secondaries.size() &&
-           pc1.hp_secondaries.size() == pc2.hp_secondaries.size() &&
-           pc1.last_committed_decree == pc2.last_committed_decree;
+    return pc1_secondaries.size() == pc2_secondaries.size();
 }
 
 class replica_helper

src/replica/duplication/replica_follower.cpp
@@ -178,7 +178,9 @@ error_code replica_follower::update_master_replica_config(error_code err, query_
         return ERR_INCONSISTENT_STATE;
     }
 
-    if (dsn_unlikely(!resp.partitions[0].hp_primary)) {
+    dsn::host_port primary;
+    GET_HOST_PORT(resp.partitions[0], primary, primary);
+    if (dsn_unlikely(!primary)) {
         LOG_ERROR_PREFIX("master[{}] partition address is invalid", master_replica_name());
         return ERR_INVALID_STATE;
     }
@@ -203,9 +205,14 @@ void replica_follower::copy_master_replica_checkpoint()
     dsn::message_ex *msg =
         dsn::message_ex::create_request(RPC_QUERY_LAST_CHECKPOINT_INFO, 0, _pc.pid.thread_hash());
     dsn::marshall(msg, request);
-    rpc::call(_pc.primary, msg, &_tracker, [&](error_code err, learn_response &&resp) mutable {
-        nfs_copy_checkpoint(err, std::move(resp));
-    });
+    dsn::host_port primary;
+    GET_HOST_PORT(_pc, primary, primary);
+    rpc::call(dsn::dns_resolver::instance().resolve_address(primary),
+              msg,
+              &_tracker,
+              [&](error_code err, learn_response &&resp) mutable {
+                  nfs_copy_checkpoint(err, std::move(resp));
+              });
 }
 
 // ThreadPool: THREAD_POOL_DEFAULT

src/shell/commands/data_operations.cpp
@@ -2240,7 +2240,9 @@ create_rdb_estimated_keys_stats_calcs(const int32_t table_id,
 
     partition_stat_map sums;
     for (size_t i = 0; i < rows.size(); ++i) {
-        if (pcs[i].hp_primary != node) {
+        dsn::host_port primary;
+        GET_HOST_PORT(pcs[i], primary, primary);
+        if (primary != node) {
             // Ignore once the replica of the metrics is not the primary of the partition.
             continue;
         }
@@ -2885,9 +2887,12 @@ bool calculate_hash_value(command_executor *e, shell_context *sc, arguments args
         tp.add_row_name_and_data("partition_index", partition_index);
         if (pcs.size() > partition_index) {
             const auto &pc = pcs[partition_index];
-            tp.add_row_name_and_data("primary", pc.hp_primary.to_string());
-            tp.add_row_name_and_data("secondaries",
-                                     fmt::format("{}", fmt::join(pc.hp_secondaries, ",")));
+            dsn::host_port primary;
+            GET_HOST_PORT(pc, primary, primary);
+            std::vector<dsn::host_port> secondaries;
+            GET_HOST_PORTS(pc, secondaries, secondaries);
+            tp.add_row_name_and_data("primary", primary);
+            tp.add_row_name_and_data("secondaries", fmt::format("{}", fmt::join(secondaries, ",")));
         }
     }
     tp.output(std::cout);

src/test/kill_test/kill_testor.cpp
@@ -111,18 +111,22 @@ dsn::error_code kill_testor::get_partition_info(bool debug_unhealthy,
         LOG_DEBUG("access meta and query partition status success");
         for (const auto &pc : pcs) {
             int replica_count = 0;
-            if (pc.hp_primary) {
+            dsn::host_port primary;
+            GET_HOST_PORT(pc, primary, primary);
+            if (primary) {
                 replica_count++;
             }
-            replica_count += pc.hp_secondaries.size();
+            std::vector<dsn::host_port> secondaries;
+            GET_HOST_PORTS(pc, secondaries, secondaries);
+            replica_count += secondaries.size();
             if (replica_count == pc.max_replica_count) {
                 healthy_partition_cnt++;
             } else {
                 const auto &info =
                     fmt::format("gpid={}, primary={}, secondaries=[{}], last_committed_decree={}",
                                 pc.pid,
-                                pc.hp_primary,
-                                fmt::join(pc.hp_secondaries, ", "),
+                                primary,
+                                fmt::join(secondaries, ", "),
                                 pc.last_committed_decree);
                 if (debug_unhealthy) {
                     LOG_INFO("found unhealthy partition, {}", info);