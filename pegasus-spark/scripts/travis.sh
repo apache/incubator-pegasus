d84545fc7f5c1502008a6c732f21be15e7096450
lupengfan1 <lupengfan1@xiaomi.com>
2025-03-19 10:49:05 +0800

    move pegasus-spark scripts to subdirectory

---
+++ pegasus-spark/scripts/travis.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -e
+
+SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
+PROJECT_DIR=$(dirname "${SCRIPT_DIR}")
+cd "${PROJECT_DIR}" || exit 1
+
+# lint all scripts, abort if there's any warning.
+function shellcheck_must_pass()
+{
+    if [[ $(shellcheck "$1") ]]; then
+        echo "shellcheck $1 failed"
+        shellcheck "$1"
+        exit 1
+    fi
+}
+shellcheck_must_pass ./scripts/format-all.sh
+shellcheck_must_pass ./scripts/travis.sh
+
+# ensure source files are well formatted
+./scripts/format-all.sh
+if [[ $(git status -s) ]]; then
+    git status -s
+    echo "please format the above files before commit"
+    exit 1
+fi
