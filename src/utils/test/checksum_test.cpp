6eaac5e5727e0cec08780161e3514708e160030d
Dan Wang <wangdan@apache.org>
2025-04-22 15:11:14 +0800

    add tests for checksum

---
+++ src/utils/test/checksum_test.cpp
@@ -0,0 +1,110 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+#include "utils/checksum.h"
+
+#include <rocksdb/env.h>
+#include <rocksdb/slice.h>
+#include <rocksdb/status.h>
+
+#include "gtest/gtest.h"
+#include "test_util/test_util.h"
+#include "utils_types.h"
+#include "utils/env.h"
+#include "utils/errors.h"
+#include "utils/defer.h"
+#include "utils/filesystem.h"
+
+DSN_DECLARE_bool(encrypt_data_at_rest);
+
+namespace dsn {
+
+namespace {
+
+void generate_test_file(const std::string &file_path, int64_t file_size)
+{
+    const auto status =
+        rocksdb::WriteStringToFile(utils::PegasusEnv(dsn::utils::FileDataType::kSensitive),
+                                   rocksdb::Slice(std::string(file_size, 'a')),
+                                   file_path,
+                                   /* should_sync */ true);
+    ASSERT_TRUE(status.ok()) << status.ToString();
+}
+
+} // anonymous namespace
+
+struct calc_checksum_case
+{
+    int64_t file_size;
+    utils::checksum_type::type type;
+    error_code expected_err;
+    std::string expected_checksum;
+};
+
+class CalcChecksumTest : public testing::TestWithParam<calc_checksum_case>
+{
+public:
+    void test_calc_md5(bool encrypted) const
+    {
+        static const std::string kFilePath("test_file_for_calc_checksum");
+
+        PRESERVE_FLAG(encrypt_data_at_rest);
+        FLAGS_encrypt_data_at_rest = encrypted;
+
+        const auto &test_case = GetParam();
+
+        generate_test_file(kFilePath, test_case.file_size);
+        const auto cleanup = defer([]() { utils::filesystem::remove_path(kFilePath); });
+
+        int64_t file_size = 0;
+        ASSERT_TRUE(
+            utils::filesystem::file_size(kFilePath, utils::FileDataType::kSensitive, file_size));
+        ASSERT_EQ(test_case.file_size, file_size);
+
+        std::string actual_checksum;
+        const auto actual_status = calc_checksum(kFilePath, test_case.type, actual_checksum);
+        ASSERT_EQ(test_case.expected_err, actual_status.code());
+        if (!actual_status) {
+            return;
+        }
+
+        ASSERT_EQ(test_case.expected_checksum, actual_checksum);
+    }
+};
+
+const std::vector<calc_checksum_case> calc_checksum_tests = {
+    {0, utils::checksum_type::CST_MD5, ERR_OK, "d41d8cd98f00b204e9800998ecf8427e"},
+    {1, utils::checksum_type::CST_MD5, ERR_OK, "0cc175b9c0f1b6a831c399e269772661"},
+    {2, utils::checksum_type::CST_MD5, ERR_OK, "4124bc0a9335c27f086f24ba207a4912"},
+    {4095, utils::checksum_type::CST_MD5, ERR_OK, "559110baa849c7608ee70abe1d76273e"},
+    {4096, utils::checksum_type::CST_MD5, ERR_OK, "21a199c53f422a380e20b162fb6ebe9c"},
+    {4097, utils::checksum_type::CST_MD5, ERR_OK, "8cfc1a0bd8cd76599e76e5e721c6e62e"},
+    {4095, utils::checksum_type::CST_NONE, ERR_OK, ""},
+    {4096, utils::checksum_type::CST_NONE, ERR_OK, ""},
+    {4097, utils::checksum_type::CST_NONE, ERR_OK, ""},
+    {4095, utils::checksum_type::CST_INVALID, ERR_NOT_IMPLEMENTED, ""},
+    {4096, utils::checksum_type::CST_INVALID, ERR_NOT_IMPLEMENTED, ""},
+    {4097, utils::checksum_type::CST_INVALID, ERR_NOT_IMPLEMENTED, ""},
+};
+
+TEST_P(CalcChecksumTest, CalcEncryptedMD5) { test_calc_md5(true); }
+
+TEST_P(CalcChecksumTest, CalcUnencryptedMD5) { test_calc_md5(false); }
+
+INSTANTIATE_TEST_SUITE_P(ChecksumTest, CalcChecksumTest, testing::ValuesIn(calc_checksum_tests));
+
+} // namespace dsn
