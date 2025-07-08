c818247bee9044dbf1ade43bacaa01d9d5af00e4
Dan Wang <wangdan@apache.org>
2025-07-08 15:02:38 +0800

    fix compilation

--- src/test/function_test/base_api/incr_test.cpp
+++ src/test/function_test/base_api/incr_test.cpp
@@ -1,21 +1,19 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
 
 #include <gtest/gtest.h>
 #include <unistd.h>
@@ -31,6 +29,8 @@ namespace pegasus {
 
 class IncrTest : public AtomicWriteTest
 {
+protected:
+    IncrTest() : AtomicWriteTest("incr_test") {}
 };
 
 TEST_P(IncrTest, unexist_key)
@@ -154,7 +154,8 @@ TEST_P(IncrTest, Overflow)
 
     int64_t new_value_int;
     ASSERT_EQ(PERR_INVALID_ARGUMENT,
-              _client->incr("incr_test_overflow", "", std::numeric_limits<int64_t>::max(), new_value_int));
+              _client->incr(
+                  "incr_test_overflow", "", std::numeric_limits<int64_t>::max(), new_value_int));
     ASSERT_EQ(1, new_value_int);
 
     std::string new_value_str;
@@ -170,7 +171,8 @@ TEST_P(IncrTest, Underflow)
 
     int64_t new_value_int;
     ASSERT_EQ(PERR_INVALID_ARGUMENT,
-              _client->incr("incr_test_underflow", "", std::numeric_limits<int64_t>::min(), new_value_int));
+              _client->incr(
+                  "incr_test_underflow", "", std::numeric_limits<int64_t>::min(), new_value_int));
     ASSERT_EQ(-1, new_value_int);
 
     std::string new_value_str;
@@ -248,6 +250,8 @@ TEST_P(IncrTest, ResetTtl)
     ASSERT_EQ(PERR_OK, _client->del("incr_test_reset_ttl", ""));
 }
 
-INSTANTIATE_TEST_SUITE_P(AtomicWriteTest, IncrTest, testing::ValuesIn(generate_atomic_write_cases()));
+INSTANTIATE_TEST_SUITE_P(AtomicWriteTest,
+                         IncrTest,
+                         testing::ValuesIn(generate_atomic_write_cases()));
 
 } // namespace pegasus
