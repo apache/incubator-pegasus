f5a72c3f0c3b5287f19106466ab8cfe377fc8dd1
samunroyu <yujingweiop@gmail.com>
2025-09-30 15:30:00 +0800

    refactor(fqdn): refactor func maintain_drops to process rpc_address and rpc_hostport

--- src/meta/meta_data.h
+++ src/meta/meta_data.h
@@ -549,30 +549,30 @@ inline int count_partitions(const app_mapper &apps)
 
 void when_update_replicas(config_type::type t, const std::function<void(bool)> &func);
 
-#define MAINTAIN_DROP_NODE(obj, field, type)                                \
-    do {                                                                    \
-        auto obj_copy = obj;                                                \
-        if (obj.primary) {                                                  \
-            maintain_drops(obj_copy.field, obj.primary, type);              \
-        }                                                                   \
-        if (obj.__isset.hp_primary && obj.hp_primary) {                     \
-            maintain_drops(obj_copy.hp_##field, obj.hp_primary, type);      \
-        }                                                                   \
-    } while (0)
+#define MAINTAIN_DROP_NODE(obj, field, type)                                                       \
+    do {                                                                                           \
+        auto obj_copy = obj;                                                                       \
+        if (obj.primary) {                                                                         \
+            maintain_drops(obj_copy.field, obj.primary, type);                                     \
+        }                                                                                          \
+        if (obj.__isset.hp_primary && obj.hp_primary) {                                            \
+            maintain_drops(obj_copy.hp_##field, obj.hp_primary, type);                             \
+        }                                                                                          \
+     } while (0)
 
 
-#define MAINTAIN_DROP_NODES(obj, field, type)                               \
-    do {                                                                    \
-        auto obj_copy = obj;                                                \
-        for (const auto &secondary : obj.secondaries) {                     \
-            maintain_drops(obj_copy.field, secondary, type);                \
-        }                                                                   \
-        if (obj.__isset.hp_secondaries) {                                   \
-            for (const auto &secondary : obj.hp_secondaries) {              \
-                maintain_drops(obj_copy.hp_##field, secondary, type);       \
-            }                                                               \
-        }                                                                   \
-    } while (0)
+#define MAINTAIN_DROP_NODES(obj, field, type)                                                      \
+    do {                                                                                           \
+        auto obj_copy = obj;                                                                       \
+        for (const auto &secondary : obj.secondaries) {                                            \
+            maintain_drops(obj_copy.field, secondary, type);                                       \
+        }                                                                                          \
+        if (obj.__isset.hp_secondaries) {                                                          \
+            for (const auto &secondary : obj.hp_secondaries) {                                     \
+                maintain_drops(obj_copy.hp_##field, secondary, type);                              \
+            }                                                                                      \
+        }                                                                                          \
+     } while (0)
 
 template <typename T>
 void maintain_drops(/*inout*/ std::vector<T> &drops, const T &node, config_type::type t)
