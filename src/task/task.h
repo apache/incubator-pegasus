e2349bb1aaa750153430fddeca16b6940f152789
lixuejian7 <lixuejian7@xiaomi.com>
2026-02-12 15:41:57 +0800

    fix(duplication): fix potential core of duplication during primary replica migration

--- src/task/task.h
+++ src/task/task.h
@@ -329,6 +329,12 @@ public:
         : task(code, hash, node), _cb(std::move(cb))
     {
     }
+    ~raw_task() override
+    {
+        ::dsn::utils::auto_lock<::dsn::utils::ex_lock_nr> l(_lock);
+        _cb = nullptr;
+    }
+
 
     void exec() override
     {
@@ -338,10 +344,16 @@ public:
     }
 
 protected:
-    void clear_non_trivial_on_task_end() override { _cb = nullptr; }
+    void clear_non_trivial_on_task_end() override
+    {
+        ::dsn::utils::auto_lock<::dsn::utils::ex_lock_nr> l(_lock);
+        _cb = nullptr;
+    }
+
 
 protected:
     task_handler _cb;
+    ::dsn::utils::ex_lock_nr  _lock;
 };
 
 //----------------- timer task -------------------------------------------------------
